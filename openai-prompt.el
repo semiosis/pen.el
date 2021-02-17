aee955d9b7b47e96f7a7a509187f70892965a6d0
Shane Mulligan <mullikine@gmail.com>
2021-02-17 16:37:42 +1300

    17.02.21

--- openai-prompt.el
+++ openai-prompt.el
@@ -12,13 +12,19 @@
 
 ;; (define-globalized-mode global-openai-prompt-mode openai-prompt-mode openai-prompt-mode)
 
-;; This is definitely a bad solution
-;; (define-key openai-prompt-mode-map (kbd "<up>") (lm (tsk "C-p")))
+;; TODO Make a shell script for querying the OpenAI API
+;; $SCRIPTS/openai-curl
+
+;; TODO oai-generate
+;; This function streams text onto the end of the currently selected region accordig to the current prompt settings
+
+;; (define-key openai-prompt-mode-map (kbd "<up>") 'oai-generate)
 ;; (define-key openai-prompt-mode-map (kbd "<down>") (lm (tsk "C-n")))
 ;; (define-key openai-prompt-mode-map (kbd "<right>") (lm (tsk "M-3")))
 
 (openai-prompt-mode 1)
 
+;; TODO Create a transient mode for quickly setting prompt settings
 
 ;; (define-transient-command openai-prompt-configure ()
 ;;   "Transient for configuring the current openai prompt fuction."
@@ -36,7 +42,27 @@
   :group 'openai-prompt
   :initialize #'custom-initialize-default)
 
-(defvar-local openai-prompt-mode nil)
+(defvar-local oai-prompt-engine nil)
+
+(defvar-local oai-prompt-frequency-penalty nil
+  "If your completion is filled with lots of\nrepetition you can increase this setting\nto prevent that from happening.\n\nHow much to penalize new tokens based on\ntheir existing frequency in the text so\nfar.\n\nDecreases the model's likelihood to repeat\nthe same line verbatim (the same text).\n\nThe likelihood of the same/similar lines\nbeing repeated in a completion.\n\nSet it high to avoid repetition.\n\nHowever, it could make sense to lower this\nvalue if writing lyrics for a song's\nchorus.")
+
+(defvar-local oai-prompt-presence-penalty nil)
+
+(defvar-local oai-prompt-best-of nil)
+
+(defvar-local oai-prompt-stop-sequences nil)
+
+(defvar-local oai-prompt-inject-start-text nil)
+
+(defvar-local oai-prompt-inject-restart-text nil)
+
+(defvar-local oai-prompt-show-probabilities nil)
+;; + States
+;;   - Off
+;;   - Most likely
+;;   - Least likely
+;;   - Full spectrum
 
 (provide 'openai-prompt-mode)
 
