81ebec153e79fe84af5b6f1714319bd871193600
Shane Mulligan <mullikine@gmail.com>
2021-03-31 20:42:22 +1300

    Wed 31 Mar 2021 20:42:21

--- examplary.el
+++ examplary.el
@@ -1,4 +1,31 @@
 (require 'pen)
 
 
+;; Running the function generates a prompt
+
+
+;; Use a hash table for the data structure? 
+
+
+(def lines->regex
+     :e "grex"
+     "example 1\nexample2" "^example [12]$"
+     "example 2\nexample3" "^example [23]$"
+     "pi4\npi5" "^pi[45]$")
+
+
+
+
+(defun generate-prompt-from-xlr-object (o)
+    (let ((tuples (("example 1\nexample2" "^example [12]$")
+                   ("example 2\nexample3" "^example [23]$")
+                   ("pi4\npi5" "^pi[45]$")))))
+
+
+  (if )
+  :e "grex"
+  ;; List of tuples
+  (cl-loop for t in 
+           ))
+
 (provide 'examplary)
