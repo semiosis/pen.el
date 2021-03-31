baa097aab9e3f63624490ff8b5bde19fd5c579a4
Shane Mulligan <mullikine@gmail.com>
2021-03-31 20:58:58 +1300

    Wed 31 Mar 2021 20:58:57

--- examplary.el
+++ examplary.el
@@ -4,7 +4,10 @@
 ;; Running the function generates a prompt
 
 
-;; Use a hash table for the data structure? 
+;; Use a hash table for the data structure?
+
+
+;; e:$MYGIT/mullikine/elisp-playground/pen.el
 
 
 (never
