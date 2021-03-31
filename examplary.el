9cadecafc521aef0f6c07ed73002d52b4ac932ef
Shane Mulligan <mullikine@gmail.com>
2021-03-31 20:58:01 +1300

    Wed 31 Mar 2021 20:58:00

--- examplary.el
+++ examplary.el
@@ -7,14 +7,12 @@
 ;; Use a hash table for the data structure? 
 
 
-(def lines->regex
-     :e "grex"
-     "example 1\nexample2" "^example [12]$"
-     "example 2\nexample3" "^example [23]$"
-     "pi4\npi5" "^pi[45]$")
-
-
-
+(never
+ (def lines->regex
+      :e "grex"
+      "example 1\nexample2" "^example [12]$"
+      "example 2\nexample3" "^example [23]$"
+      "pi4\npi5" "^pi[45]$"))
 
 (defun generate-prompt-from-xlr-object (o)
     (let ((tuples (("example 1\nexample2" "^example [12]$")
@@ -22,8 +20,7 @@
                    ("pi4\npi5" "^pi[45]$")))))
 
 
-  (if )
-  :e "grex"
+    :e "grex"
   ;; List of tuples
   (cl-loop for t in 
            ))
