b4d32e677c47bde791bce645cf4e3e9f8da463b0
Shane Mulligan <mullikine@gmail.com>
2021-03-31 20:59:50 +1300

    Wed 31 Mar 2021 20:59:49

--- examplary.el
+++ examplary.el
@@ -6,26 +6,6 @@
 
 ;; Use a hash table for the data structure?
 
-
 ;; e:$MYGIT/mullikine/elisp-playground/pen.el
-
-
-(never
- (def lines->regex
-      :e "grex"
-      "example 1\nexample2" "^example [12]$"
-      "example 2\nexample3" "^example [23]$"
-      "pi4\npi5" "^pi[45]$"))
-
-(defun generate-prompt-from-xlr-object (o)
-    (let ((tuples (("example 1\nexample2" "^example [12]$")
-                   ("example 2\nexample3" "^example [23]$")
-                   ("pi4\npi5" "^pi[45]$")))))
-
-
-    :e "grex"
-  ;; List of tuples
-  (cl-loop for t in 
-           ))
 
 (provide 'examplary)
