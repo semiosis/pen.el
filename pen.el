8593a313d09701e11337cb8d9590aa8d72a285a0
Shane Mulligan <mullikine@gmail.com>
2021-04-01 19:30:09 +1300

    Thu 01 Apr 2021 19:30:09

--- pen.el
+++ pen.el
@@ -321,6 +321,22 @@ Function names are prefixed with pen-pf- for easy searching"
                        (org-brain-pf-topic short)))
                (t
                 (let ((current-prefix-arg '(4))) ; C-u
+                  ;; Consider getting topic keywords from visual text
+                  (get-path))))))
+    (if (interactive-p)
+        (etv topic)
+      topic)))
+
+(defun pen-broader-topic ()
+  "Determine the topic used for pen functions"
+  (interactive)
+
+  (let ((topic
+         (cond ((major-mode-p 'org-brain-visualize-mode)
+                (progn (require 'my-org-brain)
+                       (org-brain-pf-topic short)))
+               (t
+                (let ((current-prefix-arg '(4))) ; C-u
                   (get-path))))))
     (if (interactive-p)
         (etv topic)
