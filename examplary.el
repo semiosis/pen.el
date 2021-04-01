ae4033fd26e38cd293e97820cdce53f2ba8145fc
Shane Mulligan <mullikine@gmail.com>
2021-04-01 19:20:22 +1300

    Thu 01 Apr 2021 19:20:22

--- examplary.el
+++ examplary.el
@@ -1,5 +1,5 @@
 (require 'pen)
-
+(require 'examplary-core)
 
 ;; Running the function generates a prompt
 
@@ -10,21 +10,5 @@
 
 (load (concat emacsdir "/config/examplary-library.el"))
 (require 'examplary-library)
-
-(defun org-brain-asktutor (question)
-  (interactive (list (read-string-hist (concat "asktutor about " (pen-topic) ": "))))
-  (let ((cname (org-brain-current-name))
-        (pname (org-brain-parent-name)))
-
-    (if (or (string-equal "infogetics" cname)
-            (string-equal "fungible" cname))
-        (setq cname "general knowledge"))
-
-    (if (or (string-equal "infogetics" pname)
-            (string-equal "fungible" pname))
-        (setq pname cname))
-
-    (etv (eval
-          `(ci (snc "ttp" (pen-pf-generic-tutor-for-any-topic ,cname ,pname ,question)))))))
 
 (provide 'examplary)
