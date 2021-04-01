44fd46262e5aff906a37792c57a247cbec3d3365
Shane Mulligan <mullikine@gmail.com>
2021-04-01 19:24:36 +1300

    Thu 01 Apr 2021 19:24:36

--- examplary-library.el
+++ examplary-library.el
@@ -1,17 +1,6 @@
-(defun examplary-asktutor (question)
+(defun examplary-asktutor (broad-topic specific-topic question)
   (interactive (list (read-string-hist (concat "asktutor about " (pen-topic t) ": "))))
-  (let ((cname (org-brain-current-name))
-        (pname (org-brain-parent-name)))
-
-    (if (or (string-equal "infogetics" cname)
-            (string-equal "fungible" cname))
-        (setq cname "general knowledge"))
-
-    (if (or (string-equal "infogetics" pname)
-            (string-equal "fungible" pname))
-        (setq pname cname))
-
-    (etv (eval
-          `(ci (snc "ttp" (pen-pf-generic-tutor-for-any-topic ,cname ,pname ,question)))))))
+  (etv (eval
+        `(ci (snc "ttp" (pen-pf-generic-tutor-for-any-topic ,cname ,pname ,question))))))
 
 (provide 'examplary-libary)
