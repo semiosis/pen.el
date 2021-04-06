5d3695723f5ae240e36663d7b5e3ca2a9d3903a7
Shane Mulligan <mullikine@gmail.com>
2021-04-06 15:56:41 +1200

    Tue 06 Apr 2021 15:56:41

--- pen-ivy.el
+++ pen-ivy.el
@@ -28,12 +28,43 @@
 (never
  (ivy-read "testing:"
            (counsel-fz-function-openai-complete-very-witty-pick-up-lines-prompt "tea")
-           :initial-input initial-input
+           ;; :initial-input initial-input
            :dynamic-collection t
            :keymap counsel-ag-map
-           :history histvar
+           ;; :history histvar
            :action 'etv
            :require-match t
-           :caller (or caller 'counsel-ag)))
+           ;; :caller (or caller 'counsel-ag)
+           ))
+
+
+(defun counsel-ag-function (string)
+  "Grep in the current directory for STRING."
+  (let* ((command-args (counsel--split-command-args string))
+         (search-term (cdr command-args)))
+    (or
+     (let ((ivy-text search-term))
+       (ivy-more-chars))
+     (let* ((default-directory (ivy-state-directory ivy-last))
+            ;; (regex (counsel--grep-regex search-term))
+            (switches (concat (car command-args)
+                              ;; (counsel--ag-extra-switches regex)
+
+                              ;; Don't add extra arguments or generic programs other than ag will have problems
+
+                              ;; (if (ivy--case-fold-p string)
+                              ;;     " -i "
+                              ;;   " -s ")
+                              )))
+       (counsel--async-command (counsel--format-ag-command
+                                switches
+                                (funcall (if (listp counsel-ag-command) #'identity
+                                           #'shell-quote-argument)
+                                         ;; regex
+                                         string
+                                         )))
+       nil))))
+
+
 
 (provide 'pen-ivy)
