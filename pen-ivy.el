414b2b20de57c1b38365d42cedb4eeee90a83c6c
Shane Mulligan <mullikine@gmail.com>
2021-04-06 19:24:04 +1200

    Tue 06 Apr 2021 19:24:03

--- pen-ivy.el
+++ pen-ivy.el
@@ -104,7 +104,7 @@ prompt additionally for EXTRA-AG-ARGS."
 (defun fz-pen-counsel ()
   (interactive)
   (mu (let* ((pfp (fz (snc "cd $MYGIT/semiosis/prompts/prompts; find . -maxdepth 1 -mindepth 1 -type f | sed 's/..//'")))
-            (cf (eval `(gen-counsel-function ,(concat "loop openai-complete -s " (q pfp)) 'etv))))
+             (cf (eval `(gen-counsel-function ,(concat "loop openai-complete -s " (q pfp)) 'etv))))
         (call-interactively cf))))
 
 
