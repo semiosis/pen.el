16ea91effa6a4e561b7b8baa2609bfef9b7ea667
Shane Mulligan <mullikine@gmail.com>
2021-04-12 17:34:14 +1200

    Mon 12 Apr 2021 17:34:14

--- pen.el
+++ pen.el
@@ -168,7 +168,7 @@ Function names are prefixed with pen-pf- for easy searching"
                       (let ((ss (mapcar 'str2sym var-slugs)))
                         (message (concat "_" prettifier))
                         (if (sor prettifier)
-                            (setq ss (append ss '(:key prettify))))
+                            (setq ss (append ss '(&key prettify))))
                         ;; (setq ss (append ss '(:key prettify)))
                         ss))
                      (pen-defaults (vector2list (ht-get yaml "pen-defaults")))
