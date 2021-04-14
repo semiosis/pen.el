4fd9215cfc63aec20a0d46a14fc1da8e608cff72
Shane Mulligan <mullikine@gmail.com>
2021-04-15 09:22:03 +1200

    Thu 15 Apr 2021 09:22:03

--- pen-core.el
+++ pen-core.el
@@ -6,7 +6,9 @@
 (defun pen-preceding-text ()
   (str (buffer-substring (point) (max 1 (- (point) 1000)))))
 
-(defun pen-surrounding-text ()
+(defun pen-surrounding-text (&optional window-line-size)
+  (if (not window-line-size)
+      (setq window-line-size 20))
   (str (buffer-substring (point) (max 1 (- (point) 1000)))))
 
 
