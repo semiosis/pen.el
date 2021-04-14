de98fdda316eacc9c0fa718cba32a0ba24ac3ac9
Shane Mulligan <mullikine@gmail.com>
2021-04-15 09:37:48 +1200

    Thu 15 Apr 2021 09:37:48

--- pen-core.el
+++ pen-core.el
@@ -6,10 +6,6 @@
 (defun pen-preceding-text ()
   (str (buffer-substring (point) (max 1 (- (point) 1000)))))
 
-(defmacro ntimes (n &rest body)
-  (flatten-once 
-   (cl-loop for i from 1 to n collect body)))
-
 (defun pen-surrounding-text (&optional window-line-size)
   (if (not window-line-size)
       (setq window-line-size 20))
