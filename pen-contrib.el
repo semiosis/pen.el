92747518b6447758447c7660d02148f2142e482e
Shane Mulligan <mullikine@gmail.com>
2021-06-02 18:36:01 +1200

    Wed 02 Jun 2021 18:36:01

--- pen-contrib.el
+++ pen-contrib.el
@@ -3,7 +3,10 @@
 
 (defun pen-tutor-mode-assist (query)
   (interactive (let* ((bl (buffer-language t t)))
-                 (list (read-string-hist (concat "asktutor (" bl "): ")))))
+                 (list
+                  (read-string-hist
+                   (concat "asktutor (" bl "): ")
+                   (my/thing-at-point)))))
   (pen-pf-asktutor bl bl query))
 
 (provide 'pen-contrib)
