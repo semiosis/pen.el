e2e59f38dc863bc53ad45ccd60f226d0d726d179
Shane Mulligan <mullikine@gmail.com>
2021-07-02 10:54:57 +1200

    1625180097

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -1,1 +1,7 @@
+# For unbuffer
+# http://github.com/semiosis/pen.el/blob/master/pen-support.el
 sudo apt install expect
+
+# For iconv
+# http://github.com/semiosis/pen.el/blob/master/scripts/slugify
+sudo api install libc-bin
