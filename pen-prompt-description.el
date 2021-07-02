62780415ccaccf1320fdbe2ab14ba480b1354adb
Shane Mulligan <mullikine@gmail.com>
2021-07-02 14:55:30 +1200

    Fri 02 Jul 2021 14:55:30

--- pen-prompt-description.el
+++ pen-prompt-description.el
@@ -5,7 +5,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.prompt\\'" . prompt-description-mode))
 
-;; I would like to disable the yaml lsp server for .prompt files
+;; I would like to disable the yaml lsp server for .prompt files.
+;; At least, until a schema for it is made in schemastore
 (defun maybe-lsp ()
   "Maybe run lsp."
   (interactive)
