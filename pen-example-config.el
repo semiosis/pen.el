9a0ceadacb6e28ffc4166cc2b0cf0b8e2ebe756c
Shane Mulligan <mullikine@gmail.com>
2021-07-02 12:54:35 +1200

    Fri 02 Jul 2021 12:54:35

--- pen-example-config.el
+++ pen-example-config.el
@@ -60,6 +60,8 @@
 (define-key pen-map (kbd "H-TAB g") 'pen-generate-prompt-functions)
 (define-key pen-map (kbd "H-s") 'fz-pen-counsel)
 (define-key pen-map (kbd "H-TAB s") 'pen-filter-with-prompt-function)
+
+(require 'org-brain)
 (define-key org-brain-visualize-mode-map (kbd "C-c a") 'org-brain-asktutor)
 (define-key org-brain-visualize-mode-map (kbd "C-c t") 'org-brain-show-topic)
 (define-key org-brain-visualize-mode-map (kbd "C-c d") 'org-brain-describe-topic)
