709a62dd88e67b83b702c6768a3a24a08915f827
Shane Mulligan <mullikine@gmail.com>
2021-07-02 17:51:19 +1200

    Fri 02 Jul 2021 17:51:19

--- pen-prompt-tablist.el
+++ pen-prompt-tablist.el
@@ -27,6 +27,80 @@
 
 (defcmdmode "prompts" "tablist")
 
+(defun tablist-buffer-from-csv-string (csvstring &optional has-header col-sizes)
+  "This creates a new tabulated list buffer from a CSV string"
+  (let* ((b (nbfs csvstring "tablist"))
+         (parsed (pcsv-parse-buffer b))
+         (header (if has-header
+                     (first parsed)
+                   (mapcar (lambda (s) "") (first parsed))))
+         (data (if has-header
+                   (-drop 1 parsed)
+                 parsed)))
+
+    (switch-to-buffer b)
+    (with-current-buffer b
+
+      (setq-local tabulated-list-format
+                  (si "tabulated-list-format format"
+                      (list2vec
+                       (let* ((sizes
+                               (or col-sizes
+                                   (mapcar (lambda (e)
+                                             (max my-tablist-min-column-width (min 30 (length e))))
+                                           header)))
+                              (trues (mapcar (lambda (e) t)
+                                             header)))
+                         (-zip header sizes trues))
+                       )))
+      (setq-local tabulated-list-sort-key (list (first header)))
+
+      (setq-local tabulated-list-entries (-map (lambda (l) (list (first l) (list2vec l))) data))
+
+      (tabulated-list-mode)
+
+      (tabulated-list-init-header)
+
+      (tabulated-list-print)
+      (tablist-enlarge-column)
+      (tablist-shrink-column)
+      (revert-buffer)
+      (tablist-forward-column 1)
+      (tabulated-list-revert))
+    b))
+
+(defun create-tablist (cmd-or-csv-path &optional modename has-header col-sizes-string wd)
+  "Try to create a tablist from a cmd or a csv path"
+  (interactive (list (read-string-hist "create-tablist: CMD or CSV path: ")))
+
+  (setq has-header
+        (if (sor has-header)
+            t
+          nil))
+
+  (let* ((path (if (and
+                    (f-file-p cmd-or-csv-path)
+                    (not (f-executable-p cmd-or-csv-path)))
+                   cmd-or-csv-path))
+         (cmd (if (not path)
+                  cmd-or-csv-path))
+         (col-sizes
+          (if (sor col-sizes-string)
+              (try (mapcar 'string-to-int (uncmd col-sizes-string))))))
+
+    (let ((b (cond ((sor path) (tablist-buffer-from-csv-string (cat path) has-header col-sizes))
+                   ((sor cmd) (tablist-buffer-from-csv-string (sn cmd) has-header col-sizes)))))
+      (if b
+          (with-current-buffer
+              b
+            (if (sor wd) (cd wd))
+            (let ((modefun (str2sym (concat (slugify (or modename cmd-or-csv-path)) "-tablist-mode"))))
+              (if (function-p modefun)
+                  (funcall modefun)))
+            b)
+        (error "tablist not created")
+        nil))))
+
 (defun pen-prompts-tablist-start ()
   (interactive)
   (let* ((sh-update (>= (prefix-numeric-value current-global-prefix-arg) 16)))
