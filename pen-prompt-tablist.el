f6b25531e1ad064be092470745cc31b8f4c500d7
Shane Mulligan <mullikine@gmail.com>
2021-07-02 17:38:07 +1200

    Fri 02 Jul 2021 17:38:07

--- pen-prompt-tablist.el
+++ pen-prompt-tablist.el
@@ -1,7 +1,9 @@
 (require 'tabulated-list)
 (require 'tablist)
 
+(defvar pen-prompts-tablist-data-command "oci prompts-details -csv")
+
 (defvar pen-prompts-tablist-meta
-  '("oci prompts-details -csv" . ("prompts" t "30 30 20 10 15 15 15 10")))
+  `(pen-prompts-tablist-data-command . ("prompts" t "30 30 20 10 15 15 15 10")))
 
 (provide 'pen-tablist)
