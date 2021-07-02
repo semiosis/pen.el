7992d209b56d155e63d760e0f85e94ee18f14e9d
Shane Mulligan <mullikine@gmail.com>
2021-07-02 17:46:01 +1200

    Fri 02 Jul 2021 17:46:01

--- pen-prompt-tablist.el
+++ pen-prompt-tablist.el
@@ -3,7 +3,34 @@
 
 (defvar pen-prompts-tablist-data-command "oci prompts-details -csv")
 
-(defvar pen-prompts-tablist-meta
-  `(pen-prompts-tablist-data-command . ("prompts" t "30 30 20 10 15 15 15 10")))
+(defvar pen-prompts-tablist-meta ("prompts" t "30 30 20 10 15 15 15 10"))
+
+(defmacro defcmdmode (cmd &optional cmdtype)
+  (setq cmd (str cmd))
+  (setq cmdtype (or cmdtype "term"))
+  (let* ((cmdslug (slugify (str cmd)))
+         (modestr (concat cmdslug "-" cmdtype "-mode"))
+         (modesym (str2sym modestr))
+         (mapsym (str2sym (concat modestr "-map"))))
+    `(progn
+       (defvar ,mapsym (make-sparse-keymap)
+         ,(concat "Keymap for `" modestr "'."))
+       (defvar-local ,modesym nil)
+
+       (define-minor-mode ,modesym
+         ,(concat "A minor mode for the '" cmd "' " cmdtype " command.")
+         :global nil
+         :init-value nil
+         :lighter ,(s-upcase cmdslug)
+         :keymap ,mapsym)
+       (provide ',modesym))))
+
+(defcmdmode "prompts" "tablist")
+
+(defun pen-prompts-tablist-start ()
+  (interactive)
+  (let* ((sh-update (>= (prefix-numeric-value current-global-prefix-arg) 16))
+         (args pen-prompts-tablist-meta))
+    (apply 'create-tablist args)))
 
 (provide 'pen-tablist)
