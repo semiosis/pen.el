494d06dcf500671f0613c104ff423692493c6ff7
Shane Mulligan <mullikine@gmail.com>
2021-07-02 12:21:35 +1200

    Fri 02 Jul 2021 12:21:35

---
+++ pen-brain.el
@@ -0,0 +1,34 @@
+(require 'org-brain)
+(require 'pen-support)
+
+(defun org-brain-current-brain ()
+  (f-base org-brain-path))
+
+(defun org-brain-parent-name ()
+  (mapconcat 'identity
+             (org-brain-remove-irrelevant-names-from-path
+              (mapcar
+               'org-brain-name-from-list-maybe
+               (org-brain-parents org-brain--vis-entry)))
+             " "))
+
+(defun org-brain-is-index-name (s)
+  (or (string-equal s "index")
+      (string-equal s "reference")))
+
+(defun org-brain-pf-topic (&optional short)
+  "Topic used for pen functions"
+  (let ((cn (org-brain-current-name t)))
+    (if (and (org-brain-is-index-name cn)
+             (not (sor (org-brain-parent-name))))
+        (if (org-brain-is-index-name (org-brain-current-brain))
+            "general knowledge"
+          (org-brain-current-brain))
+      (let ((p (org-brain-parent-name)))
+        (if (and (sor p)
+                 (not short)
+                 (not (org-brain-is-index-name p)))
+            (concat cn " (" cn " is a subtopic of " p ")")
+          cn)))))
+
+(provide 'pen-brain)
--- pen-library.el
+++ pen-library.el
@@ -1,3 +1,68 @@
+(defun get-path-semantic ()
+  (interactive)
+  (cond
+   ((major-mode-enabled 'org-brain-visualize-mode)
+    (org-brain-pf-topic))
+   (t (org-brain-pf-topic))))
+
+;; This is usually used programmatically to get a single path name
+(defun get-path (&optional soft no-create-path for-clipboard semantic-path)
+  "Get path for buffer. semantic-path means a path suitable for google/nl searching"
+  (interactive)
+
+  (setq semantic-path (or
+                       semantic-path
+                       (>= (prefix-numeric-value current-prefix-arg) 4)))
+
+  "If it's just for the clipboard then we can copy"
+  ;; (xc-m (s/rp (buffer-file-name)))
+  (let ((path
+         (or (and (eq major-mode 'Info-mode)
+                  (if soft
+                      (concat "(" (basename Info-current-file) ") " Info-current-node)
+                    (concat Info-current-file ".info")))
+
+             (and (major-mode-enabled 'eww-mode)
+                  (s-replace-regexp "^file:\/\/" ""
+                                    (url-encode-url
+                                     (or (eww-current-url)
+                                         eww-followed-link))))
+
+             (and (major-mode-enabled 'sx-question-mode)
+                  (sx-get-question-url))
+
+             (and (major-mode-enabled 'org-brain-visualize-mode)
+                  (org-brain-get-path-for-entry org-brain--vis-entry semantic-path))
+
+             (and (major-mode-enabled 'ranger-mode)
+                  (dired-copy-filename-as-kill 0))
+
+             (and (major-mode-enabled 'dired-mode)
+                  (string-or (and
+                              for-clipboard
+                              (mapconcat 'q (dired-get-marked-files) " "))
+                             (my/pwd)))
+
+             ;; This will break on eww
+             (if (and (not (eq major-mode 'org-mode))
+                      (string-match-p "\\[\\*Org Src" (or (buffer-file-name) "")))
+                 (s-replace-regexp "\\[\\*Org Src.*" "" (buffer-file-name)))
+             (buffer-file-name)
+             (try (buffer-file-path)
+                  nil)
+             dired-directory
+             (progn (if (not no-create-path)
+                        (save-temp-if-no-file))
+                    (let ((p (full-path)))
+                      (if (stringp p)
+                          (e/chomp p)))))))
+    (if (interactive-p)
+        (my/copy path)
+      path)))
+
+(defun get-path-nocreate ()
+  (get-path nil t))
+
 (defun pen-topic (&optional short)
   "Determine the topic used for pen functions"
   (interactive)
