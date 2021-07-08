3ef537c63160d43b1cbc1f12b24c19c69c9d51e5
Shane Mulligan <mullikine@gmail.com>
2021-07-08 18:27:39 +1200

    Thu 08 Jul 2021 18:27:39

--- pen-openai.el
+++ pen-openai.el
@@ -12,4 +12,28 @@
    "instruct-curie-beta"
    "instruct-davinci-beta"))
 
+;; https://github.com/socketteer/loom/blob/main/model.py#L40
+
+(defset default-generation-settings
+  '(("num-continuations" . 4)
+    ("temperature" . 0.9)
+    ("top-p" . 1)
+    ("response-length" . 100)
+    ("prompt-length" . 6000)
+    ("janus" . false)
+    ("adaptive" . false)
+    ("model" . "davinci")
+    ("memory" . "")))
+
+(defset default-visualization-settings
+        '(
+    ("textwidth" . 450)
+    ("leafdist" . 200)
+    ("leveldistance" . 150)
+    ("textsize" . 10)
+    ("horizontal" . true)
+    ("displaytext" . true)
+    ("showbuttons" . true)
+))
+
 (provide 'my-openai)
