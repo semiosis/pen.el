0209d2a59284fc238c0a045b58fef6dd7cf7d7fd
Shane Mulligan <mullikine@gmail.com>
2021-07-08 15:25:07 +1200

    Thu 08 Jul 2021 15:25:07

--- pen.el
+++ pen.el
@@ -179,7 +179,7 @@ Function names are prefixed with pen-pf- for easy searching"
 
 (defun pen-run-prompt-function ()
   (interactive)
-  (let* ((pen-sh-update (or sh-update (>= (prefix-numeric-value current-global-prefix-arg) 4)))
+  (let* ((pen-sh-update (or pen-sh-update (>= (prefix-numeric-value current-global-prefix-arg) 4)))
          (f (fz pen-prompt-functions nil nil "pen run: ")))
     (if f
         (call-interactively (intern f)))))
