12b4254b3d7ccfa86d9c6dbd8dbbecf6d625d55b
Shane Mulligan <mullikine@gmail.com>
2021-07-08 13:26:46 +1200

    Thu 08 Jul 2021 13:26:46

--- pen-example-config.el
+++ pen-example-config.el
@@ -40,7 +40,7 @@
 ;; https://github.com/perfectayush/emacs-yamlmod
 (if module-file-suffix
     (progn
-      (module-load "/home/shane/source/git/config/emacs/manual-packages/emacs-yamlmod/target/release/libyamlmod.so")
+      (module-load (getenv "YAMLMOD_PATH"))
       (add-to-list 'load-path "/home/shane/source/git/config/emacs/manual-packages/emacs-yamlmod")
       (require 'yamlmod)
       (require 'yamlmod-wrapper)))
