a2685a869bea294acad8de68e1f5c4626565b658
Shane Mulligan <mullikine@gmail.com>
2021-07-08 14:18:18 +1200

    08.07.21

---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Debian10 run Pen
+
+export YAMLMOD_PATH=/root/.emacs.d/emacs-yamlmod
+/root/emacs/src/emacs -nw --debug-init
---
+++ scripts/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Debian10 installation
+
+cd ~
+apt install git python vim emacs mosh curl make
+
+# For emacs-yamlmod
+apt install clang libclang1
+
+test -d emacs || git clone "https://github.com/emacs-mirror/emacs"
+
+# For building emacs
+apt install autoconf texinfo gnutls30 gnutls28-dev pkg-config
+
+(
+cd emacs
+git checkout 0a5e9cf2622a0282d56cc150af5a94b5d5fd71be
+make -j10
+)
+
+# rustc and cargo are for building emacs-yamlmod
+# Debian GNU/Linux 10 does not have the required version of rustc
+# rustc 1.41.1 will not build emacs-yaml
+# rustc 1.51.0 will build it
+rustc --version | grep -q " 1.5" || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+cd ~/.emacs.d/emacs-yamlmod
+source $HOME/.cargo/env
+make
