b07c35a5b3d091ae1e47e1da591617e445d7d4ad
Shane Mulligan <mullikine@gmail.com>
2021-07-08 20:53:53 +1200

    Thu 08 Jul 2021 20:53:53

--- pen-brain.el
+++ pen-brain.el
@@ -31,6 +31,57 @@
             (concat cn " (" cn " is a subtopic of " p ")")
           cn)))))
 
+(defun org-brain-show-topic ()
+  (interactive)
+  (call-interactively 'pen-topic))
+
+(defun org-brain-describe-topic ()
+  (interactive)
+
+  (let* ((p (sor (org-brain-parent-name)))
+         (pretext)
+         (question (if (and p (not (org-brain-at-child-of-index)))
+                       (concat "Could you please explain what " (pen-topic t) " is in the context of " p " and why it is important?")
+                     (concat "Could you please explain what " (pen-topic t) " is and why it is important?")))
+         (final-question
+          (if (sor pretext)
+              (concat pretext " " question)
+            question)))
+
+    (let ((description (org-brain-asktutor final-question)))
+      (if (sor description)
+          (progn
+            (let ((cb (current-buffer)))
+              (my-org-brain-goto-current)
+              (let ((block-name (concat (org-brain-current-name) "-description")))
+                (if (not (org-babel-find-named-block block-name))
+                    (progn
+                      (insert
+                       (snc (cmd "org-template-gen" "brain-description" block-name) description))
+                      (call-interactively 'save-buffer)
+                      (call-interactively 'my/revert-kill-buffer-and-window))))
+              (with-current-buffer cb
+                (revert-buffer))))))))
+
+;; TODO Fix this
+(defun org-brain-asktutor (question)
+  (interactive (list (read-string-hist (concat "asktutor about " (pen-topic) ": "))))
+  (let ((topic (org-brain-current-topic)))
+
+    (setq topic
+          )
+
+    (let ((answer
+           (eval
+            `(ci (snc "ttp" (pen-pf-generic-tutor-for-any-topic
+                             ,topic
+                             ;; ,cname
+                             ;; ,pname
+                             ,question))))))
+      (if (interactive-p)
+          (etv answer)
+        answer))))
+
 ;; TODO
 ;; j:org-brain-asktutor
 
