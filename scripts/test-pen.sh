58de2b70cf86e898ab29ea01ac1b89103384c26e
Shane Mulligan <mullikine@gmail.com>
2021-07-10 15:45:49 +1200

    1625888749

--- scripts/test-pen.sh
+++ scripts/test-pen.sh
@@ -4,11 +4,13 @@ cd /root/.emacs.d/pen.el
 git clean -fd .
 git fetch -a
 git reset --hard origin/master
+git pull
 
-cd ../prompts
+cd /root/.emacs.d/prompts
 git clean -fd .
 git fetch -a
 git reset --hard origin/master;
+git pull
 
 cd
 ./run
