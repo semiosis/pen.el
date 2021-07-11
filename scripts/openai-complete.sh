4f6fa5b2bdfb1030ad118cec61c85940c821b64a
Shane Mulligan <mullikine@gmail.com>
2021-07-12 00:52:13 +1200

    1626007933

--- scripts/openai-complete.sh
+++ scripts/openai-complete.sh
@@ -92,24 +92,19 @@ PEN_PROMPT="$(p "$PEN_PROMPT")"
 # This is designed to not trim whitespace from the ends of the stop sequence
 IFS= read -rd '' PEN_STOP_SEQUENCE < <(p "$PEN_STOP_SEQUENCE");typeset -p PEN_STOP_SEQUENCE &>/dev/null
 
-{
-    env
 # Will it complain if PEN_STOP_SEQUENCE is empty?
-cmd openai api \
+openai api \
     completions.create \
     -e "$PEN_ENGINE" \
     -t "$PEN_TEMPERATURE" \
     -M "$PEN_MAX_TOKENS" \
     -n "$PEN_N_COMPLETIONS" \
     --stop "$PEN_STOP_SEQUENCE" \
-    -p "$PEN_PROMPT" 
-} > "$tf_response"
-
-tmux neww "vim $tf_response"
+    -p "$PEN_PROMPT" > "$tf_response"
 
 : "${PEN_END_POS:="$(cat "$tf_response" | wc -c)"}"
 
-export "$PEN_END_POS"
+export PEN_END_POS
 results_dir="$(openai_results_split "$tf_response")"
 
 # The API returns the entire prompt + completion
