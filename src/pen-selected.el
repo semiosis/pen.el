93569977654d59264e87e1ab86c8e2115610137c
Shane Mulligan <mullikine@gmail.com>
2021-07-13 19:15:34 +1200

    Tue 13 Jul 2021 19:15:34

--- src/pen-selected.el
+++ src/pen-selected.el
@@ -1,5 +1,12 @@
+(defun pen-turn-on-selected-minor-mode ()
+  (interactive)
+
+  (selected-minor-mode -1)
+  (selected-region-active-mode -1)
+  (selected-minor-mode 1)
+
 (define-globalized-minor-mode
-  global-selected-minor-mode selected-minor-mode turn-on-selected-minor-mode)
+  global-selected-minor-mode selected-minor-mode pen-turn-on-selected-minor-mode)
 (global-selected-minor-mode t)
 
 (provide 'pen-selected)
