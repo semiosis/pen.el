ac417051e909550c83e20c50bf0212f94dc9dddd
Shane Mulligan <mullikine@gmail.com>
2021-07-14 01:03:36 +1200

    14.07.21

---
+++ scripts/booste.py
@@ -0,0 +1,18 @@
+#!/bin/bash
+export TTY
+
+import booste
+
+string = 'This is a test for'
+api_key = "your api key"
+model_key = "gpt-j model key via dashbaord"
+
+model_parameters = {
+   "string": string,
+   "length": 30,
+   "temperature": 0.9,
+   "topP" : 0.9,
+   "stopToken" : '.'
+}
+
+print(booste.run(api_key, model_key, model_parameters))
