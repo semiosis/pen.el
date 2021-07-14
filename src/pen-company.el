23f47175354face62e69088df14338eb678246ef
Shane Mulligan <mullikine@gmail.com>
2021-07-14 21:17:08 +1200

    Wed 14 Jul 2021 21:17:08

--- src/pen-company.el
+++ src/pen-company.el
@@ -30,8 +30,6 @@
 
 (defset pen-company-selected-backends '(pen-company-filetype))
 
-;; TODO
-
 ;; TODO: Encode where the text came from into the emacs buffer using emacs text properties
 (defun pen-company-complete ()
   (interactive)
