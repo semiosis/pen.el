9dbfeee689f4aab5fde7c7c2b949a2d79c7e9543
Shane Mulligan <mullikine@gmail.com>
2021-07-14 17:24:27 +1200

    Wed 14 Jul 2021 17:24:27

--- src/pen-prompt-description.el
+++ src/pen-prompt-description.el
@@ -1,8 +1,15 @@
+(require 'pen-lm-completers)
+
 ;; http://github.com/semiosis/prompts
 
 (define-derived-mode prompt-description-mode yaml-mode "Prompt"
   "Prompt description mode")
 
 (add-to-list 'auto-mode-alist '("\\.prompt\\'" . prompt-description-mode))
+
+;; TODO Use pcase
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Destructuring-with-pcase-Patterns.html
+(defun pen-create-prompt (tuple)
+  (interactive (list (pen-select-completion-backend))))
 
 (provide 'pen-prompt-description)
