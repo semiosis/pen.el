277e937a8ed1595552a1c5e1e703ccf10f26b518
Shane Mulligan <mullikine@gmail.com>
2021-07-14 16:32:54 +1200

    Wed 14 Jul 2021 16:32:54

--- src/pen-lm-completers.el
+++ src/pen-lm-completers.el
@@ -2,7 +2,7 @@
 
 (defset pen-completion-backends
   ;; name completion command and a list of engines
-  `(("OpenAI GPT-3" "openai-complete.sh" ,(pen-openai-list-engines))
+  `(("OpenAI GPT-3" "openai-complete.sh" ,(pen-openai-list-engines) "available")
     ("EleutherAI GPT-J" "gpt-j-complete.sh" ("6B"))
     ("EleutherAI GPT-Neo" "gpt-neo-complete.sh" ("2.7B" "1.3B"))
     ("EleutherAI GPT-NeoX" "gpt-neox-complete.sh" ("175B"))
@@ -11,10 +11,15 @@
 (defun pen-select-completion-backend ()
   (interactive)
   (let* ((options
-          (mapcar 'pp-ol
-                  (flatten-once
-                   (loop for trip in pen-completion-backends collect
-                         (pen-cartesian-product (list (car trip)) (list (nth 1 trip)) (nth 2 trip))))))
+          (mapcar
+           'pp-ol
+           (flatten-once
+            (loop for trip in pen-completion-backends collect
+                  (pen-cartesian-product
+                   (list (car trip))
+                   (list (nth 1 trip))
+                   (nth 2 trip)
+                   (list (nth 3 trip)))))))
          (sel (fz
                options
                nil nil "pen completion backend: ")))
