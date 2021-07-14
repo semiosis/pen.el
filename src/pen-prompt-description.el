66e7510e1ce38156d2308b576120170a30c7f2b2
Shane Mulligan <mullikine@gmail.com>
2021-07-14 19:38:44 +1200

    Wed 14 Jul 2021 19:38:44

--- src/pen-prompt-description.el
+++ src/pen-prompt-description.el
@@ -9,6 +9,9 @@
 
 ;; TODO Use pcase
 ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Destructuring-with-pcase-Patterns.html
+;; What about these?
+;; yas--template-expand-env
+;; yas--snippet-expand-env
 (defun pen-create-prompt ()
   (pcase-let ((`(,title ,script ,engine-list ,available) (pen-select-completion-backend))
               ;; Suggest other parameters based on existing prompts in personal repository?
