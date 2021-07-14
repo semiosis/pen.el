80445857abaf3932e0f39ca784a91741c0c8aa6d
Shane Mulligan <mullikine@gmail.com>
2021-07-15 00:52:58 +1200

    Thu 15 Jul 2021 00:52:58

--- src/pen.el
+++ src/pen.el
@@ -161,7 +161,7 @@
                 ;; See `pen-restore-chars`
                 `(("PEN_PROMPT" ,(str
                                   (->> final-prompt
-                                    (string-replace "`" "<pen-notnewline>")
+                                    (string-replace "`" "<pen-backtick>")
                                     (string-replace "\\n" "<pen-notnewline>")
                                     (string-replace "$" "<pen-dollar>"))))
                   ("PEN_LM_COMMAND" ,,lm-command)
