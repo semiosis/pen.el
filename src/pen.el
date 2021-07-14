8ce7aa6c5797b1aae3cbb49330bf74e7258281f8
Shane Mulligan <mullikine@gmail.com>
2021-07-15 03:37:08 +1200

    Thu 15 Jul 2021 03:37:08

--- src/pen.el
+++ src/pen.el
@@ -123,7 +123,7 @@
                            (eval iarg)
                          initval))))
                 (progn
-                  (loop for v in ',var-syms (until (eq v '&key)) collect (eval v)))))
+                  (tv (pps (loop for v in ',var-syms (until (eq v '&key)) collect (eval v)))))))
 
              ;; preprocess the values of the parameters
              (vals
