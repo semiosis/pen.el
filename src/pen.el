c7af8d2d0f461145539235e4bf22e0eec87466ed
Shane Mulligan <mullikine@gmail.com>
2021-07-15 12:48:42 +1200

    Thu 15 Jul 2021 12:48:42

--- src/pen.el
+++ src/pen.el
@@ -161,7 +161,8 @@
              ;; This gives string position, not byte position
              ;; (string-search "s" "ガムツリshane")
              (prompt-end-pos (or (byte-string-search "<:pp>" ,prompt)
-                                 (length final-prompt)))
+                                 ;; (length final-prompt)
+                                 (string-bytes final-prompt)))
 
              (final-prompt (string-replace "<:pp>" "" final-prompt))
 
