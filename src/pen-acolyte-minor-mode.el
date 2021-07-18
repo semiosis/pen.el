6166ef67d9f87532b322d6721ce55990b28d2b95
Shane Mulligan <mullikine@gmail.com>
2021-07-18 20:44:02 +1200

    Sun 18 Jul 2021 20:44:02

--- src/pen-acolyte-minor-mode.el
+++ src/pen-acolyte-minor-mode.el
@@ -9,3 +9,14 @@
 
 ;;;###autoload
 (define-globalized-minor-mode global-pen-acolyte-minor-mode pen-acolyte-minor-mode pen-acolyte-minor-mode)
+
+(define-key pen-acolyte-minor-mode-map (kbd "M-p") 'pen-super-newb-dired-prompts)
+(define-key pen-acolyte-minor-mode-map (kbd "M-t") 'pen-super-newb-scratch)
+(define-key pen-acolyte-minor-mode-map (kbd "M-s") 'save-buffer)
+(define-key pen-acolyte-minor-mode-map (kbd "M-r") 'pen-run-prompt-function)
+(define-key pen-acolyte-minor-mode-map (kbd "M-TAB") 'pen-company-filetype)
+(define-key pen-acolyte-minor-mode-map (kbd "M-l") 'pen-complete-long)
+(define-key pen-acolyte-minor-mode-map (kbd "M-g") 'pen-generate-prompt-functions)
+(define-key pen-acolyte-minor-mode-map (kbd "M-c") 'fz-pen-counsel)
+(define-key pen-acolyte-minor-mode-map (kbd "M-m") 'right-click-context-menu)
+(define-key pen-acolyte-minor-mode-map (kbd "M-f") 'pen-filter-with-prompt-function)
