5e5df2e2ef3b9d2b695a8815df3d8eac174d8942
Shane Mulligan <mullikine@gmail.com>
2021-07-20 21:21:08 +1200

    Tue 20 Jul 2021 21:21:08

--- src/pen-yaml.el
+++ src/pen-yaml.el
@@ -1,6 +1,8 @@
 ;; pen-yq interfaces with yq to generate yaml
 ;; TODO Use native elisp
 
+;; (yaml2json (cat "/home/shane/source/git/semiosis/prompts/prompts/analogy.prompt"))
+
 (defun yaml2json (yaml-in)
   (snc "yq ." yaml-in))
 
