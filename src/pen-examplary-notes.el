e3c140c8e15552da9f5f9c752615bc9fb013b6af
Shane Mulligan <mullikine@gmail.com>
2021-07-20 16:31:04 +1200

    Tue 20 Jul 2021 16:31:04

--- src/pen-examplary-notes.el
+++ src/pen-examplary-notes.el
@@ -23,3 +23,23 @@
 
 (defmodel gpt3
   :n-examples 3)
+
+;; Perspective:
+;; Create the language according to speculation and future need.
+;; Combine with APIs and LMs according to what they need.
+
+;; TODO It's important to implement single example prompts
+;; I can then search for a multi-example prompt
+
+;; TODO examples
+;; TODO counterexamples
+;; TODO tasks
+
+;; The initial characters of the prompt have more weight than the latter.
+;; A 'metaprompt' may be designed which is the initial part of the prompt.
+;; It's not a great name as there is no strict part which could be considered.
+;; And a metaprompt may be further divided ad infinitum.
+
+;; (defmacro defprompt (inputtype outputtype &rest data)
+;;   ""
+;;   `(,@data))
