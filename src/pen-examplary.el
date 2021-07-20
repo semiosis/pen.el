61e7064fe734e9254d414b4b86d6a031e87d6c0c
Shane Mulligan <mullikine@gmail.com>
2021-07-21 01:47:06 +1200

    Wed 21 Jul 2021 01:47:06

--- src/pen-examplary.el
+++ src/pen-examplary.el
@@ -127,6 +127,7 @@ The last element in the list is the output/return value"
          (progn
            ;; Firstly, ensure that the prompt function un-onelinerises the output
            (plist-put data :examples examples)
+           (plist-put data :stop-sequences '("Input"))
            (concat
             task "\n\n"
             (list2str
