21e1fc60c2d641224060ff5126f3b93f55f2b3a8
Shane Mulligan <mullikine@gmail.com>
2021-07-20 14:48:16 +1200

    Tue 20 Jul 2021 14:48:16

--- src/pen-examplary.el
+++ src/pen-examplary.el
@@ -14,16 +14,9 @@
 ;; It's not a great name as there is no strict part which could be considered.
 ;; And a metaprompt may be further divided ad infinitum.
 
-(defmacro defprompt (&rest body)
+(defmacro defprompt (inputtype outputtype &rest data)
   ""
-  `(progn ,@body))
-
-(defmacro defexamples (task examples counterexamples)
-  :e "grex"
-  "example 1\nexample2" "^example [12]$"
-  "example 2\nexample3" "^example [23]$"
-  "pi4\npi5" "^pi[45]$")
-(defalias 'defex 'defexample)
+  `(,@data))
 
 ;; TODO Start with an input/output prompt
 
@@ -36,6 +29,15 @@
               ("example 2\nexample3" "^example [23]$")
               ("pi4\npi5" "^pi[45]$"))
   :counterexamples '())
+
+;; (etv (plist-get (expand-macro '(defprompt lines regex
+;;                                  :external "grex"
+;;                                  ;; The task may also be used as a metaprompt.
+;;                                  :task "Convert lines to regex"
+;;                                  :examples '(("example 1\nexample2" "^example [12]$")
+;;                                              ("example 2\nexample3" "^example [23]$")
+;;                                              ("pi4\npi5" "^pi[45]$"))
+;;                                  :counterexamples '())) :external))
 
 ;; Only use as many examples as required by the model
 
