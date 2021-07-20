f5e76fdd4880ab175c7f854dc5d80526516d5c5c
Shane Mulligan <mullikine@gmail.com>
2021-07-20 21:18:53 +1200

    Tue 20 Jul 2021 21:18:53

--- src/pen-yq.el
+++ src/pen-yq.el
@@ -1,6 +1,9 @@
 ;; pen-yq interfaces with yq to generate yaml
 ;; TODO Use native elisp
 
+(defun yaml2json (yaml-in)
+  (snc "yq ." yaml-in))
+
 (defun json2yaml (json-in)
   (snc "json2yaml" json-in))
 
