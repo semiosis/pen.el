31da91c2ef4194128e001430e1d3f110a4a5cb5c
Shane Mulligan <mullikine@gmail.com>
2021-07-21 15:57:04 +1200

    Wed 21 Jul 2021 15:57:04

--- scripts/pen.ps1
+++ scripts/pen.ps1
@@ -2,6 +2,4 @@
 
 # resolvepath of directory
 
-function docker-run {
-docker.exe
-}
+docker run -v /home/shane/.pen:/root/.pen -v /home/ shane/var/smulliga/source/git/semiosis/prompts :/root/.emacs.d/host/prompts -v pen.el:/root/.emacs.d/host/pen.el -ti --entrypoint= semiosis/pen.el:latest ./run.sh 'docker' 'run' '-v' '/home/shane/.pen:/root/.pen' '-v' '/home/shan e/var/smulliga/source/git/semiosis/prompts:/ro ot/.emacs.d/host/prompts' '-v' 'pen.el:/root/.emacs.d/host/pen.el' '-ti' '-- entrypoint=' 'semiosis/pen.el:latest' './run.sh'
