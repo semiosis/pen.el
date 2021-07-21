a10e0ba0a5559d21f91e9c96d70bb2afce9c377a
Shane Mulligan <mullikine@gmail.com>
2021-07-22 11:48:03 +1200

    Thu 22 Jul 2021 11:48:03

--- src/pen-yaml.el
+++ src/pen-yaml.el
@@ -1,4 +1,5 @@
-;; the elisp library is slower and less reliable than the module
+;; The elisp library is slower and less reliable than the module,
+;; so don't use it at the moment.
 (require 'yaml)
 
 ;; pen-yq interfaces with yq to generate yaml
