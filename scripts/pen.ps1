243bdec6b900374066c640cbff0724c0cca3cd18
Shane Mulligan <mullikine@gmail.com>
2021-07-21 17:49:13 +1200

    Wed 21 Jul 2021 17:49:13

--- scripts/pen.ps1
+++ scripts/pen.ps1
@@ -1,10 +1,11 @@
-$pen_config_dir = "$HOME\.pen"
+$pen_config_dir = "$HOME" | Join-Path -ChildPath '.pen'
+Write-Host $pen_config_dir
 If(!(test-path $pen_config_dir))
 {
     New-Item -ItemType Directory -Force -Path $pen_config_dir
 }
 
-$openai_api_key_file = "$pen_config_dir\openai_api_key"
+$openai_api_key_file = "$pen_config_dir" | Join-Path -ChildPath 'openai_api_key'
 If(!(test-path $openai_api_key_file))
 {
     $openai_api_key = Read-Host -Prompt 'Input OpenAI API key'
@@ -12,14 +13,16 @@ If(!(test-path $openai_api_key_file))
     $openai_api_key | Out-File $openai_api_key_file
 }
 
-$here = $PSScriptRoot
-$prompts_dir = "$here\prompts"
+# $penelloc = "$PSScriptRoot"
+$here = $pwd
+$prompts_dir = "$here" | Join-Path -ChildPath 'prompts'
+Write-Host $prompts_dir
 If(!(test-path $prompts_dir))
 {
     git clone "https://github.com/semiosis/prompts"
 }
 
-docker.exe run `
+docker run `
   -v "${pen_config_dir}:/root/.pen" `
   -v "${prompts_dir}:/root/.emacs.d/host/prompts" `
   -ti --entrypoint= semiosis/pen.el:latest ./run.sh
