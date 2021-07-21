bb9571096d56203e004842dc1c2f4bf956489fb3
Shane Mulligan <mullikine@gmail.com>
2021-07-21 20:45:14 +1200

    Wed 21 Jul 2021 20:45:14

--- src/pen-diagnostics.el
+++ src/pen-diagnostics.el
@@ -4,7 +4,7 @@
                  "keytest: "
                  (pen-onelineify
                   (snc
-                   "OPENAI_API_KEY=\"$(cat ~/.pen/openai_api_key)\" pen-openai api completions.create -e davinci -t 0.8 -M 60 -n 1 --stop '###' -p \"Hello\"")))))
+                   "OPENAI_API_KEY=\"$(cat ~/.pen/openai_api_key)\" pen-openai api completions.create -e davinci -t 0.8 -M 10 -n 1 --stop '###' -p \"Hello\"")))))
     (if (interactive-p)
         (etv output)
       output)))
