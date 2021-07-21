fd2cc2cd448871c442785d42e53cc72cb93920d4
Shane Mulligan <mullikine@gmail.com>
2021-07-21 19:19:10 +1200

    Wed 21 Jul 2021 19:19:10

---
+++ src/pen-diagnostics.el
@@ -0,0 +1,3 @@
+(defun pen-diagnostics-test-key ()
+  (interactive)
+  (etv (snc "OPENAI_API_KEY=\"$(cat ~/.pen/openai_api_key)\" pen-openai api completions.create -e davinci -t 0.8 -M 60 -n 1 --stop '###' -p \"Hello\"")))
