43156448965e491b68713370a3996bb00a016939
Shane Mulligan <mullikine@gmail.com>
2021-07-21 17:38:46 +1200

    Wed 21 Jul 2021 17:38:46

--- scripts/pen.ps1
+++ scripts/pen.ps1
@@ -1,62 +1,20 @@
-# run docker with volume
-
-# resolvepath of directory
-
-docker run -v $HOME/.pen:/root/.pen -v $MYGIT/semiosis/prompts :/root/.emacs.d/host/prompts -v pen.el:/root/.emacs.d/host/pen.el -ti --entrypoint= semiosis/pen.el:latest ./run.sh 'docker' 'run' '-v' '$HOME/.pen:/root/.pen' '-v' '$HOME e$MYGIT/semiosis/prompts:/ro ot/.emacs.d/host/prompts' '-v' 'pen.el:/root/.emacs.d/host/pen.el' '-ti' '-- entrypoint=' 'semiosis/pen.el:latest' './run.sh'
-
-test -d "prompts" && : "${PROMPTS_DIR:="prompts"}"
-
-if (Test-Path -Path $Folder) {
-    "Path exists!"
-} else {
-    "Path doesn't exist."
+$pen_config_dir = "$HOME\.pen"
+If(!(test-path $pen_config_dir))
+{
+    New-Item -ItemType Directory -Force -Path $pen_config_dir
 }
 
-# test -d "$MYGIT/semiosis/pen.el" && : "${PENEL_DIR:="$MYGIT/semiosis/pen.el"}"
-test -d "pen.el" && : "${PENEL_DIR:="pen.el"}"
-# : "${PENEL_DIR:="$(read -ep "PENEL_DIR (leave empty to use docker): ")"}"
+$openai_api_key_file = "$pen_config_dir\openai_api_key"
+If(!(test-path $openai_api_key_file))
+{
+    $openai_api_key = Read-Host -Prompt 'Input OpenAI API key'
+    New-Item -ItemType Directory -Force -Path $pen_config_dir
+    $openai_api_key | Out-File $openai_api_key_file
+}
 
-test -d "$HOME/.pen" && : "${PEN_CONFIG_DIR:="$HOME/.pen"}"
-: "${PEN_CONFIG_DIR:="$(read -ep "PEN_CONFIG_DIR (leave empty to use docker): ")"}"
-
-# yn "Pull docker image?" && docker pull semiosis/pen.el:latest
-
-set -v
-docker pull semiosis/pen.el:latest
-
-if test -d "$PROMPTS_DIR"; then
-    yn "Pull prompts repo?" && (
-        cd "$PROMPTS_DIR"
-        git pull origin master
-    )
-else
-    yn "Clone prompts repo here?" && (
-        git clone "http://github.com/semiosis/prompts"
-    )
-fi
-
-test -d "prompts" && : "${PROMPTS_DIR:="prompts"}"
-
-test -d "$PENEL_DIR" && yn "Pull pen.el repo?" && (
-    cd "$PENEL_DIR"
-    git pull origin master
-)
-
-if test -d "$PROMPTS_DIR"; then
-    PROMPTS_DIR="$(realpath "$PROMPTS_DIR")"
-fi
-
-if test -d "$PEN_CONFIG_DIR"; then
-    PEN_CONFIG_DIR="$(realpath "$PEN_CONFIG_DIR")"
-fi
-
-IFS= read -r -d '' shcode <<HEREDOC
-    cmd docker run \
-        $(test -n "$OPENAI_API_KEY" && printf -- "%s " -e "OPENAI_API_KEY:$OPENAI_API_KEY" ) \
-        $(test -n "$PEN_CONFIG_DIR" && printf -- "%s " -v "$PEN_CONFIG_DIR:/root/.pen" ) \
-        $(test -n "$PROMPTS_DIR" && printf -- "%s " -v "$PROMPTS_DIR:/root/.emacs.d/host/prompts" ) \
-        $(test -n "$PENEL_DIR" && printf -- "%s " -v "$PENEL_DIR:/root/.emacs.d/host/pen.el" ) \
-        -ti --entrypoint= semiosis/pen.el:latest ./run.sh
-HEREDOC
-
-eval "$shcode"
+$here = $PSScriptRoot
+docker.exe run `
+  -v "$HOME/.pen:/root/.pen" `
+  --directory "$project_root\lisp" `
+  --directory "$project_root\langs" `
+  --eval "(progn (require 'tree-sitter-langs) (tree-sitter-langs-ensure '$lang))"
