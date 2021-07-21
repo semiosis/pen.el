df48fc92836ecd4cf6231191952e18c9ca729c6b
Shane Mulligan <mullikine@gmail.com>
2021-07-21 19:37:27 +1200

    Wed 21 Jul 2021 19:37:27

--- src/pen-diagnostics.el
+++ src/pen-diagnostics.el
@@ -1,6 +1,10 @@
 (defun pen-diagnostics-test-key ()
   (interactive)
-  (let ((output (concat "keytest: " (pen-onelineify (snc "OPENAI_API_KEY=\"$(cat ~/.pen/openai_api_key)\" pen-openai api completions.create -e davinci -t 0.8 -M 60 -n 1 --stop '###' -p \"Hello\"")))))
+  (let ((output (concat
+                 "keytest: "
+                 (pen-onelineify
+                  (snc
+                   "OPENAI_API_KEY=\"$(cat ~/.pen/openai_api_key)\" pen-openai api completions.create -e davinci -t 0.8 -M 60 -n 1 --stop '###' -p \"Hello\"")))))
     (if (interactive-p)
         (etv output)
       output)))
