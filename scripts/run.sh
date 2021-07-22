bf44adbf1c48488bb988610b76e6682c34d5896a
Shane Mulligan <mullikine@gmail.com>
2021-07-22 17:08:02 +1200

    Thu 22 Jul 2021 17:08:02

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,6 +8,6 @@ export LANG=en_US
 
 export EMACSD=/root/.emacs.d
 export YAMLMOD_PATH=$EMACSD/emacs-yamlmod
-export PATH=$PATH:$EMACSD/pen.el/scripts
+export PATH=$PATH:$EMACSD/host/pen.el/scripts:$EMACSD/pen.el/scripts
 
 emacs -nw --debug-init
