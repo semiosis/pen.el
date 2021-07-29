71968595da9c6baca7cf44fd462c817b49ce443b
Shane Mulligan <mullikine@gmail.com>
2021-07-29 16:08:36 +1200

    1627531716

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -10,8 +10,5 @@ export EMACSD=/root/.emacs.d
 export YAMLMOD_PATH=$EMACSD/emacs-yamlmod
 export PATH=$PATH:$EMACSD/host/pen.el/scripts:$EMACSD/pen.el/scripts
 
-CMD="$(cmd "$@")"
-: ${CMD:="$(cmd "$@")"}
-
-# emacs -nw --debug-init
-emacsclient -a "" -e "$CMD"
+# Can't use cmd because elisp doesn't use single quote for strings
+emacsclient -a "" -e "$@"
