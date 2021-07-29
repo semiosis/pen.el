8a92ca323f9133d6eb2c619047fa35eeebb8b7ab
Shane Mulligan <mullikine@gmail.com>
2021-07-29 22:50:17 +1200

    Thu 29 Jul 2021 22:50:17

--- src/pen.el
+++ src/pen.el
@@ -533,7 +533,7 @@ Function names are prefixed with pf- for easy searching"
 (defmacro pen-long-complete (&rest body)
   "This wraps around a pen function calls to make them complete long"
   `(let ((max-tokens 200)
-         (stop-sequencemfs "##long complete##")
+         (stop-sequence "##long complete##")
          (stop-sequences '("##long complete##")))
      ,@body))
 
