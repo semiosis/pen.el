22984c15ed7caa5502821e464e0e8aedc4d6767f
Shane Mulligan <mullikine@gmail.com>
2021-07-30 02:15:43 +1200

    Fri 30 Jul 2021 02:15:43

--- src/pen-test.el
+++ src/pen-test.el
@@ -4,6 +4,7 @@
          5)))
 ;; It works in code
 (defun pen-test-closure2 ()
+  (interactive)
   (let ((n-collate 1)) (pen-test-closure)))
 ;; But not in the M-: minibuffer
 (comment
@@ -11,3 +12,14 @@
 ;; But if I eval, then it DOES work in the minibuffer
 (comment
  (eval `(let ((n-collate 1)) (pen-test-closure))))
+
+(comment
+ ;; This still doesn't work. Actually, it does, partially
+ (eval `(let ((n-collate 1)
+              ;; (n-completions 1)
+              )
+          (pf-asktutor "emacs" "key bindings" "How do I kill a buffer?" :no-select-result t)))
+
+ ;; When n-completions is set, it breaks
+ (eval `(let ((n-completions 1))
+          (pf-asktutor "emacs" "key bindings" "How do I kill a buffer?" :no-select-result t))))
