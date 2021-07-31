789023dde949d032ef1f5ef48af878fe85eba6db
Shane Mulligan <mullikine@gmail.com>
2021-08-01 05:40:47 +1200

    Sun 01 Aug 2021 05:40:47

--- src/pen-right-click-menu.el
+++ src/pen-right-click-menu.el
@@ -123,14 +123,17 @@ If `INITIAL-INDEX' is non-nil, this is an initial index value for
   (or (not (selected-p))
       (= 1 (length (s-split " " (selection))))))
 
+(defun identity-command (&optional body)
+  (interactive)
+  (identity body))
+
 (setq right-click-context-global-menu-tree
-      `(("Cancel" :call identity)
+      `(("Cancel" :call identity-command)
         ("pen: translate" :call pf-translate-from-world-language-x-to-y)
         ("pen: transpile" :call pf-transpile-from-programming-language-x-to-y)
         ("pen (prose)"
          ("pick up line" :call pen-tutor-mode-assist :if (derived-mode-p 'prog-mode))
-         ("translate" :call pf-translate-from-world-language-x-to-y))
-        ("pen (prose, selected)" :if (selected-p)
+         ("translate" :call pf-translate-from-world-language-x-to-y)
          ("tldr" :call pf-tldr-summarization :if (selected-p))
          ("eli5" :call pf-eli5-explain-like-i-m-five :if (selected-p))
          ("correct grammar" :call pf-correct-grammar :if (selected-p))
@@ -141,7 +144,7 @@ If `INITIAL-INDEX' is non-nil, this is an initial index value for
          ("asktutor" :call pen-tutor-mode-assist :if (derived-mode-p 'prog-mode))
          ("transpile" :call pf-transpile-from-programming-language-x-to-y)
          ("add comments" :call pf-annotate-code-with-commentary))
-        ("word/term" :if (pen-word-clickable)
+        ("word/term"
          ("pick up line" :call pf-very-witty-pick-up-lines-for-a-topic :if (pen-word-clickable))
          ("define word" :call pf-define-word-for-glossary :if (pen-word-clickable)))))
 
