d8dc2817a6804ad2b4e3d0442dfffe59470041f5
Shane Mulligan <mullikine@gmail.com>
2021-08-02 01:13:50 +1200

    Mon 02 Aug 2021 01:13:50

--- src/pen-right-click-menu.el
+++ src/pen-right-click-menu.el
@@ -122,8 +122,8 @@ If `INITIAL-INDEX' is non-nil, this is an initial index value for
         ("transpile" :call pf-transpile-from-programming-language-x-to-y)
         ("prose"
          ("pick up line" :call pf-very-witty-pick-up-lines-for-a-topic)
-         ("translate" :call pf-translate-from-world-language-x-to-y)
-         ("paraphrase" :call pf-paraphrase)
+         ("translate" :call pf-translate-from-world-language-x-to-y :if (selected-p))
+         ("paraphrase" :call pf-paraphrase :if (selected-p))
          ("tldr" :call pf-tldr-summarization :if (selected-p))
          ("eli5" :call pf-eli5-explain-like-i-m-five :if (selected-p))
          ("correct grammar" :call pf-correct-grammar :if (selected-p))
