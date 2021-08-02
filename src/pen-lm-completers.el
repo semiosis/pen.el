40753ad7b32e4944a779fbd76ed5783f6e33253d
Shane Mulligan <mullikine@gmail.com>
2021-08-03 11:18:35 +1200

    Tue 03 Aug 2021 11:18:35

--- src/pen-lm-completers.el
+++ src/pen-lm-completers.el
@@ -3,6 +3,7 @@
 (defset pen-completion-backends
   ;; name completion command and a list of engines
   `(("OpenAI GPT-3" "openai-complete.sh" ,(pen-openai-list-engines) "available")
+    ("EleutherAI GPT-J API from AIX" "aix-complete.sh" ("6B"))
     ("EleutherAI GPT-J" "gpt-j-complete.sh" ("6B"))
     ("EleutherAI GPT-Neo" "gpt-neo-complete.sh" ("2.7B" "1.3B"))
     ("EleutherAI GPT-NeoX" "gpt-neox-complete.sh" ("175B"))
