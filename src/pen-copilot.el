8fa10634208f489ae06b0dbdba337bab489156a6
Shane Mulligan <mullikine@gmail.com>
2021-08-03 11:16:56 +1200

    Tue 03 Aug 2021 11:16:56

--- src/pen-copilot.el
+++ src/pen-copilot.el
@@ -1,6 +1,8 @@
 ;; Copilot support
 
-(require 'openai-api)
+;; I could use an API or re-implement the same stuff using Pen + codex.
+;; I think I will go with the latter option.
 
+(require 'openai-api)
 
 (provide 'pen-copilot)
