0bb4b73be813a7e0333eaaba5f2fdf416eeda9bc
Shane Mulligan <mullikine@gmail.com>
2021-08-03 16:24:43 +1200

    Tue 03 Aug 2021 16:24:43

--- src/pen-configure.el
+++ src/pen-configure.el
@@ -2,27 +2,36 @@
 
 (defvar penconfdir (f-join user-home-directory ".pen"))
 
-(defun pen-add-key-booste ()
-  (interactive)
-  (let ((pen-booste-key-file-path (f-join user-home-directory ".pen" "booste_api_key")))
+(defun pen-read-service-key (service-name)
+  (interactive (list (read-string "service: ")))
+  (let* ((key-path (f-join user-home-directory ".pen" (format "%s_api_key" service-name)))
+         (maybe-key (if (f-file-p key-path)
+                        (f-read key-path))))
+    (if maybe-key
+        (read-string (format "update %s key: " service-name) maybe-key)
+      (read-string (format "%s key: " service-name)))))
+
+(defun pen-add-key (service-name key)
+  (interactive (let* ((service-name (read-string "service: "))
+                      (key (pen-read-service-key service-name)))
+                 (list service-name key)))
+  (let ((key-path (f-join user-home-directory ".pen" (format "%s_api_key" service-name))))
     (if (not (f-dir-p penconfdir))
         (f-mkdir penconfdir))
-    (if (not (f-file-p pen-booste-key-file-path))
-        (let ((key (read-passwd "booste key: ")))
-          (if (sor key)
-              (progn
-                (f-touch pen-booste-key-file-path)
-                (f-write-text key 'utf-8 pen-booste-key-file-path)))))))
+
+    (f-touch key-path)
+    (f-write-text key 'utf-8 key-path)))
+
+(defun pen-add-key-booste (key)
+  (interactive (list (pen-read-service-key "booste")))
+  (pen-add-key "booste" key))
 
 (defun pen-add-key-openai (key)
-  (interactive (list (read-passwd "OpenAI key: ")))
-  (let ((pen-openai-key-file-path (f-join user-home-directory ".pen" "openai_api_key")))
-    (if (not (f-dir-p penconfdir))
-        (f-mkdir penconfdir))
-    (if (not (f-file-p pen-openai-key-file-path))
-        (if (sor key)
-            (progn
-              (f-touch pen-openai-key-file-path)
-              (f-write-text key 'utf-8 pen-openai-key-file-path))))))
+  (interactive (list (pen-read-service-key "openai")))
+  (pen-add-key "openai" key))
+
+(defun pen-add-key-aix (key)
+  (interactive (list (pen-read-service-key "aix")))
+  (pen-add-key "aix" key))
 
 (provide 'pen-configure)
