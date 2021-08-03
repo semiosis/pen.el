55c857538fb288df37b8d7b281ee41fd56206c62
Shane Mulligan <mullikine@gmail.com>
2021-08-03 14:10:05 +1200

    1627956605

---
+++ scripts/pen-aix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from aixapi import AIxResource
+import os
+
+# Get your API Key at apps.aixsolutionsgroup.com
+
+# model=os.environ.get("PEN_ENGINE"),
+
+if __name__ == "__main__":
+    api_key = os.environ.get("AIX_API_KEY")
+    aix_resource = AIxResource(api_key)
+    print(
+        str(
+            aix_resource.compose(
+                os.environ.get("PEN_PROMPT"),
+                response_length=int(os.environ.get("PEN_MAX_TOKENS")),
+                top_p=float(os.environ.get("PEN_TOP_P")),
+                temperature=float(os.environ.get("PEN_TEMPERATURE")),
+                stop_sequence=os.environ.get("PEN_STOP_SEQUENCE"),
+            )
+            .get("data", dict())
+            .get("text")
+        )
+    )
