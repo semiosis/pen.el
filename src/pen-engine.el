3cfd5cc42e48ea7c8ccc836716601e6cd4e692f7
Shane Mulligan <mullikine@gmail.com>
2021-08-03 16:33:05 +1200

    Tue 03 Aug 2021 16:33:05

--- src/pen-engine.el
+++ src/pen-engine.el
@@ -1,4 +1,6 @@
 ;; pen-engine allows you to configure the engines you want
 ;; For example, you may only want to use free(libre) engines, or private(local) engines, or powerful(openai) engines.
 
+;; It should also provide redundancy
+
 (provide 'pen-engine)
