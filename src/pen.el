f21210ab7ea1e26fe49d7c6b36d8cc61b82cf77d
Shane Mulligan <mullikine@gmail.com>
2021-08-04 21:09:23 +1200

    Wed 04 Aug 2021 21:09:23

--- src/pen.el
+++ src/pen.el
@@ -103,27 +103,6 @@
              if (>= (car tp) start-pos)
              return (car tp))))
 
-(defun string-replace (fromstring tostring instring)
-  "Replace FROMSTRING with TOSTRING in INSTRING each time it occurs."
-  (declare (pure t))
-  (when (equal fromstring "")
-    (signal 'wrong-length-argument fromstring))
-  (let ((start 0)
-        (result nil)
-        pos)
-    (while (setq pos (string-search fromstring instring start))
-      (unless (= start pos)
-        (push (substring instring start pos) result))
-      (push tostring result)
-      (setq start (+ pos (length fromstring))))
-    (if (null result)
-        ;; No replacements were done, so just return the original string.
-        instring
-      ;; Get any remaining bit.
-      (unless (= start (length instring))
-        (push (substring instring start) result))
-      (apply #'concat (nreverse result)))))
-
 (defun byte-string-search (needle haystack)
   "get byte position or needing in haystack"
   (let ((b (new-buffer-from-string haystack))
