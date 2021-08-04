2350d1846e6bb790cb3651803d94df7da325edfb
Shane Mulligan <mullikine@gmail.com>
2021-08-04 20:29:13 +1200

    Wed 04 Aug 2021 20:29:13

--- src/pen-emacs27.el
+++ src/pen-emacs27.el
@@ -1,5 +1,14 @@
 ;; emacs/share/emacs/28.0.50/lisp/subr.el.gz
 
+;; This is necessary because the string-search
+;; command is not available in emacs27
+(defun pen-string-search (needle haystack &optional start-pos)
+  (setq start-pos (or start-pos 0))
+  (let ((results (s-matched-positions-all needle haystack)))
+    (cl-loop for tp in results
+             if (>= (car tp) start-pos)
+             return (car tp))))
+
 ;; This is probably from emacs 28
 (defun string-replace (fromstring tostring instring)
   "Replace FROMSTRING with TOSTRING in INSTRING each time it occurs."
@@ -9,7 +18,7 @@
   (let ((start 0)
         (result nil)
         pos)
-    (while (setq pos (string-search fromstring instring start))
+    (while (setq pos (pen-string-search fromstring instring start))
       (unless (= start pos)
         (push (substring instring start pos) result))
       (push tostring result)
@@ -24,27 +33,3 @@
 
 ;; Also, mouse configs dont work with emacs 27
 ;; http://github.com/semiosis/pen.el/blob/master/src/pen-mouse.el
-
-(defun string-search (needle haystack &optional start-pos)
-  (setq start-pos (or start-pos 0))
-  (let ((results (s-matched-positions-all needle haystack)))
-    (cl-loop for tp in results
-             if (>= (car tp) start-pos)
-             return (car tp))))
-
-(defun string-search-beta (needle haystack &optional start-pos)
-  (setq start-pos (or start-pos 0))
-  (let ((results (s-matched-positions-all needle haystack)))
-    (cl-loop for tp in results
-             if (>= (car tp) start-pos)
-             return (car tp))))
-
-(defun string-search-test ()
-  (interactive)
-  ;; (etv (string-search-beta "shane" "is shane cool"))
-  (etv (string-search-beta "shane" "is shane cool" 3)))
-
-(defun string-search-test ()
-  (interactive)
-  ;; (etv (string-search-beta "shane" "is shane cool"))
-  (etv (string-search "shane" "is shane cool" 3)))
