2d06c66da36f70eb99b44cccffd0ec77ddd24212
Shane Mulligan <mullikine@gmail.com>
2021-08-04 19:25:37 +1200

    Wed 04 Aug 2021 19:25:37

---
+++ src/pen-emacs27.el
@@ -0,0 +1,22 @@
+;; emacs/share/emacs/28.0.50/lisp/subr.el.gz
+
+(defun string-replace (fromstring tostring instring)
+  "Replace FROMSTRING with TOSTRING in INSTRING each time it occurs."
+  (declare (pure t))
+  (when (equal fromstring "")
+    (signal 'wrong-length-argument fromstring))
+  (let ((start 0)
+        (result nil)
+        pos)
+    (while (setq pos (string-search fromstring instring start))
+      (unless (= start pos)
+        (push (substring instring start pos) result))
+      (push tostring result)
+      (setq start (+ pos (length fromstring))))
+    (if (null result)
+        ;; No replacements were done, so just return the original string.
+        instring
+      ;; Get any remaining bit.
+      (unless (= start (length instring))
+        (push (substring instring start) result))
+      (apply #'concat (nreverse result)))))
