e94015721ff43450820c8ecea7135224afdb61eb
Shane Mulligan <mullikine@gmail.com>
2021-08-04 15:51:53 +1200

    Wed 04 Aug 2021 15:51:53

--- src/pen-diagnostics.el
+++ src/pen-diagnostics.el
@@ -1,10 +1,24 @@
 (defun pen-diagnostics-test-key ()
   (interactive)
-  (let ((output (concat
-                 "keytest: "
-                 (pen-onelineify
-                  (pen-snc
-                   "OPENAI_API_KEY=\"$(cat ~/.pen/openai_api_key)\" pen-openai api completions.create -e davinci -t 0.8 -M 10 -n 1 --stop '###' -p \"Hello\"")))))
+  (let* ((aix-key (pen-snc "cat ~/.pen/aix_api_key"))
+         (aix-cmd
+          (tv (concat
+               (sh-construct-envs `(("AIX_API_KEY" ,aix-key)
+                                    ("PEN_PROMPT" "Once upon a time")
+                                    ("PEN_MAX_TOKENS" "60")
+                                    ("PEN_ENGINE" "GPT-J-6B")
+                                    ("PEN_TOP_P" "1.0")
+                                    ("PEN_STOP_SEQUENCE" "###")))
+               " "
+               "pen-aix")))
+         (output (concat
+                  "openai keytest: "
+                  (pen-onelineify
+                   (pen-snc
+                    "OPENAI_API_KEY=\"$(cat ~/.pen/openai_api_key)\" pen-openai api completions.create -e davinci -t 0.8 -M 10 -n 1 --stop '###' -p \"Hello\""))
+                  "\n"
+                  "aix keytest: "
+                  (pen-onelineify (pen-snc aix-cmd)))))
     (if (interactive-p)
         (etv output)
       output)))
