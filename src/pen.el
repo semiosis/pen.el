461db3c77e78a3c7e4cf59c141adc71ca324b1fd
Shane Mulligan <mullikine@gmail.com>
2021-08-04 16:35:50 +1200

    Wed 04 Aug 2021 16:35:50

--- src/pen.el
+++ src/pen.el
@@ -184,8 +184,8 @@
              ;; Keep in mind this both updates memoization and the bash cache
              (do-pen-update (pen-var-value-maybe 'do-pen-update))
 
-             (sh-update (or (pen-var-value-maybe 'sh-update)
-                            do-pen-update))
+             (pen-sh-update (or (pen-var-value-maybe 'pen-sh-update)
+                                do-pen-update))
 
              (cache
               (and (not do-pen-update)
