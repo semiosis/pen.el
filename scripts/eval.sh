971ef74d35bc60cb124058b4b1093daaaace4cdf
Shane Mulligan <mullikine@gmail.com>
2021-08-05 16:06:50 +1200

    1628136410

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -14,6 +14,7 @@ export EMACSD=/root/.emacs.d
 export YAMLMOD_PATH=$EMACSD/emacs-yamlmod
 export PATH=$PATH:$EMACSD/host/pen.el/scripts:$EMACSD/pen.el/scripts
 
+rm -f /tmp/eval-output.txt
 # Can't use cmd because elisp doesn't use single quote for strings
 unbuffer emacsclient -a "" -e "(pen-eval-for-host $1)" &>/dev/null
 sleep 0.1
