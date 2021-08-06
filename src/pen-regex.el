adec0f737f3e3b3e2260fe885d64d0cf45bbf848
Shane Mulligan <mullikine@gmail.com>
2021-08-07 00:56:51 +1200

    Sat 07 Aug 2021 00:56:51

--- src/pen-regex.el
+++ src/pen-regex.el
@@ -1,31 +1,37 @@
+;; TODO Take all from here
+;; $EMACSD/config/my-regex.el
+
 (defalias 'pen-unregexify 'regexp-quote)
 
-(defun ire-in-region-or-buffer-p (r)
-  (if (use-region-p)
-      (ire-in-region-p r)
-    (ire-in-buffer-p r)))
+(defun str-in-buffer-or-path-p (s)
+  (let ((p (get-path-nocreate))
+        (bs (buffer-string)))
+    (if (and (stringp s)
+             (stringp bs))
+        (or (string-match-literal s bs)
+            (string-match-literal s p)))))
 
-(defun str-in-region-or-buffer-p (s)
+(defun pen-str-p (s)
   (if (use-region-p)
       (str-in-region-p s)
     (str-in-buffer-p s)))
 
-(defun re-in-region-or-buffer-or-path-p (r)
+(defun pen-re-p (r)
   (if (use-region-p)
       (re-in-region-or-path-p r)
     (re-in-buffer-or-path-p r)))
 
-(defun istr-in-region-or-buffer-or-path-p (s)
+(defun pen-istr-p (s)
   (if (use-region-p)
       (istr-in-region-or-path-p s)
     (istr-in-buffer-or-path-p s)))
 
-(defun istr-in-region-or-buffer-p (s)
+(defun pen-istr-p (s)
   (if (use-region-p)
       (istr-in-region-p s)
     (istr-in-buffer-p s)))
 
-(defun ire-in-region-or-buffer-or-path-p (r)
+(defun pen-ire-p (r)
   (if (use-region-p)
       (ire-in-region-or-path-p r)
     (ire-in-buffer-or-path-p r)))
