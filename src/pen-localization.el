52ae6adcba1abdeab99284433cc2cc3c0932f152
Shane Mulligan <mullikine@gmail.com>
2021-08-07 23:04:43 +1200

    Sat 07 Aug 2021 23:04:43

--- src/pen-localization.el
+++ src/pen-localization.el
@@ -1,5 +1,6 @@
 ;; It's important to be able to understand all the user prompts in Pen.el
 
 ;; All strings should be associated with a base language model
+;; This way their meanings are preserved for the future
 
 (provide 'pen-localization)
