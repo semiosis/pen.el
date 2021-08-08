79e68004436d5f902044ee06bfec4c4465417b78
Shane Mulligan <mullikine@gmail.com>
2021-08-08 20:57:42 +1200

    Sun 08 Aug 2021 20:57:42

--- src/pen-fun.el
+++ src/pen-fun.el
@@ -1,3 +1,5 @@
+;; Language games
+
 (defun pen-oob (s)
   "Replace all vowels with 'oob'"
 
@@ -9,5 +11,15 @@
 (defun pen-oob-test ()
   (interactive)
   (etv (pen-oob "To be or not to be, that is the question.")))
+
+(defun pen-piglatin-word (w)
+  (cond
+   ((string-match "^[aeiou]" w) (concat w "yay"))
+   ((string-match "^[^aeiou]" w) (concat w "yay"))))
+
+(defun pen-piglatin (s)
+  "If a word begins with a vowel, just add \"yay\" to the end. For example, \"out\" is translated into \"outyay\".
+If it begins with a consonant, then we take all consonants before the first vowel and we put them on the end of the word.
+For example, \"which\" is translated into \"ichwhay\".")
 
 (provide 'pen-fun)
