d1d79334f4b4a537e88f18171d011ee07e24baab
Shane Mulligan <mullikine@gmail.com>
2021-08-08 13:17:27 +1200

    Sun 08 Aug 2021 13:17:27

--- src/pen-fun.el
+++ src/pen-fun.el
@@ -2,6 +2,8 @@
   "Replace all vowels with 'oob'"
 
   ;; TODO Improve this by taking into account 'y'
+  ;; Phoenetic vowels are better
+  ;; Perhaps use an IPA translator
   (replace-regexp-in-string "[aeiou]" "oob" s))
 
 (provide 'pen-fun)
