4ce5137a29e8b929a269a94d2427b7230a0648d5
Shane Mulligan <mullikine@gmail.com>
2021-08-08 20:41:49 +1200

    Sun 08 Aug 2021 20:41:49

--- src/pen-hf.el
+++ src/pen-hf.el
@@ -1,3 +1,10 @@
 ;; https://huggingface.co/docs/hub/endpoints
 
+;; Interface with huggingface model hub
+
+;; https://api-inference.huggingface.co/docs/python/html/quicktour.html#get-your-api-token
+;; 10,000+ transformer models for different tasks
+
+(provide 'pen-huggingface)
+
 (provide 'pen-hf)
