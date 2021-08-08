c27b879f134cd98c1f3ebb689bab55bc15b03ad8
Shane Mulligan <mullikine@gmail.com>
2021-08-08 20:41:53 +1200

    Sun 08 Aug 2021 20:41:53

--- src/pen-hf.el
+++ src/pen-hf.el
@@ -5,6 +5,4 @@
 ;; https://api-inference.huggingface.co/docs/python/html/quicktour.html#get-your-api-token
 ;; 10,000+ transformer models for different tasks
 
-(provide 'pen-huggingface)
-
 (provide 'pen-hf)
