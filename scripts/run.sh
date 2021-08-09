6883f5e8e151fca329000c50be4c82e946e2646e
Shane Mulligan <mullikine@gmail.com>
2021-08-09 18:50:17 +1200

    09.08.21

--- scripts/run.sh
+++ scripts/run.sh
@@ -18,9 +18,9 @@ butterfly.server.py \
     --host=0.0.0.0 \
     --port=57575 \
     --unsecure \
-    --cmd="bash -l $HOME/.emacs.d/pen.el/scripts/newframe.sh" \
+    --cmd="bash -l $EMACSD/pen.el/scripts/newframe.sh" \
     --shell=bash \
-    --i-hereby-declare-i-dont-want-any-security-whatsoever
+    --i-hereby-declare-i-dont-want-any-security-whatsoever &
 
 mkdir -p ~/.pen/ht-cache
 
