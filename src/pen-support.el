6a0fa4113e723ac6478fe3d50a02580bcccac838
Shane Mulligan <mullikine@gmail.com>
2021-08-09 19:13:09 +1200

    Mon 09 Aug 2021 19:13:09

--- src/pen-support.el
+++ src/pen-support.el
@@ -38,19 +38,20 @@
 
 (defun pen-yas-expand-string (ys)
   (interactive)
-  (save-excursion
-    (let ((m major-mode)
-          (b (new-buffer-from-string
-              ys)))
-      (str
-       (with-current-buffer b
-         (let ((s))
-           (funcall m)
-           (yas-minor-mode 1)
-           (yas-expand-snippet (buffer-string) (point-min) (point-max))
-           (setq s (buffer-string))
-           (kill-buffer b)
-           s))))))
+  (save-window-excursion
+    (save-excursion
+      (let ((m major-mode)
+            (b (new-buffer-from-string
+                ys)))
+        (str
+         (with-current-buffer b
+           (let ((s))
+             (funcall m)
+             (yas-minor-mode 1)
+             (yas-expand-snippet (buffer-string) (point-min) (point-max))
+             (setq s (buffer-string))
+             (kill-buffer b)
+             s)))))))
 
 (defun pen-get-glossary-topic (&optional fp)
   (if (pen-is-glossary-file)
