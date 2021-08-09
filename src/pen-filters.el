1c10030040ece9907aa0bf32c554cd9a51e373a3
Shane Mulligan <mullikine@gmail.com>
2021-08-10 01:59:15 +1200

    Tue 10 Aug 2021 01:59:15

--- src/pen-filters.el
+++ src/pen-filters.el
@@ -1,6 +1,6 @@
 (defun pen-filter-shellscript (script)
   "This will pipe the selection into fzf filters,\nreplacing the original region. If no region is\nselected, then the entire buffer is passed\nread only."
-  (interactive (list (fz (cat "$HOME/filters/filters.sh") nil nil "my/fwfzf: ")))
+  (interactive (list (fz (cat "$HOME/filters/filters.sh") nil nil "pen-filter-shellscript: ")))
   (if (region-active-p)
       (region-pipe
        (chomp
