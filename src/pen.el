728b5f6b8d65a0a51782d6b3a9e85e7bb1647843
Shane Mulligan <mullikine@gmail.com>
2021-08-12 01:05:32 +1200

    Thu 12 Aug 2021 01:05:32

--- src/pen.el
+++ src/pen.el
@@ -766,6 +766,7 @@ Function names are prefixed with pf- for easy searching"
                              (sor func-name)
                              func-sym
                              (sor title))
+                        ;; Unfortunately, this doesn't catch rust panics from yamlmod
                         (try
                          (let ((funcsym (define-prompt-function)))
                            (if funcsym
