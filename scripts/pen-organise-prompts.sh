2cce6fcb4a4db98bdc59bc68e21ab9bd66dfa576
Shane Mulligan <mullikine@gmail.com>
2021-08-17 01:56:40 +1200

    17.08.21

--- scripts/pen-organise-prompts.sh
+++ scripts/pen-organise-prompts.sh
@@ -2,7 +2,9 @@
 export TTY
 
 for fp in *.prompt; do 
-    name="$(cat "$fp" | yq -r .title | slugify )"
+    name="$(cat "$fp" | yq -r ".title // empty" | slugify)"
+    : "${name:="$(cat "$fp" | yq -r ".task // empty" | slugify)"}"
+    : "${name:="$(cat "$fp" | yq -r ".doc // empty" | slugify)"}"
     echo "$name"
     mv "$fp" "${name}.prompt"
 done
