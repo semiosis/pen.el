3f9a91b32f6022d330010f4b74d67c3e6d246832
Shane Mulligan <mullikine@gmail.com>
2021-08-16 22:22:02 +1200

    Mon 16 Aug 2021 22:22:02

--- src/pen-ink.el
+++ src/pen-ink.el
@@ -1,3 +1,23 @@
 ;; https://github.com/semiosis/ink.el
 
+(defun ink-encode (text &optional engine language topic)
+  (if (not engine)
+      (setq engine "OpenAI GPT-3"))
+  (if (not language)
+      (setq language "English"))
+
+  (let ((buf (new-buffer-from-string text))
+        (ink)
+        (start 1)
+        (end))
+    (with-current-buffer buf
+      ;; (insert text)
+      (setq end (length text))
+      (put-text-property start end 'engine engine)
+      (put-text-property start end 'language language)
+      (put-text-property start end 'topic topic)
+      (setq ink (format "%S" (buffer-string))))
+    (kill-buffer buf)
+    ink))
+
 (provide 'pen-ink)
