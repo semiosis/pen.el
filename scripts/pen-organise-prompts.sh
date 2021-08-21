511e6da2f2513ec4ae4886a23e71dd7a1695c1f5
Shane Mulligan <mullikine@gmail.com>
2021-08-22 11:11:18 +1200

    1629587478

--- scripts/pen-organise-prompts.sh
+++ scripts/pen-organise-prompts.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 export TTY
 
+# This is obsolete
+# For 2 reasons:
+# - task may be in ink
+# - task may be templated
+
 for fp in *.prompt; do 
     name="$(cat "$fp" | yq -r ".title // empty" | slugify)"
     : "${name:="$(cat "$fp" | yq -r ".task // empty" | slugify)"}"
