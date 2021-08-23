ad557a537b8c17bf034bf235bf7e7bb6c456b4a7
Shane Mulligan <mullikine@gmail.com>
2021-08-23 17:49:09 +1200

    1629697749

--- scripts/pen-aix.py
+++ scripts/pen-aix.py
@@ -3,7 +3,27 @@
 from aixapi import AIxResource
 import os
 
+def hard_bound(x, lower_lim, upper_lim):
+    if x and \
+       upper_lim and \
+       x > upper_lim:
+        x = upper_lim
+    elif x and \
+       lower_lim and \
+       x < lower_lim:
+        x = lower_lim
+    else:
+        x
+
 # Get your API Key at apps.aixsolutionsgroup.com
+
+max_tokens = os.environ.get("PEN_MAX_TOKENS") and float(os.environ.get("PEN_MAX_TOKENS"))
+min_tokens = os.environ.get("PEN_MIN_TOKENS") and float(os.environ.get("PEN_MIN_TOKENS"))
+engine_max_tokens = os.environ.get("PEN_ENGINE_MAX_TOKENS") and float(os.environ.get("PEN_ENGINE_MAX_TOKENS"))
+engine_min_tokens = os.environ.get("PEN_ENGINE_MIN_TOKENS") and float(os.environ.get("PEN_ENGINE_MIN_TOKENS"))
+
+max_tokens = hard_bound(max_tokens, engine_min_tokens, engine_max_tokens)
+min_tokens = hard_bound(min_tokens, engine_min_tokens, engine_max_tokens)
 
 if __name__ == "__main__":
     api_key = os.environ.get("AIX_API_KEY")
@@ -12,13 +32,13 @@ if __name__ == "__main__":
         str(
             aix_resource.compose(
                 os.environ.get("PEN_PROMPT"),
-                token_min_length = os.environ.get("PEN_MIN_TOKENS") and float(os.environ.get("PEN_MIN_TOKENS")),
-                token_max_length = os.environ.get("PEN_MAX_TOKENS") and float(os.environ.get("PEN_MAX_TOKENS")),
-                top_p = os.environ.get("PEN_TOP_P") and float(os.environ.get("PEN_TOP_P")),
-                top_k = os.environ.get("PEN_TOP_K") and float(os.environ.get("PEN_TOP_K")),
+                token_min_length = min_tokens,
+                token_max_length = max_tokens,
+                #  top_p = os.environ.get("PEN_TOP_P") and float(os.environ.get("PEN_TOP_P")),
+                #  top_k = os.environ.get("PEN_TOP_K") and float(os.environ.get("PEN_TOP_K")),
                 temperature = os.environ.get("PEN_TEMPERATURE") and float(os.environ.get("PEN_TEMPERATURE")),
                 stop_sequence = os.environ.get("PEN_STOP_SEQUENCE"),
-                model = os.environ.get("PEN_MODEL"),
+                #  model = os.environ.get("PEN_MODEL"),
             )
             .get("data", dict())
             .get("text")
