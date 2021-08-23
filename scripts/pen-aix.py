afd2b8f3ef91fe0eb20113181661e14afe8dd7ef
Shane Mulligan <mullikine@gmail.com>
2021-08-23 17:25:35 +1200

    1629696335

--- scripts/pen-aix.py
+++ scripts/pen-aix.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
         str(
             aix_resource.compose(
                 os.environ.get("PEN_PROMPT"),
-                token_min_length=int(os.environ.get("PEN_MIN_TOKENS")),
-                token_max_length=int(os.environ.get("PEN_MAX_TOKENS")),
-                top_p=float(os.environ.get("PEN_TOP_P")),
-                top_k=float(os.environ.get("PEN_TOP_K")),
-                temperature=float(os.environ.get("PEN_TEMPERATURE")),
-                stop_sequence=os.environ.get("PEN_STOP_SEQUENCE"),
-                custom_model_id=os.environ.get("PEN_MODEL"),
+                token_min_length = os.environ.get("PEN_MIN_TOKENS") and int(os.environ.get("PEN_MIN_TOKENS")),
+                token_max_length = os.environ.get("PEN_MAX_TOKENS") and int(os.environ.get("PEN_MAX_TOKENS")),
+                top_p = os.environ.get("PEN_TOP_P") and float(os.environ.get("PEN_TOP_P")),
+                top_k = os.environ.get("PEN_TOP_K") and float(os.environ.get("PEN_TOP_K")),
+                temperature = os.environ.get("PEN_TEMPERATURE") and float(os.environ.get("PEN_TEMPERATURE")),
+                stop_sequence = os.environ.get("PEN_STOP_SEQUENCE"),
+                custom_model_id = os.environ.get("PEN_MODEL"),
             )
             .get("data", dict())
             .get("text")
