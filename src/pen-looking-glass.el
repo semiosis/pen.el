ddd2f8f63b6380425a39516b8ef8a93b57790917
Shane Mulligan <mullikine@gmail.com>
2021-09-10 11:45:26 +1200

    Fri 10 Sep 2021 11:45:26

--- src/pen-looking-glass.el
+++ src/pen-looking-glass.el
@@ -5,4 +5,6 @@
 ;; If a url is 404, then use the imaginary web-browser.
 ;; If a url is real then go to that.
 
+;; When running an imaginary web search, optionally say which ones are real
+
 (provide 'pen-looking-glass)
