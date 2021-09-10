9fd83534e0802957a8149b98f450d29ae6fa2ad6
Shane Mulligan <mullikine@gmail.com>
2021-09-10 17:58:43 +1200

    Fri 10 Sep 2021 17:58:43

--- src/pen-tty.el
+++ src/pen-tty.el
@@ -30,12 +30,54 @@
 (define-key key-translation-map (kbd "<f20>") (kbd "<S-f8>"))
 (define-key key-translation-map (kbd "<f21>") (kbd "<S-f9>"))
 
+;; https://www.emacswiki.org/emacs/TheMysteriousCaseOfShiftedFunctionKeys
+
+(global-set-key [S-f10] 'help-with-tutorial)
+
+(progn
+  ;; Not sure if these are necessary
+  (define-key input-decode-map (kbd "<S-f10>") nil)
+  (define-key function-key-map (kbd "<S-f10>") nil)
+  (define-key key-translation-map (kbd "<S-f10>") nil)
+  (define-key key-translation-map (kbd "<S-f11>") nil)
+  (define-key key-translation-map (kbd "<S-f12>") nil)
+
+  (define-key function-key-map (kbd "<f22>") (kbd "<S-f10>"))
+  (define-key function-key-map (kbd "<f23>") (kbd "<S-f11>"))
+  (define-key function-key-map (kbd "<f24>") (kbd "<S-f12>")))
+
+(define-key input-decode-map (kbd "<f22>") (kbd "<S-f10>"))
+(define-key input-decode-map (kbd "<f23>") (kbd "<S-f11>"))
+(define-key input-decode-map (kbd "<f24>") (kbd "<S-f12>"))
+
 ;; Why doen't these exist?
 (define-key key-translation-map (kbd "<f22>") (kbd "<S-f10>"))
 (define-key key-translation-map (kbd "<f23>") (kbd "<S-f11>"))
 (define-key key-translation-map (kbd "<f24>") (kbd "<S-f12>"))
 
+(comment
+ (defun yank-function-from-binding (sequence)
+   "Copies the function name associated with a key binding after entering it"
+   (interactive (list (format "%s" (key-description (read-key-sequence-vector "Key: ")))))
+   (let* ((fun (str (key-binding (kbd sequence)))))
+     (xc fun)
+     (message "%s" (concat "copied: " fun)))))
+
+(defun test-yank-function-from-binding ()
+  (yank-function-from-binding "\e[21;2~")
+  (yank-function-from-binding "[S-f10]")
+  (yank-function-from-binding "<S-f10>")
+  (yank-function-from-binding "<f10>"))
+
+(define-key input-decode-map "[21;2~" [S-f10])
+(define-key input-decode-map [S-f10] nil)
 (define-key input-decode-map "\e[21;2~" [S-f10])
+(define-key global-map "\e[21;2~" [S-f10])
+
+;; For some reason, this is actually mapping to f10 from emacs
+(define-key input-decode-map (kbd "<esc>[21;2~") [S-f10])
+;; (define-key input-decode-map (kbd "<esc>[21;2~") [f1])
+
 (define-key input-decode-map "\e[23;2~" [S-f11])
 (define-key input-decode-map "\e[24;2~" [S-f12])
 
