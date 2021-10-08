ee5d2616b587919c721eca893750d577e1a1e722
Shane Mulligan <mullikine@gmail.com>
2021-10-08 13:23:01 +1300

    Fri 08 Oct 2021 13:23:01

--- src/pen.el
+++ src/pen.el
@@ -523,6 +523,7 @@ Reconstruct the entire yaml-ht for a different language."
                            ;; <delim>
                            (pen-expand-template-keyvals it subprompts-al t)
                            (pen-expand-template it vals t)
+                           ;; I also want to encode newlines into <pen-newline> and <pen-dnl>
                            (pen-expand-template-keyvals it (list (cons "delim" (pen-encode-string final-delimiter))) t)
                            (pen-expand-template-keyvals it var-keyvals-slugged t)
                            (pen-expand-template-keyvals it var-keyvals t)
