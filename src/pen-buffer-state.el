8e8825799249c75b60174af44751f2676faa4965
Shane Mulligan <mullikine@gmail.com>
2021-10-14 17:14:22 +1300

    Thu 14 Oct 2021 17:14:22

--- src/pen-buffer-state.el
+++ src/pen-buffer-state.el
@@ -5,15 +5,18 @@
 
 (define-key pen-map (kbd "H-H") 'buffer-hyperparameters-transient)
 
+
+;; TODO Figure out how to *load* state into the UI, so I can see if an option is already enabled
+
 (define-transient-command buffer-hyperparameters-transient ()
   "Transient for pulling images."
   :man-page "docker-image-pull"
   ["Arguments"
    ("-a" "All" "-a")]
   [:description docker-utils-generic-actions-heading
-   ("F" "Pull selection" docker-utils-generic-action)
-   ("T" "Pull specific tag (sps)" my-docker-pull-specific-tag)
-   ("N" "Pull a new image" docker-image-pull-one)])
+                ("F" "Pull selection" docker-utils-generic-action)
+                ("T" "Pull specific tag (sps)" my-docker-pull-specific-tag)
+                ("N" "Pull a new image" docker-image-pull-one)])
 
 
 
