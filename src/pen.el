df2c05c211d4d388a1548fcf247ac4ac32fbadcc
Shane Mulligan <mullikine@gmail.com>
2021-10-15 13:06:13 +1300

    Fri 15 Oct 2021 13:06:13

--- src/pen.el
+++ src/pen.el
@@ -658,6 +658,10 @@ Reconstruct the entire yaml-ht for a different language."
                                         ;; This is necessary because most modes
                                         ;; do not allow allow you to change the faces.
                                         ("INK_TYPE" . "generated")))
+
+           ;; Many a  transformation pipeline here could benefit from transducers
+           ;; https://dev.solita.fi/2021/10/14/grokking-clojure-transducers.html
+           ;; https://github.com/FrancisMurillo/transducer.el
            (let* (;; Keep in mind this both updates memoization and the bash cache
                   (do-pen-update (pen-var-value-maybe 'do-pen-update))
 
