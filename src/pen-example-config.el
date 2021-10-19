e0e559a3619db4ed7d27e2f1caa676d7fc165a6f
Shane Mulligan <mullikine@gmail.com>
2021-10-19 19:56:12 +1300

    Tue 19 Oct 2021 19:56:12

--- src/pen-example-config.el
+++ src/pen-example-config.el
@@ -295,6 +295,7 @@
 ;; g is for generation
 (define-key pen-map (kbd "H-\" g b") 'pf-write-a-blog-post/2)
 ;; U is for understand
+(define-key pen-map (kbd "H-\" T") 'pf-transpile/3)
 (define-key pen-map (kbd "H-\" U") (dff (etv (pf-transpile/3 nil nil (sor pen-fav-programming-language)))))
 (define-key pen-map (kbd "H-\" W") (dff (etv (pf-transpile/3 nil nil (sor pen-fav-world-language)))))
 
