691f63eb792d6c06460d1585ed953e738a60e0c5
Shane Mulligan <mullikine@gmail.com>
2021-10-20 16:58:05 +1300

    Wed 20 Oct 2021 16:58:05

--- src/pen-diagnostics.el
+++ src/pen-diagnostics.el
@@ -1,24 +1,49 @@
+(defun pen-strip-unicode (s)
+  (pen-snc "pen-c strip-unicode" s))
+
+(defun pen-tmp-preview (slug)
+  (pen-strip-unicode
+   (pen-snc
+    (pen-cmd
+     "head" "-n" 5
+     (f-join
+      penconfdir
+      "temp"
+      (concat (slugify slug) ".txt"))))))
+
 (defun pen-diagnostics-show-context ()
   (interactive)
-  (let* ((plist (list
-                 :semantic-path (get-path-semantic)
-                 :last-final-command (pen-snc "cat ~/.pen/last-final-command.txt")
-                 ;; strangely, some characters break plist2yaml plist
-                 :last-final-prompt (pen-snc "pen-c strip-unicode" (pen-snc "cat ~/.pen/last-final-prompt.txt"))
-                 :last-pen-command-exprs (pen-snc "cat ~/.pen/last-pen-command-exprs.txt")
-                 :last-pen-command (pen-snc "cat ~/.pen/last-pen-command.txt")
-                 :pen-force-engine pen-force-engine
-                 :lm-complete-stderr (pen-snc (pen-cmd "cat" "/tmp/lm-complete-stderr.txt"))
-                 :lm-complete-stdout (pen-snc (pen-cmd "cat" "/tmp/lm-complete-stdout.txt"))
-                 :lm-complete-results (pen-snc (concat "find " (pen-q (pen-snc (pen-cmd "cat" "/tmp/lm-complete-stdout.txt"))) " | while read line; do cat \$line; echo; done"))
-                 :openai-last-output (pen-snc (pen-cmd "cat" "/tmp/openai-temp.txt"))
-                 :hf-last-output (pen-snc (pen-cmd "cat" "/tmp/hf-temp.txt"))
-                 :aix-last-output (pen-snc (pen-cmd "cat" "/tmp/aix-temp.txt"))
-
-                 ;; This is usually so long it causes problems
-                 ;; :ai21-last-output (pen-snc (pen-cmd "cat" "/tmp/ai21-temp.txt"))
-                 )))
-    (find-file (pen-tf "pen context" (plist2yaml plist) "yaml"))))
+  (if (and (sor penconfdir)
+           (f-directory-p penconfdir))
+      (let* ((plist (list
+                     :semantic-path (get-path-semantic)
+                     :last-final-command (pen-snc "cat ~/.pen/last-final-command.txt")
+                     ;; strangely, some characters break plist2yaml plist
+                     :last-final-prompt (pen-strip-unicode (pen-snc "cat ~/.pen/last-final-prompt.txt"))
+                     :last-pen-command-exprs (pen-snc "cat ~/.pen/last-pen-command-exprs.txt")
+                     :last-pen-command (pen-snc "cat ~/.pen/last-pen-command.txt")
+                     :pen-force-engine pen-force-engine
+                     :lm-complete-stderr (pen-tmp-preview "lm-complete-stderr")
+                     :lm-complete-stdout (pen-tmp-preview "lm-complete-stdout")
+                     :lm-complete-results
+                     (pen-snc
+                      (concat
+                       "find "
+                       (pen-q (pen-snc (pen-cmd "cat" (f-join penconfdir "temp" "lm-complete-stdout.txt"))))
+                       " | while read line; do cat \$line; echo; done"))
+                     :openai-last-output (pen-tmp-preview "openai-temp")
+                     :openai-last-output-fp (f-join penconfdir "temp" "openai-temp.txt")
+                     :hf-last-output (pen-tmp-preview "hf-temp")
+                     :hf-last-output-fp (f-join penconfdir "temp" "hf-temp.txt")
+                     :nlpcloud-last-output (pen-tmp-preview "nlpcloud-temp")
+                     :nlpcloud-last-output-fp (f-join penconfdir "temp" "nlpcloud-temp.txt")
+                     :aix-last-output (pen-tmp-preview "aix-temp")
+                     :aix-last-output-fp (f-join penconfdir "temp" "aix-temp.txt")
+                     :ruby-gen-next-user-prompt (f-join penconfdir "temp" "ruby-gen-next-user-prompt.txt")
+                     ;; This is usually so long it causes problems
+                     ;; :ai21-last-output (pen-snc (pen-cmd "cat" "/tmp/ai21-temp.txt"))
+                     )))
+        (find-file (pen-tf "pen context" (plist2yaml plist) "yaml")))))
 
 (defun pen-diagnostics-test-key ()
   (interactive)
