ea206c86ed45ad3b41d646753bb7b102f3bb3278
Shane Mulligan <mullikine@gmail.com>
2021-10-24 18:14:37 +1300

    Sun 24 Oct 2021 18:14:37

--- src/pen.el
+++ src/pen.el
@@ -2671,7 +2671,7 @@ Function names are prefixed with pf- for easy searching"
                                        (pen-selected-text)
                                      ;; (eval-string default-readstring-cmd)
                                      ;; (read-string-hist ,(concat varslug ": ") ,example)
-                                     (read-string-hist ,(concat title " (" varslug "): ") ,example)
+                                     (read-string-hist ,(concat varslug ": ") ,example)
                                      ;; TODO Find a way to do multiline entry
                                      ;; (if ,(> (length (s-lines example)) 1)
                                      ;;     (multiline-reader ,example)
@@ -2690,7 +2690,7 @@ Function names are prefixed with pf- for easy searching"
                                       ',',(pen-subprompts-to-alist subprompts)
                                       (if ,,default
                                           (eval-string ,,(str default))
-                                        (read-string-hist ,,(concat title " (" varname "): ") ,,example)))))))
+                                        (read-string-hist ,,(concat varname ": ") ,,example)))))))
                             do
                             (progn
                               (setq iteration (+ 1 iteration))
