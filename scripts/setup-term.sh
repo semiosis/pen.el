59ba9f7262b2c4aaea0557556b55a6af81a24fb1
Shane Mulligan <mullikine@gmail.com>
2021-10-26 13:35:52 +1300

    Tue 26 Oct 2021 13:35:52

--- scripts/setup-term.sh
+++ scripts/setup-term.sh
@@ -11,3 +11,4 @@ export LANG=en_US
 export EMACSD=/root/.emacs.d
 export YAMLMOD_PATH=$EMACSD/emacs-yamlmod
 export PATH=$PATH:$EMACSD/host/pen.el/scripts:$EMACSD/pen.el/scripts
+export PATH="$PATH:/root/go/bin"
