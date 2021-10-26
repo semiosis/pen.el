bf51fd8565530ed62b9318142f53585a1a6ef7f3
Shane Mulligan <mullikine@gmail.com>
2021-10-26 17:48:26 +1300

    Tue 26 Oct 2021 17:48:26

--- src/pen.el
+++ src/pen.el
@@ -3467,6 +3467,7 @@ May use to generate code from comments."
          (vals (eval-string (concat "'" (cdr (assoc "PEN_VALS" al)))))
          ;; (orig-inject-len (string-bytes (cdr (assoc "PEN_INJECT_GEN_START" al))))
          (orig-inject-len (length (cdr (assoc "PEN_INJECT_GEN_START" al))))
+         ;; Sometimes this is a number
          (result (cdr (assoc "PEN_RESULT" al)))
          ;; This is json encoded
          (results (cdr (assoc "PEN_RESULTS" al)))
