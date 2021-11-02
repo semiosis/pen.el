535b75be4abdb7c1f635be09d5b824f6f7b71598
Shane Mulligan <mullikine@gmail.com>
2021-11-03 00:20:14 +1300

    Wed 03 Nov 2021 00:20:14

--- scripts/pen-alephalpha.py
+++ scripts/pen-alephalpha.py
@@ -4,9 +4,11 @@
 import json
 import os
 
-API_TOKEN = os.environ.get("ALEPHALPHA_API_KEY")
-PEN_MODEL = os.environ.get("PEN_MODEL")
+ALEPHALPHA_API_KEY = os.environ.get("ALEPHALPHA_API_KEY")
+PEN_MODEL = os.environ.get("PEN_MODEL") or "EUTranMultimodal"
+# model = "EUTranLarge128kAlpha001DataAlpha1"
 PEN_PROMPT = os.environ.get("PEN_PROMPT")
+PEN_API_ENDPOINT = os.environ.get("PEN_API_ENDPOINT") or "https://api.aleph-alpha.de"
 PEN_PAYLOADS = os.environ.get("PEN_PAYLOADS")
 PEN_MODE = os.environ.get("PEN_MODE")
 PEN_TRAILING_WHITESPACE = os.environ.get("PEN_TRAILING_WHITESPACE")
@@ -14,13 +16,11 @@ PEN_TRAILING_WHITESPACE = os.environ.get("PEN_TRAILING_WHITESPACE")
 from aleph_alpha_client import ImagePrompt, AlephAlphaClient
 
 client = AlephAlphaClient(
-    host="https://test.api.aleph-alpha.de",
-    token=key
+    host=PEN_API_ENDPOINT,
+    token=ALEPHALPHA_API_KEY
 )
 
 # You need to choose a model with multimodal capabilities for this example.
-model = "EUTranMultimodal"
-# model = "EUTranLarge128kAlpha001DataAlpha1"
 # url = "https://cdn-images-1.medium.com/max/1200/1*HunNdlTmoPj8EKpl-jqvBA.png"
 path = "/home/shane/blog/posts/148658560_2839287366296108_857180560792297037_n.jpg"
 
@@ -28,8 +28,11 @@ path = "/home/shane/blog/posts/148658560_2839287366296108_857180560792297037_n.j
 image = ImagePrompt.from_file(path)
 prompt = [
     image,
-    """In this surreal artwork,"""
+    PEN_PROMPT
 ]
-result = client.complete(model, prompt=prompt, temperature=0.0, maximum_tokens=30)
+result = client.complete(PEN_MODEL,
+                         prompt=prompt,
+                         temperature=os.environ.get("PEN_TEMPERATURE") and float(os.environ.get("PEN_TEMPERATURE")),
+                         maximum_tokens=30)
 
 print(result["completions"][0]["completion"])
