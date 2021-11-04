5e5f7128e0d4b2f891fb2a8e80d3f2d6a021aaf1
Shane Mulligan <mullikine@gmail.com>
2021-11-04 21:24:19 +1300

    Thu 04 Nov 2021 21:24:19

--- src/pen-custom.el
+++ src/pen-custom.el
@@ -23,6 +23,13 @@ This is useful for code-understanding when reading languages you don't understan
   :initialize (lambda(_sym _exp)
                 (custom-initialize-default _sym nil)))
 
+(defcustom pen-describe-images t
+  "Strip unicode from input"
+  :type 'boolean
+  :group 'pen
+  :initialize (lambda(_sym _exp)
+                (custom-initialize-default _sym nil)))
+
 (defcustom pen-force-strip-unicode t
   "Strip unicode from input"
   :type 'boolean
