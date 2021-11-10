6443314f04ee6565914ef7403831262e138c4e57
Shane Mulligan <mullikine@gmail.com>
2021-11-11 00:51:13 +1300

    Thu 11 Nov 2021 00:51:13

--- scripts/newframe.sh
+++ scripts/newframe.sh
@@ -11,4 +11,4 @@ export EMACSD=/root/.emacs.d
 export YAMLMOD_PATH=$EMACSD/emacs-yamlmod
 export PATH=$PATH:$EMACSD/host/pen.el/scripts:$EMACSD/pen.el/scripts
 
-emacsclient -a "" -s /home/shane/.emacs.d/server/SPACEMACS -t "$@"
+emacsclient -a "" -t "$@"
