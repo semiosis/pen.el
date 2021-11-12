1346aa324abac29f82ddfdb179e06a5ec36db8b1
Shane Mulligan <mullikine@gmail.com>
2021-11-12 21:02:35 +1300

    1636704155

--- src/pen-edit.el
+++ src/pen-edit.el
@@ -2,11 +2,12 @@
 
 ;; Only do this if it's in foundation mode?
 
+;; This appears to not work. Try a restart
 (defun paste-around-advice (proc &rest args)
   (let* ((text (car kill-ring))
          (res (apply proc args)))
 
-    (if (major-mode-p 'foundation-modee)
+    (if (major-mode-p 'fundamental-mode)
         (detect-language-set-mode))
     res))
 (advice-add 'paste :around #'yank)
