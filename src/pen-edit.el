4354d7484e47eb6f566c5930e7d99da202d4c318
Shane Mulligan <mullikine@gmail.com>
2021-11-12 20:48:20 +1300

    Fri 12 Nov 2021 20:48:20

--- src/pen-edit.el
+++ src/pen-edit.el
@@ -2,4 +2,14 @@
 
 ;; Only do this if it's in foundation mode?
 
+(defun paste-around-advice (proc &rest args)
+  (let* ((text (car kill-ring))
+         (res (apply proc args)))
+
+    (if (major-mode-p 'foundation-modee)
+        (detect-language-set-mode))
+    res))
+(advice-add 'paste :around #'cua-paste)
+(advice-add 'paste :around #'lispy-yank)
+
 (provide 'pen-edit)
