edfa93f372e568e269f40aa3b584cddb5606ab89
Shane Mulligan <mullikine@gmail.com>
2021-11-14 01:34:11 +1300

    1636806851

--- scripts/run.sh
+++ scripts/run.sh
@@ -19,17 +19,19 @@ export PATH="$PATH:/root/go/bin"
 # for ttyd
 export LD_LIBRARY_PATH=/root/libwebsockets/build/lib:$LD_LIBRARY_PATH
 
-ttyd -p 7681 bash -l newframe.sh &
+ttyd -p 7681 bash -l newframe.sh &>/dev/null &
 
-# This should be 'pen' if on the host but 'emacs -nwemacsclient -a "" -t' if inside docker
-butterfly.server.py \
-    --login=False \
-    --host=0.0.0.0 \
-    --port=57575 \
-    --unsecure \
-    --cmd="bash -l newframe.sh" \
-    --shell=bash \
-    --i-hereby-declare-i-dont-want-any-security-whatsoever &
+echo "ttyd running on port 7681, serving Pen.el on http"
+
+# # This should be 'pen' if on the host but 'emacs -nwemacsclient -a "" -t' if inside docker
+# butterfly.server.py \
+#     --login=False \
+#     --host=0.0.0.0 \
+#     --port=57575 \
+#     --unsecure \
+#     --cmd="bash -l newframe.sh" \
+#     --shell=bash \
+#     --i-hereby-declare-i-dont-want-any-security-whatsoever &
 
 mkdir -p ~/.pen/ht-cache
 
