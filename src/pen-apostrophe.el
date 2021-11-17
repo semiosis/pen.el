3d5c825f8e2fa0f8e70396fff91904d10e064364
Shane Mulligan <mullikine@gmail.com>
2021-11-18 12:30:15 +1300

    Thu 18 Nov 2021 12:30:15

--- src/pen-apostrophe.el
+++ src/pen-apostrophe.el
@@ -5,7 +5,7 @@
        (blurb (pf-generate-wiki-blurb-for-a-famous-person/1 sme)))
 
   (let* ((el (pen-snc (cmd "apostrophe" "-getcomintcmd" sme "" blurb))))
-    (pen-eval-string el)
+    (pen-e-sps (pen-lm (pen-eval-string el)))
     (never (sps (cmd "apostrophe" "" blurb))))))
 
 (provide 'pen-apostrophe)
