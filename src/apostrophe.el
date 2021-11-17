0d2776c18f2deb5bf8b182c1cb096dfb488b0e28
Shane Mulligan <mullikine@gmail.com>
2021-11-17 21:01:32 +1300

    Wed 17 Nov 2021 21:01:32

--- src/apostrophe.el
+++ src/apostrophe.el
@@ -1,8 +1,8 @@
 (defun apostrophe-start-chatbot-from-selection (text)
   (interactive (list (pen-screen-or-selection)))
 
-(let* ((sme (pf-who-is-the-subject-matter-expert-for-/1 text))
-       (blurb (pf-generate-wiki-blurb-for-a-famous-person/1 sme)))
-  (sps (cmd "apostrophe" sme "" blurb))))
+  (let* ((sme (pf-who-is-the-subject-matter-expert-for-/1 text))
+         (blurb (pf-generate-wiki-blurb-for-a-famous-person/1 sme)))
+    (sps (cmd "apostrophe" sme "" blurb))))
 
 (provide 'pen-apostrophe)
