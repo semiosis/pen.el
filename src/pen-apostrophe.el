72edc04d4e1947c2ba8d5ae93b52a7bfb34ee48e
Shane Mulligan <mullikine@gmail.com>
2021-11-18 13:11:54 +1300

    Thu 18 Nov 2021 13:11:54

--- src/pen-apostrophe.el
+++ src/pen-apostrophe.el
@@ -1,12 +1,15 @@
 (defun apostrophe-start-chatbot-from-selection (text)
   (interactive (list (pen-screen-or-selection)))
 
-(let* ((sme (pf-who-is-the-subject-matter-expert-for-/1 text))
-       (blurb (pf-generate-wiki-blurb-for-a-famous-person/1 sme)))
+  (if (not text)
+      (setq text (pen-screen-or-selection)))
 
-  (let* ((el (pen-snc (pen-cmd "apostrophe" "-getcomintcmd" sme "" blurb))))
-    ;; TODO Run multiple daemons and run tasks from a pool?
-    (pen-e-sps (pen-lm (pen-eval-string el)))
-    (never (sps (cmd "apostrophe" "" blurb))))))
+  (let* ((sme (pf-who-is-the-subject-matter-expert-for-/1 text))
+         (blurb (pf-generate-wiki-blurb-for-a-famous-person/1 sme)))
+
+    (let* ((el (pen-snc (pen-cmd "apostrophe" "-getcomintcmd" sme "" blurb))))
+      ;; TODO Run multiple daemons and run tasks from a pool?
+      (pen-e-sps (pen-lm (pen-eval-string el)))
+      (never (sps (cmd "apostrophe" "" blurb))))))
 
 (provide 'pen-apostrophe)
