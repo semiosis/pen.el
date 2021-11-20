c27e094417445b38032a9af62d6a91ffd09286e2
Shane Mulligan <mullikine@gmail.com>
2021-11-20 16:16:37 +1300

    Sat 20 Nov 2021 16:16:37

--- src/pen-client.el
+++ src/pen-client.el
@@ -1,5 +1,5 @@
 ;; Pen thin client for emacs
-;; This communicates with a Pen.el docker container for basic prompt functions with no interactivity
+;; This communicates with a Pen.el docker container for basic prompt functions
 
 (require 'pp)
 
@@ -56,21 +56,40 @@
                       "'"
                       (pen-snc (cmd "pene" "(pen-list-signatures-for-client)"))))))
 
-    (etv
-     (cl-loop
-      for s in sig-sexps collect
-      (let* ((fn-name
-              (replace-regexp-in-string "(pf-\\([^ )]*\\).*" "pen-fn-\\1" (pp-oneline s)))
-             (fn-sym
-              (intern fn-name))
-             (args
-              (replace-regexp-in-string "^[^ ]* &optional\\(.*\\))$" "\\1" (pp-oneline s))))
+    (cl-loop
+     for s in sig-sexps do
+     (let* ((fn-name
+             (replace-regexp-in-string "(pf-\\([^ )]*\\).*" "pen-fn-\\1" (pp-oneline s)))
+            (remote-fn-name
+             (replace-regexp-in-string "(\\([^ )]*\\).*" "\\1" (pp-oneline s)))
+            (fn-sym
+             (intern fn-name))
+            (args
+             (replace-regexp-in-string "^[^ ]* &optional *\\(.*\\))$" "\\1" (pp-oneline s)))
+            (arg-list
+             (s-split " " args))
+            (arg-list-syms
+             (mapcar 'intern arg-list))
+            (ilist
+             (cons
+              'list
+              (cl-loop
+               for a in arg-list collect
+               (eval-string (concat "'(read-string " (pen-q (concat a ": ")) ")")))))
+            ;; (sn-cmd (concat "0</dev/null " (eval `(cmd "penf" ,remote-fn-name ,@arg-list-syms))))
+            (sn-cmd `(cmd "penf" ,remote-fn-name ,@arg-list-syms)))
 
-        `(defun ,fn-sym ,(eval-string (format "'(&optional %s)" args))
-           (interactive)
-           (etv ,args)
-           (etv (pen-sn-basic (cmd "penf" ,fn-name))))
-        ;; (pp-oneline (eval-string (format "'(&optional %s)" args)))
-        )))))
+       (eval
+        `(defun ,fn-sym ,(eval-string
+                          (if (string-equal args "")
+                              "'()"
+                            (format "'(&optional %s)" args)))
+           ,(cons 'interactive (list ilist))
+           (let ((result (chomp (eval `(pen-sn-basic ,,sn-cmd)))))
+             (if (interactive-p)
+                 (etv result)
+               result))))
+       ;; (pp-oneline (eval-string (format "'(&optional %s)" args)))
+       ))))
 
 (provide 'pen-client)
