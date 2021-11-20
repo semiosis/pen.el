58ad9b882d25e5c06dc09168f5e28e2eaee9fff6
Shane Mulligan <mullikine@gmail.com>
2021-11-20 14:07:04 +1300

    Sat 20 Nov 2021 14:07:04

--- src/pen-client.el
+++ src/pen-client.el
@@ -1,4 +1,13 @@
 ;; Pen thin client for emacs
-;; This communicates with a Pen.el docker container for prompt functions
+;; This communicates with a Pen.el docker container for basic prompt functions with no interactivity
+
+(defun pen-client-generate-functions ()
+  (interactive)
+
+  (let* ((fn-names (pen-str2list (pen-snc "penl")))))
+
+  (loop for )
+
+  )
 
 (provide 'pen-client)
