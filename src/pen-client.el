589fd4ea0a6350b777ce9871108073ee9e915f11
Shane Mulligan <mullikine@gmail.com>
2021-11-20 13:56:18 +1300

    Sat 20 Nov 2021 13:56:18

--- src/pen-client.el
+++ src/pen-client.el
@@ -1,4 +1,4 @@
 ;; Pen thin client for emacs
-;; This communicates with a Pen.el docker container for everything
+;; This communicates with a Pen.el docker container for prompt functions
 
 (provide 'pen-client)
