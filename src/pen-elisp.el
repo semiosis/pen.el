bfdf365b2f3f9e5804bbba61d6ccfdc57110d726
Shane Mulligan <mullikine@gmail.com>
2021-11-27 15:47:14 +1300

    27.11.21

--- src/pen-elisp.el
+++ src/pen-elisp.el
@@ -2,4 +2,7 @@
 
 ;; (add-hook 'emacs-lisp-mode-hook '(lambda () (lispy-mode 1)))
 
+(advice-add 'lispy-mark-symbol :around #'ignore-errors-around-advice)
+(advice-add 'kill-ring-save :around #'ignore-errors-around-advice)
+
 (provide 'pen-elisp)
