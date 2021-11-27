f48fabbb2d0e694367b09bda1bc448f3034bdbe9
Shane Mulligan <mullikine@gmail.com>
2021-11-27 21:26:08 +1300

    Sat 27 Nov 2021 21:26:08

--- src/pen-comint.el
+++ src/pen-comint.el
@@ -4,4 +4,15 @@
 (define-key comint-mode-map (kbd "C-a") 'comint-bol)
 (define-key comint-mode-map (kbd "C-e") 'end-of-line)
 
+(defun pen-comint-del ()
+  (interactive)
+
+  (let ((pos
+         (save-excursion-and-region-reliably
+          (comint-bol))))
+    (if (< pos (point))
+        (delete-backward-char 1))))
+
+(define-key pen-map (kbd "DEL") 'pen-comint-del)
+
 (provide 'pen-comint)
