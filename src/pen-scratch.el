a526404bb6f0115d3778b22235dc88c4535b6b82
Shane Mulligan <mullikine@gmail.com>
2021-11-28 19:38:43 +1300

    Sun 28 Nov 2021 19:38:43

--- src/pen-scratch.el
+++ src/pen-scratch.el
@@ -6,4 +6,14 @@
     (bury-buffer)
     nil))
 
+(defun pen-create-scratch-buffer ()
+  "create a scratch buffer"
+  (interactive)
+  (switch-to-buffer (get-buffer-create "*scratch*"))
+  (text-mode)
+  ;; (lisp-interaction-mode)
+  )
+
+;; (pen-create-scratch-buffer)
+
 (provide 'pen-scratch)
