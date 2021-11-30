d7d5ad5b59294bf5c1ff2a23f0f46492b174c972
Shane Mulligan <mullikine@gmail.com>
2021-12-01 00:38:20 +1300

    1638272300

---
+++ config/example-client-init.el
@@ -0,0 +1,5 @@
+;; TODO Make two melpa packages? Or one?
+;; Call it Pen Client, or ilambda?
+
+(load "/home/shane/source/git/semiosis/pen.el/src/ilambda.el")
+(load "/home/shane/source/git/semiosis/pen.el/src/pen-client.el")
