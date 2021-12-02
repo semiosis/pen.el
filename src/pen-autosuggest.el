3644174bb2ce4daea7cc4a6431283c923d830c2e
Shane Mulligan <mullikine@gmail.com>
2021-12-02 19:15:18 +1300

    Thu 02 Dec 2021 19:15:18

---
+++ src/pen-autosuggest.el
@@ -0,0 +1,135 @@
+;;; pen-autosuggest.el --- Language model autosuggestions for pen.el -*- lexical-binding: t; -*-
+
+;; Adapted from:
+;; https://github.com/dieggsy/esh-autosuggest
+
+(require 'company)
+(require 'cl-lib)
+
+(defgroup pen-autosuggest nil
+  "Fish-like autosuggestions for penell."
+  :group 'company)
+
+(defcustom pen-autosuggest-delay 0
+  "Delay for history autosuggestion."
+  :group 'pen-autosuggest
+  :type 'number)
+
+(defcustom pen-autosuggest-use-company-map nil
+  "Instead of overriding `company-active-map', use as-is.
+
+This is disabled by default, as bindings in `company-active-map'
+to RET and TAB may interfere with command input and completion
+respectively."
+  :group 'pen-autosuggest
+  :type 'boolean)
+
+(defvar pen-autosuggest-active-map
+  (let ((keymap (make-sparse-keymap)))
+    (define-key keymap (kbd "<right>") 'company-complete-selection)
+    (define-key keymap (kbd "C-f") 'company-complete-selection)
+    (define-key keymap (kbd "M-<right>") 'pen-autosuggest-complete-word)
+    (define-key keymap (kbd "M-f") 'pen-autosuggest-complete-word)
+    keymap)
+  "Keymap that is enabled during an active history
+  autosuggestion.")
+
+(defun pen-autosuggest-candidates (prefix)
+  "Select the first penell history candidate that starts with PREFIX."
+  (let* ((history
+          (delete-dups
+           (mapcar (lambda (str)
+                     (string-trim (substring-no-properties str)))
+                   (ring-elements penell-history-ring))))
+         (most-similar (cl-find-if
+                        (lambda (str)
+                          (string-prefix-p prefix str))
+                        history)))
+    (when most-similar
+      `(,most-similar))))
+
+(defun pen-autosuggest-complete-word ()
+  (interactive)
+  (save-excursion
+    (let ((pos (point)))
+      (company-complete-selection)
+      (goto-char pos)
+      (forward-word)
+      (unless (or (eobp) (eolp))
+        (kill-line))))
+  (end-of-line)
+  (ignore-errors
+    (let ((inhibit-message t))
+      (company-begin-backend 'pen-autosuggest))))
+
+(defun pen-autosuggest--prefix ()
+  "Get current penell input."
+  (let* ((input-start (progn
+                        (save-excursion
+                          (beginning-of-line)
+                          (while (not (looking-at-p penell-prompt-regexp))
+                            (forward-line -1))
+                          (re-search-forward penell-prompt-regexp nil 'noerror)
+                          (penell-bol))))
+         (prefix
+          (string-trim-left
+           (buffer-substring-no-properties
+            input-start
+            (line-end-position)))))
+    (if (not (string-empty-p prefix))
+        prefix
+      'stop)))
+
+;;;###autoload
+(defun pen-autosuggest (command &optional arg &rest ignored)
+  "`company-mode' backend to provide penell history suggestion."
+  (interactive (list 'interactive))
+  (cl-case command
+    (interactive (company-begin-backend 'pen-autosuggest))
+    (prefix (and (eq major-mode 'penell-mode)
+                 (pen-autosuggest--prefix)))
+    (candidates (pen-autosuggest-candidates arg))
+    (require-match 'never)))
+
+;;;###autoload
+(define-minor-mode pen-autosuggest-mode
+  "Enable fish-like autosuggestions in penell.
+
+You can use <right> to select the suggestion. This is
+customizable through `pen-autosuggest-active-map'. If
+you prefer to use the default value of `company-active-map', you
+may set the variable
+`pen-autosuggest-use-company-map', though this isn't
+recommended as RET and TAB may not work as expected (send input,
+trigger completions, respectively) when there is an active
+suggestion.
+
+The delay defaults to 0 seconds to emulate fish shell's
+instantaneous suggestions, but is customizable with
+`pen-autosuggest-delay'.
+
+Note: This assumes you want to use something other than company
+for shell completion, e.g. `penell-pcomplete',
+`completion-at-point', or helm-pen-pcomplete, since
+`company-active-map', `company-backends', and `company-frontends'
+will be locally overriden and company will be used solely for
+history autosuggestions."
+  :init-value nil
+  :group 'pen-autosuggest
+  (if pen-autosuggest-mode
+      (progn
+        (company-mode 1)
+        (unless pen-autosuggest-use-company-map
+          (setq-local company-active-map pen-autosuggest-active-map))
+        (setq-local company-idle-delay pen-autosuggest-delay)
+        (setq-local company-backends '(pen-autosuggest))
+        (setq-local company-frontends '(company-preview-frontend)))
+    (company-mode -1)
+    (kill-local-variable 'company-active-map)
+    (kill-local-variable 'company-idle-delay)
+    (kill-local-variable 'company-backends)
+    (kill-local-variable 'company-frontends)))
+
+(provide 'pen-autosuggest)
+
+;;; pen-autosuggest.el ends here
