a746990f4fb7cd17e1b935e05232b21aead8991d
Shane Mulligan <mullikine@gmail.com>
2021-12-10 01:45:04 +1300

    Fri 10 Dec 2021 01:45:04

--- src/pen-minor-modes.el
+++ src/pen-minor-modes.el
@@ -1,3 +1,9 @@
-
+;; * TODO =continue-mode= should not be the default mode for prompts, but I should be able to enable it in emacs
+;; ** TODO Add a set of modes to emacs buffers
+;; Toggle them on, along with default variables.
+;; -
+;; Also, enable default variables to be evaluated.
+;; Then create pen-minor-modes.
+;; These minor modes should set defaults in prompt functions.
 
 (provide 'pen-minor-mode)
