88f4cf9c4dea657e47ce73a9a8b7fe0eb5e67cb2
Shane Mulligan <mullikine@gmail.com>
2021-12-10 20:22:04 +1300

    Fri 10 Dec 2021 20:22:04

--- src/pen-links.el
+++ src/pen-links.el
@@ -1,9 +1,11 @@
 (defun pen-go-to-prompt-function-definition (prompt-function-name-or-sym)
   (interactive (list (fz pen-prompt-functions nil nil "prompt function: ")))
 
-  (let ((prompt-fp))
-    (find-file )
-    (wgrep (concat "\\b" pattern "\\b") (mu "$EMACSD/config"))))
+  (if (sor prompt-function-name-or-sym)
+      (let* ((prompt-fn (str prompt-function-name-or-sym))
+             (prompt-fp)))
+    (find-file (concat pen-prompts-directory))
+    (wgrep (concat "\\b" pattern "\\b") (mu "$EMACSD/config")))))
 
 (org-add-link-type "prompt" 'pen-go-to-prompt-function-definition)
 
