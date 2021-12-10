0a3710a5044b455e2994aa8c7b7693a475a724cb
Shane Mulligan <mullikine@gmail.com>
2021-12-10 21:10:20 +1300

    Fri 10 Dec 2021 21:10:20

--- src/pen-links.el
+++ src/pen-links.el
@@ -4,11 +4,12 @@
   ;; I could, rather, consult the prompt function database
   (if (sor prompt-function-name-or-sym)
       (let* ((prompt-fn
-              (--> (str prompt-function-name-or-sym)
+              (->> (str prompt-function-name-or-sym)
                 (s-replace-regexp "\\.prompt$" "")
                 (s-replace-regexp "^\\(pf\\|pen-fn\\)-" "")
+                (s-replace-regexp "/" "-")
                 (string-replace "--" "-")
-                (s-replace-regexp "^\\(.*\\)/\\([0-9]*\\)" "\\1-\\2.prompt"))))
+                (s-replace-regexp "$" ".prompt"))))
         (find-file (f-join pen-prompts-directory "prompts" prompt-fn)))))
 
 (org-add-link-type "prompt" 'pen-go-to-prompt-function-definition)
