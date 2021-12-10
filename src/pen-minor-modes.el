494e4a87e51714559a11b6145dcd6e95dcae58d7
Shane Mulligan <mullikine@gmail.com>
2021-12-10 15:13:00 +1300

    Fri 10 Dec 2021 15:13:00

--- src/pen-minor-modes.el
+++ src/pen-minor-modes.el
@@ -6,4 +6,19 @@
 ;; Then create pen-minor-modes.
 ;; These minor modes should set defaults in prompt functions.
 
+
+(defvar pen-mydefaults-minor-mode-map (make-sparse-keymap)
+  "Keymap for `pen-mydefaults-minor-mode'.")
+
+;;;###autoload
+(define-minor-mode pen-mydefaults-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  ;; If init-value is not set to t, this mode does not get enabled in
+  ;; `fundamental-mode' buffers even after doing \"(global-pen-mydefaults-minor-mode 1)\".
+  ;; More info: http://emacs.stackexchange.com/q/16693/115
+  :init-value t
+  :lighter " pen-mydefaults-minor-mode"
+  :keymap pen-mydefaults-minor-mode-map)
+
+
 (provide 'pen-minor-modes)
