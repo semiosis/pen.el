a5f480d700c66c3038bfbe02dcf842720ede16e4
Shane Mulligan <mullikine@gmail.com>
2021-12-10 20:19:14 +1300

    Fri 10 Dec 2021 20:19:14

--- src/pen-links.el
+++ src/pen-links.el
@@ -1,8 +1,9 @@
-(defun ead-emacs-config (pattern)
-  (interactive (list (read-string-hist "ead-emacs-config: ")))
+(defun pen-go-to-prompt-function-definition (prompt-function-name-or-sym)
+  (interactive (list (fz pen-prompt-functions nil nil "prompt function: ")))
+
   (wgrep (concat "\\b" pattern "\\b") (mu "$EMACSD/config"))
   ;; (mu (sps (concat "cd $EMACSD/config; ead " (q pattern))))
   )
-(org-add-link-type "prompt" 'ead-emacs-config)
+(org-add-link-type "prompt" 'pen-go-to-prompt-function-definition)
 
 (provide 'pen-links)
