c0848d55c4fc278aca2ba9a50af7064d8ed57c58
Shane Mulligan <mullikine@gmail.com>
2021-12-10 20:29:48 +1300

    Fri 10 Dec 2021 20:29:48

--- src/pen-links.el
+++ src/pen-links.el
@@ -2,10 +2,8 @@
   (interactive (list (fz pen-prompt-functions nil nil "prompt function: ")))
 
   (if (sor prompt-function-name-or-sym)
-      (let* ((prompt-fn (str prompt-function-name-or-sym))
-             (prompt-fp)))
-    (find-file (concat pen-prompts-directory))
-    (wgrep (concat "\\b" pattern "\\b") (mu "$EMACSD/config")))))
+      (let* ((prompt-fn (s-replace-regexp "^\\(pf\\|pen-fn\\)-\\(.*\\)/\\([0-9]*\\)" "\\2-\\3.prompt" (str prompt-function-name-or-sym))))
+        (find-file (etv (f-join pen-prompts-directory "prompts" prompt-fn))))))
 
 (org-add-link-type "prompt" 'pen-go-to-prompt-function-definition)
 
