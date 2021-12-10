97b5baffec4433b984757093681b7034bebfa32d
Shane Mulligan <mullikine@gmail.com>
2021-12-10 20:30:59 +1300

    Fri 10 Dec 2021 20:30:59

--- src/pen-links.el
+++ src/pen-links.el
@@ -1,6 +1,7 @@
 (defun pen-go-to-prompt-function-definition (prompt-function-name-or-sym)
   (interactive (list (fz pen-prompt-functions nil nil "prompt function: ")))
 
+  ;; I could, rather, consult the prompt function database
   (if (sor prompt-function-name-or-sym)
       (let* ((prompt-fn
               (string-replace "--" "-"
