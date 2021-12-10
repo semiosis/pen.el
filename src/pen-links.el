4e163a3746b42bbcd0a26ef50cf26c8005d5e243
Shane Mulligan <mullikine@gmail.com>
2021-12-10 20:18:05 +1300

    Fri 10 Dec 2021 20:18:04

--- src/pen-links.el
+++ src/pen-links.el
@@ -3,6 +3,6 @@
   (wgrep (concat "\\b" pattern "\\b") (mu "$EMACSD/config"))
   ;; (mu (sps (concat "cd $EMACSD/config; ead " (q pattern))))
   )
-(org-add-link-type "lrk" 'ead-emacs-config)
+(org-add-link-type "prompt" 'ead-emacs-config)
 
 (provide 'pen-links)
