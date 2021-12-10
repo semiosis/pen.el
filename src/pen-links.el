182497797f5378eee36884fdeb5c91555d4a9666
Shane Mulligan <mullikine@gmail.com>
2021-12-10 20:30:43 +1300

    Fri 10 Dec 2021 20:30:43

--- src/pen-links.el
+++ src/pen-links.el
@@ -2,7 +2,9 @@
   (interactive (list (fz pen-prompt-functions nil nil "prompt function: ")))
 
   (if (sor prompt-function-name-or-sym)
-      (let* ((prompt-fn (s-replace-regexp "^\\(pf\\|pen-fn\\)-\\(.*\\)/\\([0-9]*\\)" "\\2-\\3.prompt" (str prompt-function-name-or-sym))))
+      (let* ((prompt-fn
+              (string-replace "--" "-"
+                              (s-replace-regexp "^\\(pf\\|pen-fn\\)-\\(.*\\)/\\([0-9]*\\)" "\\2-\\3.prompt" (str prompt-function-name-or-sym)))))
         (find-file (etv (f-join pen-prompts-directory "prompts" prompt-fn))))))
 
 (org-add-link-type "prompt" 'pen-go-to-prompt-function-definition)
