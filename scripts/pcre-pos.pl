023e60baa9c53aaa5704e1d75b7c252e03bc324f
Shane Mulligan <mullikine@gmail.com>
2021-12-15 12:59:00 +1300

    15.12.21

---
+++ scripts/pcre-pos.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+$r=$ARGV[0];
+shift;
+
+# my $quoted=quotemeta($r);
+
+$| = 1; # make the current file handle (stdout) hot. This has the same effect as disabling buffering
+# I CANT use unbuffer
+
+while(<>) {
+    my $line = $_;
+    # print $.,",",$+[0],"\n" if /a/;
+    print $. - 1," ",$-[0],"\n" if ($line =~ /$r/);
+}
