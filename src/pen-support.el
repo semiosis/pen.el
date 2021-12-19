7956297d929178aa6ad45adb82d5058c97b90708
Shane Mulligan <mullikine@gmail.com>
2021-12-19 13:45:39 +1300

    Sun 19 Dec 2021 13:45:39

--- src/pen-support.el
+++ src/pen-support.el
@@ -637,7 +637,8 @@ This also exports PEN_PROMPTS_DIR, so lm-complete knows where to find the .promp
 
                   (setq sel (completing-read prompt list nil must-match initial-input hist-var)))
 
-                ;; This refreshes the term usually
+                ;; This refreshes the term usually, but not always. It realigns up some REPLs, such as lein.
+                ;; Not worth it just for hhgttg 
                 (if (major-mode-p 'term-mode)
                     (run-with-timer 0.2 nil (lambda () (term-send-raw-string "\C-l"))))
                 sel)))
