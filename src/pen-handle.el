3ac09e816e46f01c3a7f171dae7244c705f6b74c
Shane Mulligan <mullikine@gmail.com>
2021-12-27 14:45:56 +1300

    Mon 27 Dec 2021 14:45:56

--- src/pen-handle.el
+++ src/pen-handle.el
@@ -66,4 +66,9 @@
         :docs '(pf-get-documentation-for-syntax-given-screen/2)
         :preverr '())
 
+(handle '(term-mode)
+        :nexterr '()
+        :docs '(pf-get-documentation-for-syntax-given-screen/2)
+        :preverr '())
+
 (provide 'pen-handle)
