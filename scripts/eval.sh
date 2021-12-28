1f25e6980c05878485d8afb97f2079679ddfceee
Shane Mulligan <mullikine@gmail.com>
2021-12-29 03:02:39 +1300

    Wed 29 Dec 2021 03:02:39

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -65,7 +65,7 @@ cmd1 unbuffer emacsclient -a "" -s ~/.emacs.d/server/$SOCKET -e "(pen-eval-for-h
 # unbuffer emacsclient -a "" -s ~/.emacs.d/server/$SOCKET -e "(pen-eval-for-host \"$SOCKET\" $last_arg)" &>/dev/null
 # These hang sometimes. I want to know why.
 
-# Fix the frame
+# Fix the frame. This works, but it's a dodgy hack
 tmux neww -d pen-x -sh "emacsclient -t -a '' -s $HOME/.emacs.d/server/$SOCKET" -e UUU -c g -i
 sleep 0.1
 tmux neww -d emacsclient -t -a "" -s $HOME/.emacs.d/server/$SOCKET -e "(progn (pen-eval-for-host \"$fp\" $last_arg)(kill-frame))"
