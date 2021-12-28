a23ee7fa3d2316b0c837160e433577a52f746a9e
Shane Mulligan <mullikine@gmail.com>
2021-12-28 18:16:23 +1300

    28.12.21

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -39,11 +39,27 @@ export PATH=$PATH:$EMACSD/host/pen.el/scripts:$EMACSD/pen.el/scripts
 last_arg="${@: -1}"
 last_arg="$(p "$last_arg" | pen-bs '\\')"
 
+if test "$USE_POOL" = "y"; then
+    # Just take the first one
+    for socket_fp in ~/.pen/pool/available/*; do
+        SOCKET="$(basename "$socket_fp")"
+        break
+    done
+
+    rm -f ~/.pen/pool/available/$SOCKET
+fi
+
+echo "$SOCKET"
+
 fp="/tmp/eval-output-${SOCKET}.txt"
 rm -f "$fp"
 # Can't use cmd because elisp doesn't use single quote for strings
 unbuffer emacsclient -a "" -s ~/.emacs.d/server/$SOCKET -e "(pen-eval-for-host \"$SOCKET\" $last_arg)" &>/dev/null
 sleep 0.1
+
+if test "$USE_POOL" = "y"; then
+    touch ~/.pen/pool/available/$SOCKET
+fi
 
 # I need to hide the fact that it failed. Otherwise, I can't cancel comint commands without polluting the repl
 cat "$fp" 2>/dev/null
