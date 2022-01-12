380215240f2519efedc160deb0e83c9a01c40e45
Shane Mulligan <mullikine@gmail.com>
2022-01-12 21:42:56 +1300

    Wed 12 Jan 2022 21:42:56

--- src/pen-channel.el
+++ src/pen-channel.el
@@ -1,3 +1,23 @@
+(defun apostrophe-start-chatbot-from-name (name &optional auto)
+  (interactive (list (read-string-hist "person: ")))
 
+  (if (and (not (pen-inside-docker))
+           (not (pen-container-running)))
+      (progn
+        (pen-term-nsfa (cmd "pen" "-n"))
+        (message "Starting Pen server")))
+
+  (if (not name)
+      (setq name "Marco Polo"))
+
+  (if auto
+      (let* ((blurb (car (pen-one (pf-generate-wiki-blurb-for-a-famous-person/1 name :no-select-result t)))))
+
+        (let* ((el (pen-snc (pen-cmd "apostrophe-repl" "-getcomintcmd" name "" blurb))))
+          (pen-e-sps (pen-lm (pen-eval-string el)))))
+    (let* ((blurb (pf-generate-wiki-blurb-for-a-famous-person/1 name)))
+
+      (let* ((el (pen-snc (pen-cmd "apostrophe-repl" "-getcomintcmd" name "" blurb))))
+        (pen-e-sps (pen-lm (pen-eval-string el)))))))
 
 (provide 'pen-channel)
