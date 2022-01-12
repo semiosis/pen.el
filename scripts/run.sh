41570159f8ae8ee1459ac64315d079dee257b36b
Shane Mulligan <mullikine@gmail.com>
2022-01-13 00:32:24 +1300

    Thu 13 Jan 2022 00:32:24

--- scripts/run.sh
+++ scripts/run.sh
@@ -56,7 +56,7 @@ export LD_LIBRARY_PATH=/root/libwebsockets/build/lib:$LD_LIBRARY_PATH
 # Must quit all emacs daemons and relinquish their reservations before ever checking available pool
 # run.sh *should* only happen when starting Pen for the first time, except for when the first argument is a file.
 if ! test "$NO_QUIT_DAEMONS" = y; then
-    pen-e qa
+    unbuffer pen-e qa &>/dev/null
 fi
 
 # This is a hack to run only on the initial docker run
