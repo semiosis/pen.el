834d9818e335a2e71f08975f9e0c34d2c4dd59af
Shane Mulligan <mullikine@gmail.com>
2022-01-13 14:23:59 +1300

    Thu 13 Jan 2022 14:23:59

--- src/pen-channel.el
+++ src/pen-channel.el
@@ -1,3 +1,5 @@
+;; I suppose that `chann`, being the mantissa of chann.el is the unique name identifying Chann.el
+
 (defun channel-chatbot-from-name (name command &optional auto)
   "`name` is the name of the personality.
 `command` is the terminal command the personality commands.
@@ -18,9 +20,11 @@
               (car (pen-one (pf-generate-wiki-blurb-for-a-famous-person/1 name :no-select-result t)))
             ;; Select from possible blurbs, then do a final human edit with a different emacs daemon
             (pen-eipec
-             (fz (pf-generate-wiki-blurb-for-a-famous-person/1 name :no-select-result nil))))))
+             (fz (pf-generate-wiki-blurb-for-a-famous-person/1 name :no-select-result nil)))))
+         (bufname (concat "chann")))
 
     ;; TODO Start a cterm with the channeled chatbot running as a program loop inside of that buffer
+    (pen-term (pen-nsfa command) t bufname)
     (let* ((el (pen-snc (pen-cmd "channel-repl" "-getcomintcmd" name "" blurb))))
       (pen-e-sps (pen-lm (pen-eval-string el))))))
 
