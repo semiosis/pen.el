4d9bad243c196e60816113e3d81e49b46dfe5414
Shane Mulligan <mullikine@gmail.com>
2022-01-13 19:59:53 +1300

    Thu 13 Jan 2022 19:59:53

--- src/pen-tty.el
+++ src/pen-tty.el
@@ -51,6 +51,7 @@
 (define-key input-decode-map (kbd "<f24>") (kbd "<S-f12>"))
 
 ;; Why doen't these exist?
+;; I think I needed my new infocomp definitions
 (define-key key-translation-map (kbd "<f22>") (kbd "<S-f10>"))
 (define-key key-translation-map (kbd "<f23>") (kbd "<S-f11>"))
 (define-key key-translation-map (kbd "<f24>") (kbd "<S-f12>"))
