dc8a515972082427e747de10503d161d7db237ea
Shane Mulligan <mullikine@gmail.com>
2022-01-13 16:22:41 +1300

    Thu 13 Jan 2022 16:22:41

--- scripts/newframe.sh
+++ scripts/newframe.sh
@@ -77,6 +77,7 @@ if test "$USE_POOL" = "y"; then
         if test -n "$SOCKET"; then
             break
         fi
+        echo
         echo "$(date-ts-hr)"
         echo "Waiting for socket..." 1>&2
         sleep 1
