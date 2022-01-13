3ea241d55ad0d0a3a95e4430d80ea7bd24dc17ad
Shane Mulligan <mullikine@gmail.com>
2022-01-13 14:10:51 +1300

    Thu 13 Jan 2022 14:10:51

--- src/pen-channel.el
+++ src/pen-channel.el
@@ -13,6 +13,15 @@
   (if (not name)
       (setq name "Marco Polo"))
 
+  (let* ((blurb
+          (if auto
+              (car (pen-one (pf-generate-wiki-blurb-for-a-famous-person/1 name :no-select-result t)))
+            ;; TODO Then a final human edit with a different emacs daemon
+            (fz (pf-generate-wiki-blurb-for-a-famous-person/1 name :no-select-result nil)))))
+
+    (let* ((el (pen-snc (pen-cmd "channel-repl" "-getcomintcmd" name "" blurb))))
+      (pen-e-sps (pen-lm (pen-eval-string el)))))
+
   (if auto
       (let* ((blurb (car (pen-one (pf-generate-wiki-blurb-for-a-famous-person/1 name :no-select-result t)))))
 
