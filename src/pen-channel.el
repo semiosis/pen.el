8416046bd7dde8ffc593228a96da56550d342c27
Shane Mulligan <mullikine@gmail.com>
2022-01-13 14:42:36 +1300

    Thu 13 Jan 2022 14:42:36

--- src/pen-channel.el
+++ src/pen-channel.el
@@ -1,7 +1,7 @@
-;; I suppose that `chann`, being the mantissa of chann.el is the unique name identifying Chann.el
+;; I suppose that `chann`, being the mantissa of chann.el is the unique name-or-names identifying Chann.el
 
-(defun channel-chatbot-from-name (name command &optional auto)
-  "`name` is the name of the personality.
+(defun channel-chatbot-from-name (name-or-names command &optional auto)
+  "`name-or-names` is the name-or-names of the personalit(y|ies).
 `command` is the terminal command the personality commands.
 `auto`, if set to `t` will come up with the personality blurb without human interaction."
   (interactive (list (read-string-hist "personalit(y|ies): ")))
@@ -12,15 +12,15 @@
         (pen-term-nsfa (pen-cmd "pen" "-n"))
         (message "Starting Pen server")))
 
-  (if (not name)
-      (setq name "The Mad Tea-Pa"))
+  (if (not name-or-names)
+      (setq name-or-names "The March Hare, the Hatter and the Dormouse"))
 
   (let* ((blurb
           (if auto
-              (car (pen-one (pf-generate-wiki-blurb-for-a-famous-person/1 name :no-select-result t)))
+              (car (pen-one (pf-generate-wiki-blurb-for-a-famous-person/1 name-or-names :no-select-result t)))
             ;; Select from possible blurbs, then do a final human edit with a different emacs daemon
             (pen-eipec
-             (fz (pf-generate-wiki-blurb-for-a-famous-person/1 name :no-select-result nil)))))
+             (fz (pf-generate-wiki-blurb-for-a-famous-person/1 name-or-names :no-select-result nil)))))
          (slug (slugify command nil 30))
          (bufname (concat "chann-" slug))
          (buf
@@ -28,7 +28,7 @@
           (pen-term (pen-nsfa command) t bufname)))
 
     ;; TODO Start a cterm with the channeled chatbot running as a program loop inside of that buffer
-    (let* ((el (pen-snc (pen-cmd "channel-repl" "-getcomintcmd" name "" blurb))))
+    (let* ((el (pen-snc (pen-cmd "channel-repl" "-getcomintcmd" name-or-names "" blurb))))
       (pen-e-sps (pen-lm (pen-eval-string el))))))
 
 (provide 'pen-channel)
