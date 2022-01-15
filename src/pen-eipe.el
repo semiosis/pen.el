e95e87133ca53e5cfab18d4684f7d16daff494be
Shane Mulligan <mullikine@gmail.com>
2022-01-15 22:42:26 +1300

    Sat 15 Jan 2022 22:42:26

--- src/pen-eipe.el
+++ src/pen-eipe.el
@@ -19,8 +19,22 @@
   (ignore-errors
     (put-text-property (point-min) end 'face 'pen-read-only)
     (put-text-property (point-min) end 'read-only t)
+    (put-text-property (point-min) end 'pen-eipe-prompt t)
     (try (goto-char (+ 1 end))
          (goto-char end)
          nil)))
+
+(defun pen-find-file-read-only-context ()
+  (if (f-exists-p (concat "~/.pen/eipe_" (pen-daemon-name) "_prompt"))))
+
+(defset pen-eipe-hook '())
+
+(add-hook 'pen-eipe-hook 'pen-find-file-read-only-context)
+
+(defun run-eipe-hooks ()
+  (interactive)
+  (run-hooks 'pen-eipe-hook))
+
+(add-hook 'find-file-hooks 'run-eipe-hooks t)
 
 (provide 'pen-eipe)
