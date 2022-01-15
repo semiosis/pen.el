14d6cd756cef3c03bf2f3774f83328a6b9ae24ed
Shane Mulligan <mullikine@gmail.com>
2022-01-15 21:27:07 +1300

    Sat 15 Jan 2022 21:27:07

--- src/pen-eipe.el
+++ src/pen-eipe.el
@@ -1,3 +1,8 @@
 ;; This is for creating a read-only area at the top of human input buffers
+;; - used to place the prompt
+;; - used to tell the user what they are doing
+
+(defun pen-eipe-set-prompt-ro (end)
+  (put-text-property (point-min) end 'read-only t))
 
 (provide 'pen-eipe)
