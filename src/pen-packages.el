5601a7799e04196c72c822869e319f87445f58df
Shane Mulligan <mullikine@gmail.com>
2022-01-17 15:28:39 +1300

    Mon 17 Jan 2022 15:28:39

--- src/pen-packages.el
+++ src/pen-packages.el
@@ -1,24 +1,45 @@
 (package-initialize)
 
+(defun pen-slurp-file (f)
+  (with-temp-buffer
+    (insert-file-contents f)
+    (buffer-substring-no-properties
+     (point-min)
+     (point-max))))
+
+(defun pen-load-list-file (f)
+  "split-string splits the file into a list of strings. mapcar intern turns the list of strings into a list of symbols"
+  (mapcar 'intern
+          (split-string
+           (pen-slurp-file f) "\n" t)))
+
 ;; Auto install package are installed if they are missing
-(setq package-list
-      (append
-       '(
-         markdown-mode
-          )
-       (my/load-list-file
-        "$HOME/var/smulliga/source/git/config/emacs/packages.txt")))
 
-;; This one requires emacs25
-;; lsp-javascript-typescript
+(defun pen-auto-load-packages ()
+  (interactive)
 
-;; fetch the list of packages available
-(unless package-archive-contents
-  (package-refresh-contents))
+  (let ((pl "~/.pen/emacs-packages.txt"))
+    (if (f-exists-p pen-auto-load-packages)
+        (setq package-list
+              (-uniq
+               (append
+                package-list
+                '(markdown-mode)
+                (pen-load-list-file
+                 pl))))
 
-; install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (yes (ignore-errors (package-install package)))))
+      ;; This one requires emacs25
+      ;; lsp-javascript-typescript
+
+      ;; fetch the list of packages available
+      (unless package-archive-contents
+        (package-refresh-contents))
+
+      ;; install the missing packages
+      (dolist (package package-list)
+        (unless (package-installed-p package)
+          (yes (ignore-errors (package-install package))))))))
+
+(pen-auto-load-packages)
 
 (provide 'pen-packages)
