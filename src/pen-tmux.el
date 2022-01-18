4ba3cce0d759fc05045bd406ca12fee5f1a4283c
Shane Mulligan <mullikine@gmail.com>
2022-01-18 17:03:34 +1300

    Tue 18 Jan 2022 17:03:34

--- src/pen-tmux.el
+++ src/pen-tmux.el
@@ -14,35 +14,35 @@ START and END can be in either order."
 
 ;; emacs term and window functions (for avoiding tmux, say in the gui)
 
-(defun e/nw (&optional run)
+(defun pen-e-nw (&optional run)
   (interactive)
   (if run
       (call-interactively run)))
-(defalias 'enw 'e/nw)
+(defalias 'enw 'pen-e-nw)
 
-(defun e/sps (&optional run)
+(defun pen-e-sps (&optional run)
   (interactive)
   (split-window-sensibly)
   (other-window 1)
   (if run
       (call-interactively run)))
-(defalias 'esps 'e/sps)
+(defalias 'esps 'pen-e-sps)
 
-(defun e/spv (&optional run)
+(defun pen-e-spv (&optional run)
   (interactive)
   (split-window-horizontally)
   (other-window 1)
   (if run
       (call-interactively run)))
-(defalias 'espv 'e/spv)
+(defalias 'espv 'pen-e-spv)
 
-(defun e/sph (&optional run)
+(defun pen-e-sph (&optional run)
   (interactive)
   (split-window-vertically)
   (other-window 1)
   (if run
       (call-interactively run)))
-(defalias 'esph 'e/sph)
+(defalias 'esph 'pen-e-sph)
 
 ;; tmux scripts
 
@@ -78,7 +78,7 @@ START and END can be in either order."
       (setq cmd (tmuxify-cmd "zsh"))
     ;; (setq cmd "TMUX= tmux new -n zsh \"CWD= zsh\"")
     )
-  (e/sph (pen-lm (pen-term-nsfa cmd nil "zsh" nil nil dir))))
+  (pen-e-sph (pen-lm (pen-term-nsfa cmd nil "zsh" nil nil dir))))
 
 ;; (defalias 'sph-term 'pen-e-sph-zsh)
 ;; (defalias 'term-sph 'pen-e-sph-zsh)
@@ -90,7 +90,7 @@ START and END can be in either order."
       (setq dir (cwd)))
   (if (not cmd)
       (setq cmd "TMUX= tmux new -n zsh \"CWD= zsh\""))
-  (e/spv (pen-lm (pen-term-nsfa cmd nil "zsh" nil nil dir))))
+  (pen-e-spv (pen-lm (pen-term-nsfa cmd nil "zsh" nil nil dir))))
 
 ;; (defalias 'term-spv 'pen-e-spv-zsh)
 ;; (defalias 'tspv 'pen-e-spv-zsh)
@@ -108,6 +108,50 @@ START and END can be in either order."
 (defalias 'term-sps 'pen-e-sps-zsh)
 (defalias 'tsps 'pen-e-sps-zsh)
 
+(defun pen-e-nw (&optional run)
+  (interactive)
+  (if run
+      (call-interactively run)))
+
+(defun pen-e-sps (&optional run)
+  (interactive)
+  (split-window-sensibly)
+  (other-window 1)
+  (if run
+      (call-interactively run)))
+
+(defun pen-e-spv (&optional run)
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1)
+  (if run
+      (call-interactively run)))
+
+(defun pen-e-sph (&optional run)
+  (interactive)
+  (split-window-vertically)
+  (other-window 1)
+  (if run
+      (call-interactively run)))
+
+(defun pen-e-nw-zsh (&optional cmd window-type dir)
+  (interactive)
+  (if (not dir)
+      (setq dir (cwd)))
+  (if (not cmd)
+      (progn
+        ;; (setq cmd (concat "TMUX= tmux new -c " (pen-q dir) " -n zsh \"CWD= zsh\""))
+        (setq cmd "zsh")
+        (setq cmd (tmuxify-cmd cmd dir cmd))))
+  (if (not (sor window-type))
+      (setq window-type "nw"))
+  (cond
+   ((string-equal "nw" window-type) (pen-e-nw (pen-lm (pen-term-nsfa cmd nil "zsh" nil nil dir))))
+   ((string-equal "sps" window-type) (pen-e-sps (pen-lm (pen-term-nsfa cmd nil "zsh" nil nil dir))))
+   ((string-equal "spv" window-type) (pen-e-spv (pen-lm (pen-term-nsfa cmd nil "zsh" nil nil dir))))
+   ((string-equal "sph" window-type) (pen-e-sph (pen-lm (pen-term-nsfa cmd nil "zsh" nil nil dir))))
+   (t (pen-e-nw (pen-lm (pen-term-nsfa cmd nil "zsh" nil nil dir))))))
+
 (defun pen-tm-nw (&optional cmd window-type nw_args input dir)
   "Runs command in a new window/pane"
   (interactive)
@@ -118,7 +162,7 @@ START and END can be in either order."
   (if input
       (pen-sn (concat "pen-tm -tout -S " window-type " " nw_args " " (pen-q cmd) " &") input (or dir (get-dir)))
     (if (display-graphic-p)
-        (pen-e-sps-zsh cmd)
+        (pen-e-nw-zsh cmd window-type)
       (progn
         (if (and (variable-p 'sh-update)
                  (eval 'sh-update))
@@ -126,7 +170,7 @@ START and END can be in either order."
         (let ((cmd-tm-split (concat "unbuffer pen-tm -f -d -te " window-type " " nw_args " -c " (pen-q (or dir (get-dir))) " " (pen-q cmd) " &"))
               (cmd-tm-here (concat "pen-tm ns -np -s -c " (pen-q (or dir (get-dir))) " " (pen-q cmd))))
           (if (>= (prefix-numeric-value current-prefix-arg) 4)
-              (pen-e-sps-zsh cmd-tm-here)
+              (pen-e-nw-zsh cmd-tm-here window-type)
             (pen-snc cmd-tm-split)))))))
 
 (defun pen-nw (&optional cmd nw_args input dir)
