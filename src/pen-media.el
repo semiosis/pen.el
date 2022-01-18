6f97bd056dded8d6b74b5eeea0f344e488394c16
Shane Mulligan <mullikine@gmail.com>
2022-01-18 21:34:55 +1300

    Tue 18 Jan 2022 21:34:55

--- src/pen-media.el
+++ src/pen-media.el
@@ -11,14 +11,14 @@
 ;; (syms2str music-extensions)
 ;; (list2str music-extensions)
 
-(defun kill-music ()
+(defun pen-kill-music ()
   (interactive)
   (ignore-errors (vuiet-stop))
-  (pen-sn "kill-music")
+  (pen-sn "pen-kill-music")
   (pen-sn "killall vlc"))
 
-(cl-defun play-video-path (fp &optional window-function external-cmd &key loop)
-  (kill-music)
+(cl-defun pen-play-video-path (fp &optional window-function external-cmd &key loop)
+  (pen-kill-music)
   (if (not window-function)
       (setq window-function 'sps))
   (if (not (sor external-cmd))
@@ -29,16 +29,16 @@
     (let ((use-tty-str (if use-tty "export USETTY=y; " "")))
       (eval `(,window-function (concat use-tty-str external-cmd " " (pen-q fp)) "-d")))))
 
-(cl-defun play-youtube-url (url &optional window-function &key loop)
-  (play-video-path url window-function "yt -v" :loop loop))
+(cl-defun pen-play-youtube-url (url &optional window-function &key loop)
+  (pen-play-video-path url window-function "yt -v" :loop loop))
 
-(cl-defun play-movie (path &optional term-and-transcript use-tty &key loop)
+(cl-defun pen-play-movie (path &optional term-and-transcript use-tty &key loop)
   (setq path (pen-umn path))
-  (kill-music)
+  (pen-kill-music)
   (let ((use-tty-str (if use-tty "export USETTY=y; " ""))
         (play-function (if (string-match "\\bhttp" path)
-                           'play-youtube-url
-                         'play-video-path)))
+                           'pen-play-youtube-url
+                         'pen-play-video-path)))
     (if term-and-transcript
         (progn
           (eval `(,play-function path 'spv :loop ,loop))
@@ -47,31 +47,31 @@
       (eval `(,play-function path))))
   nil)
 (cl-defun pm (path &optional term-and-transcript &key loop)
-  (play-movie path term-and-transcript t :loop t))
-(defalias 'play-video 'play-movie)
+  (pen-play-movie path term-and-transcript t :loop t))
+(defalias 'play-video 'pen-play-movie)
 
 (defun play-movie-with-transcript (path)
   (interactive (list (read-string-hist "youtube url: ")))
-  (play-movie path t))
+  (pen-play-movie path t))
 (defalias 'pmt 'play-movie-with-transcript)
 
-(defun play-song (path &optional loop)
+(defun pen-play-song (path &optional loop)
   (setq path (pen-umn path))
 
   (if loop
-      (shut-up (eval `(pen-sn (cmd "play-song" "-l" ,path)
+      (shut-up (eval `(pen-sn (cmd "pen-play-song" "-l" ,path)
                               nil nil nil t)))
-    (shut-up (eval `(pen-sn (cmd "play-song" ,path)
+    (shut-up (eval `(pen-sn (cmd "pen-play-song" ,path)
                             nil nil nil t))))
   nil)
-(defalias 'ps 'play-song)
+(defalias 'ps 'pen-play-song)
 
-(defun play-playlist (path)
+(defun pen-play-playlist (path)
   (shut-up (eval ` (pen-snc (cmd "play-yt-playlist" ,path)
                             nil nil nil t)))
   nil)
 
-(defun get-yt-playlist (path)
+(defun pen-get-yt-playlist (path)
   (interactive (list (read-string "path:")))
 
   (if (string-empty-p path) (setq path "[[https://www.youtube.com/playlist?list=PLGYGe2PKknX2kydiv28aq8dBXBWeJfxgg][The Lion King 2019 soundtrack - YouTube]]"))
@@ -91,62 +91,44 @@
         (new-buffer-from-string result)
       result)))
 
-(defun org-clink-urls (ms)
+(defun pen-org-clink-urls (ms)
   (interactive)
 
   (pen-sn "oc" ms))
 
-(defalias 'oc 'org-clink-urls)
-
-(defun lion-king-2019 ()
-  (interactive)
-  (play-playlist "[[https://www.youtube.com/playlist?list=PLGYGe2PKknX2kydiv28aq8dBXBWeJfxgg][The Lion King 2019 soundtrack - YouTube]]"))
-
-(defun daft-punk-discovery ()
-  (interactive)
-  (play-playlist "[[https://www.youtube.com/playlist?list=OLAK5uy_lMA_iEf3aqk5YSDsnrPKojXegOiecSF94][Discovery - YouTube]]"))
+(defalias 'oc 'pen-org-clink-urls)
 
 (defun swordfish-ost ()
   (interactive)
-  (play-playlist "[[https://www.youtube.com/playlist?list=OLAK5uy_mPdRDb_IJmG3DDeOlSwI5_rKKtAEZogys][Swordfish The Album (Original Motion Picture Soundtrack) - YouTube]]"))
-
-;; (get-yt-playlist "[[https://www.youtube.com/playlist?list=OLAK5uy_mPdRDb_IJmG3DDeOlSwI5_rKKtAEZogys][Swordfish The Album (Original Motion Picture Soundtrack) - YouTube]]")
-
-(defun brother-bear ()
-  (interactive)
-  (play-playlist "[[https://www.youtube.com/playlist?list=PLA8VHLKYzqTsKkpZ1SeJ1HmcGH6qHZTlt][Brother Bear OST - YouTube]]"))
+  (pen-play-playlist "[[https://www.youtube.com/playlist?list=OLAK5uy_mPdRDb_IJmG3DDeOlSwI5_rKKtAEZogys][Swordfish The Album (Original Motion Picture Soundtrack) - YouTube]]"))
 
 (defun clubbed-to-death ()
   (interactive)
-  (play-playlist "[[https://www.youtube.com/watch?v=pFS4zYWxzNA][clubbed to death - Matrix soundtrack - YouTube]]"))
+  (pen-play-playlist "[[https://www.youtube.com/watch?v=pFS4zYWxzNA][clubbed to death - Matrix soundtrack - YouTube]]"))
 
 (defun furious-angels ()
   (interactive)
-  ;; (bld play-song "$DUMP$NOTES/ws/music/furious-angels/Rob Dougan - Furious Angels-jtAmFKaThNE.m4a")
-  (bld play-song "[[https://www.youtube.com/watch?v=PJ8vJ2Qjuuc][Furious Angels - Rob Dougan - YouTube]]"))
+  ;; (bld pen-play-song "$DUMP$NOTES/ws/music/furious-angels/Rob Dougan - Furious Angels-jtAmFKaThNE.m4a")
+  (bld pen-play-song "[[https://www.youtube.com/watch?v=PJ8vJ2Qjuuc][Furious Angels - Rob Dougan - YouTube]]"))
 
-(defun mus-epic1 ()
-  (interactive)
-  (bld play-song "[[https://www.youtube.com/watch?v=8oXo_QsxtDM][The Best of Epic Music August 2018 | Epic Powerful & Heroic Music Mix - YouTube]]"))
-
-(defun ytsearch (query)
+(defun pen-ytsearch (query)
   (if (string-match-p "\\bhttps?:" query)
       (setq query (chomp (sh/xurls query)))
     (pen-cl-sn (concat "yt-search " (pen-q query)) :chomp t)))
 
-(defun search-play-yt (query-or-url &optional audioonly)
+(defun pen-search-play-yt (query-or-url &optional audioonly)
   (interactive (list (read-string-hist "yt play-song: " (my/selected-text))))
 
-  (kill-music)
+  (pen-kill-music)
   (if (string-match-p "\\bhttps?:" query-or-url)
       (setq query-or-url (chomp (sh/xurls query-or-url)))
-    (setq query-or-url (ytsearch query-or-url)))
+    (setq query-or-url (pen-ytsearch query-or-url)))
 
   (if audioonly
-      (my/shut-up (pen-cl-sn (concat "play-song " (pen-q query-or-url)) :detach t :chomp t))
+      (shut-up (pen-cl-sn (concat "pen-play-song " (pen-q query-or-url)) :detach t :chomp t))
     (pen-sps (concat "yt -tty -v " (pen-q query-or-url)) "-d")))
 
-(defalias 'yt 'search-play-yt)
+(defalias 'yt 'pen-search-play-yt)
 
 (defun yta (query-or-url)
   (interactive (list (read-string-hist "yt play-song: ")))
@@ -157,10 +139,10 @@
 (defun search-play-yt-transcript (query-or-url &optional lang)
   (interactive (list (read-string-hist "youtube query-or-url: " (my/selected-text))))
 
-  (kill-music)
+  (pen-kill-music)
   (if (string-match-p "\\bhttps?:" query-or-url)
       (setq query-or-url (chomp (sh/xurls query-or-url)))
-    (setq query-or-url (ytsearch query-or-url)))
+    (setq query-or-url (pen-ytsearch query-or-url)))
 
   (if (not lang)
       (setq lang "en"))
@@ -187,7 +169,7 @@
 
   (if (and (not (string-match "^http" url))
            (sor url))
-      (setq url (ytsearch url)))
+      (setq url (pen-ytsearch url)))
 
   (setq do-etv
         (or
@@ -211,41 +193,41 @@
 (defun mus-epic ()
   (interactive)
   "Search for epic music and play it."
-  (search-play-yt "epic music"))
+  (pen-search-play-yt "epic music"))
 
 (defun elements-of-life ()
   (interactive)
-  (play-song "[[https://www.youtube.com/watch?v=V9HKtPVms_Y][Tiesto elements of Life - YouTube]]"))
+  (pen-play-song "[[https://www.youtube.com/watch?v=V9HKtPVms_Y][Tiesto elements of Life - YouTube]]"))
 
 (defun prince-of-egypt-reprise ()
   (interactive)
-  (play-song "https://www.youtube.com/watch?v=E1rEmAQJ7EQ&t=415"))
+  (pen-play-song "https://www.youtube.com/watch?v=E1rEmAQJ7EQ&t=415"))
 
 (defun neverending-story ()
   (interactive)
-  (bld play-song "[[https://www.youtube.com/watch?v=heHdOTt_iGc&t][The Neverending Story (1984)  Soundtrack  - YouTube]]"))
+  (bld pen-play-song "[[https://www.youtube.com/watch?v=heHdOTt_iGc&t][The Neverending Story (1984)  Soundtrack  - YouTube]]"))
 
 (defun dark-knight-end-credits ()
   (interactive)
-  (bld play-song "[[https://www.youtube.com/watch?v=fTr89ENLZPc][The Dark Knight - End Credits Music (HQ) - YouTube]]"))
+  (bld pen-play-song "[[https://www.youtube.com/watch?v=fTr89ENLZPc][The Dark Knight - End Credits Music (HQ) - YouTube]]"))
 
 (defun dark-knight-theme ()
   (interactive)
-  (bld play-song "[[https://www.youtube.com/watch?v=w1B3Mgklfd0][Batman The Dark Knight Theme - Hans Zimmer - YouTube]]"))
+  (bld pen-play-song "[[https://www.youtube.com/watch?v=w1B3Mgklfd0][Batman The Dark Knight Theme - Hans Zimmer - YouTube]]"))
 
 (defun dark-knight-watchful-guardian ()
   "5 / 5"
   (interactive)
-  (bld play-song "[[https://www.youtube.com/watch?v=buejiFXN7Hw][Hans Zimmer - A Watchful Guardian The Dark Knight - YouTube]]"))
+  (bld pen-play-song "[[https://www.youtube.com/watch?v=buejiFXN7Hw][Hans Zimmer - A Watchful Guardian The Dark Knight - YouTube]]"))
 
 (defun william-tell-overture ()
   (interactive)
-  (bld play-song "[[https://www.youtube.com/watch?v=xoBE69wdSkQ][ROSSINI: William Tell Overture (full version) - YouTube]]"))
+  (bld pen-play-song "[[https://www.youtube.com/watch?v=xoBE69wdSkQ][ROSSINI: William Tell Overture (full version) - YouTube]]"))
 (defalias 'wto 'william-tell-overture)
 
 (defun real-estate-its-real ()
   (interactive)
-  (play-song "[[https://youtu.be/4HWcViTXdYc][Real Estate - Its Real (Official Video) - YouTube]]"))
+  (pen-play-song "[[https://youtu.be/4HWcViTXdYc][Real Estate - Its Real (Official Video) - YouTube]]"))
 
 (defun pen-media-test ()
   (interactive)
