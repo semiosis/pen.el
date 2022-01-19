19336b1724baaef9e2e87be94f3aa45eeb74993c
Shane Mulligan <mullikine@gmail.com>
2022-01-19 14:13:45 +1300

    Wed 19 Jan 2022 14:13:45

--- src/pen-pensieve.el
+++ src/pen-pensieve.el
@@ -1,6 +1,7 @@
 ;; What you are looking at are memories.
 ;; In this case pertaining to one individual...
 ;; This file contains the most particular memory...
+;; I'd like you to see it, if you would.
 
 ;; http://github.com/semiosis/pensieve
 
