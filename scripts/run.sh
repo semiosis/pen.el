405e6431aef7fe225e170e40daad301372ecb08e
Shane Mulligan <mullikine@gmail.com>
2022-01-21 10:45:06 +1300

    1642715106

--- scripts/run.sh
+++ scripts/run.sh
@@ -68,8 +68,7 @@ if ! ls ~/.pen/pool/available/* 2>/dev/null | grep -q pen-emacsd; then
 export PEN_USE_GUI=n
 echo "ttyd running on port 7681, serving Pen.el on http"
 ttyd -p 7681 bash -l /root/.emacs.d/pen.el/scripts/newframe.sh &>/dev/null &
-
-/inspircd-2.0.25/run/inspircd start
+/inspircd-2.0.25/run/inspircd start &>/dev/null
 )
 fi
 
