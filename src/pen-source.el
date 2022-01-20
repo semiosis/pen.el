a7f24176c9119acc3d340f164db6a5eee0065930
Shane Mulligan <mullikine@gmail.com>
2022-01-21 10:19:26 +1300

    Fri 21 Jan 2022 10:19:26

--- src/pen-source.el
+++ src/pen-source.el
@@ -1,10 +1,11 @@
 (defun pen-reload-config-file ()
   "Fuzzy selects a selects file to be loaded."
   (interactive)
-  (let ((r (umn (fz (sh-notty "list-emacs-config-files.sh") nil nil "reload config: "))))
-    (if (not (empty-string-p r))
+  (let ((r (pen-umn (fz (pen-sn "list-emacs-config-files.sh") nil nil "reload config: "))))
+    (if (not (s-blank? r))
         (load r))))
 
-(define-key global-map (kbd "M-l M-p M-r") 'pen-reload-config-file)
+(if (pen-snq "inside-docker-p")
+    (define-key pen-map (kbd "M-l M-p M-r") 'pen-reload-config-file))
 
 (provide 'pen-source)
