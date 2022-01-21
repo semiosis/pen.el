dc31567b8edf271bd964983c4258facdbb1c2113
Shane Mulligan <mullikine@gmail.com>
2022-01-21 15:29:41 +1300

    Fri 21 Jan 2022 15:29:40

--- src/pen-mad-teaparty.el
+++ src/pen-mad-teaparty.el
@@ -1,5 +1,5 @@
 (defun pen-mtp-connect-with-name (name)
   (interactive (list (read-string-hist "Chatbot name")))
-  (pen-sps (pen-cmd "mtp") name))
+  (pen-sps (pen-cmd "mtp" name)))
 
 (provide 'pen-mad-teaparty)
