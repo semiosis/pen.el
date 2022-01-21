449c6490294d4b51b5060cd7897eaab1c4c912b6
Shane Mulligan <mullikine@gmail.com>
2022-01-21 14:39:38 +1300

    Fri 21 Jan 2022 14:39:38

--- src/pen-menu-bar.el
+++ src/pen-menu-bar.el
@@ -229,6 +229,9 @@
     (bindings--define-key menu [mi-pen-start-gui-web-browser]
       '(menu-item "Start Pen.el in a GUI web browser" pen-start-gui-web-browser
                   :help "Start Pen.el in a GUI web browser"))
+    (bindings--define-key menu [mi-pen-of-imagination]
+      '(menu-item "The pen of imagination" pen-of-imagination
+                  :help "Start Pen.el in a GUI web browser"))
     (bindings--define-key menu [cancel-menu]
       '(menu-item "Cancel" identity-command
                   :help "Cancel out of this menu"))
