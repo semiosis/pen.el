ffdcb529ced0df6912106d90af004fb06f03df04
Shane Mulligan <mullikine@gmail.com>
2022-01-21 13:18:46 +1300

    Fri 21 Jan 2022 13:18:46

--- src/pen-menu-bar.el
+++ src/pen-menu-bar.el
@@ -220,7 +220,6 @@
     (bindings--define-key menu [mi-pen-start-hidden-terminal]
       '(menu-item "Start hidden human terminal" pen-start-hidden-terminal
                   :help "When a human is prompted, it will appear in the hidden terminal"))
-
     (bindings--define-key menu [cancel-menu]
       '(menu-item "Cancel" identity-command
                   :help "Cancel out of this menu"))
