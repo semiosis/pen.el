559912d27dfef12d9007acff37aaacf5bfb4c6b2
Shane Mulligan <mullikine@gmail.com>
2022-01-21 14:53:20 +1300

    Fri 21 Jan 2022 14:53:20

--- src/pen.el
+++ src/pen.el
@@ -78,7 +78,7 @@
 (require 'pen-human)
 (require 'pen-cterm)
 (require 'pen-web)
-
+(require 'pen-dired)
 
 ;; Not sure I need the following in pen.el
 ;; Might be fine to just have them in init.el only
