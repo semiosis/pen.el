d61c7ee0edf9bce5f15d578e2ab7f7dc952d0bfc
Shane Mulligan <mullikine@gmail.com>
2022-01-21 13:24:50 +1300

    Fri 21 Jan 2022 13:24:50

--- src/pen-human.el
+++ src/pen-human.el
@@ -1,6 +1,6 @@
 (defun pen-start-hidden-terminal ()
   (interactive)
-  (sps "pen-hidden-terminal")
+  (pen-sps "pen-hidden-terminal")
   (message "The hidden terminal has started somewhere."))
 
 (provide 'pen-human)
