22c194dcbc6d214bb4f10c856bfda6959f743703
Shane Mulligan <mullikine@gmail.com>
2022-01-23 12:30:11 +1300

    1642894211

--- scripts/run.sh
+++ scripts/run.sh
@@ -73,6 +73,9 @@ export PEN_USE_GUI=n
 echo "ttyd running on port 7681, serving Pen.el on http"
 ttyd -p 7681 bash -l /root/.emacs.d/pen.el/scripts/newframe.sh &>/dev/null &
 /inspircd-2.0.25/run/inspircd start &>/dev/null
+
+echo Postgres running
+pg_ctlcluster 11 main start
 )
 fi
 
