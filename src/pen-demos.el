34ef558e27878d60234c27006b69748b5cf760d1
Shane Mulligan <mullikine@gmail.com>
2022-01-22 20:56:56 +1300

    Sat 22 Jan 2022 20:56:56

--- src/pen-demos.el
+++ src/pen-demos.el
@@ -1,13 +1,19 @@
 (defun pen-demo-installation ()
   (interactive)
-  (pen-tm-asciinema-play "https://asciinema.org/a/at9VpN22g7ZZkf4Vkoy8hIOJJ"))
+  (pen-tm-asciinema-play
+   "https://asciinema.org/a/at9VpN22g7ZZkf4Vkoy8hIOJJ"
+   "Pen.el Installation Demo"))
 
 (defun pen-demo-apostrophe ()
   (interactive)
-  (pen-tm-asciinema-play "https://asciinema.org/a/bjhdM0VBAuG4LrSOFxcPITmyI"))
+  (pen-tm-asciinema-play
+   "https://asciinema.org/a/bjhdM0VBAuG4LrSOFxcPITmyI"
+   "Apostrophe Demo"))
 
 (defun pen-demo-paracosm ()
   (interactive)
-  (pen-tm-asciinema-play "https://asciinema.org/a/9Pbe1TbYMvbOd6DnlqYpomIan"))
+  (pen-tm-asciinema-play
+   "https://asciinema.org/a/9Pbe1TbYMvbOd6DnlqYpomIan"
+   "Paracosm Demo"))
 
 (provide 'pen-demos)
