7fea7784f70e7545521dba8c895279a463c02678
Shane Mulligan <mullikine@gmail.com>
2022-01-22 21:50:46 +1300

    Sat 22 Jan 2022 21:50:46

--- src/pen-asciinema.el
+++ src/pen-asciinema.el
@@ -2,10 +2,12 @@
   (interactive (list (read-string "asciinema url:")
                      ""))
 
-  (if (display-graphic-p)
-      (eval
-       `(pen-use-vterm
-         (pen-nw (concat "pen-asciinema-play -h " (pen-q ,url)))))
-    (pen-sn (concat "pen-asciinema-play " (pen-q url)))))
+  (if (internet-connected-p)
+      (if (display-graphic-p)
+          (eval
+           `(pen-use-vterm
+             (pen-nw (concat "pen-asciinema-play -h " (pen-q ,url)))))
+        (pen-sn (concat "pen-asciinema-play " (pen-q url))))
+    (message "Internet is not connected")))
 
 (provide 'pen-asciinema)
