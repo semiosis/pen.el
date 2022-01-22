9efb0b68baec04f6e3d755c71be9d2c63d5510ec
Shane Mulligan <mullikine@gmail.com>
2022-01-22 13:33:23 +1300

    Sat 22 Jan 2022 13:33:23

--- src/pen-configure.el
+++ src/pen-configure.el
@@ -34,32 +34,4 @@
     (f-touch key-path)
     (f-write-text key 'utf-8 key-path)))
 
-(defun pen-add-key-booste (key)
-  (interactive (list (pen-read-service-key "booste")))
-  (pen-add-key "booste" key))
-
-(defun pen-add-key-alephalpha (key)
-  (interactive (list (pen-read-service-key "alephalpha")))
-  (pen-add-key "alephalpha" key))
-
-(defun pen-add-key-openai (key)
-  (interactive (list (pen-read-service-key "openai")))
-  (pen-add-key "openai" key))
-
-(defun pen-add-key-cohere (key)
-  (interactive (list (pen-read-service-key "cohere")))
-  (pen-add-key "cohere" key))
-
-(defun pen-add-key-ai21 (key)
-  (interactive (list (pen-read-service-key "ai21")))
-  (pen-add-key "ai21" key))
-
-(defun pen-add-key-aix (key)
-  (interactive (list (pen-read-service-key "aix")))
-  (pen-add-key "aix" key))
-
-(defun pen-add-key-hf (key)
-  (interactive (list (pen-read-service-key "hf")))
-  (pen-add-key "hf" key))
-
 (provide 'pen-configure)
