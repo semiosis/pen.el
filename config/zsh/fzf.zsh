335333c5cf285977705f574dcca908056144f0df
Shane Mulligan <mullikine@gmail.com>
2022-01-22 13:05:15 +1300

    Sat 22 Jan 2022 13:05:15

---
+++ config/zsh/fzf.zsh
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/shane/source/git/fzf/bin* ]]; then
+  export PATH="$PATH:/home/shane/source/git/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+# [[ $- == *i* ]] && source "/home/shane/source/git/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+# source "/home/shane/source/git/fzf/shell/key-bindings.zsh"
+
