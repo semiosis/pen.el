cfbe81c1248fc046d525340e7ecc3a39c3612234
Shane Mulligan <mullikine@gmail.com>
2022-01-22 18:10:43 +1300

    Sat 22 Jan 2022 18:10:43

--- src/pen.el
+++ src/pen.el
@@ -88,6 +88,7 @@
 (require 'pen-keys)
 (require 'pen-swipe)
 (require 'pen-ilambda)
+(require 'pen-demos)
 
 ;; Not sure I need the following in pen.el
 ;; Might be fine to just have them in init.el only
