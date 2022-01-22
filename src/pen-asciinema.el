765ead1d0ad3839271aced438924c6906eddf893
Shane Mulligan <mullikine@gmail.com>
2022-01-22 18:42:35 +1300

    Sat 22 Jan 2022 18:42:35

--- src/pen-asciinema.el
+++ src/pen-asciinema.el
@@ -1,5 +1,8 @@
 (defun pen-tm-asciinema-play (url)
   (interactive (list (read-string "asciinema url:")))
-  (pen-sn (concat "pen-asciinema-play " (pen-q url))))
+
+  (if (display-graphic-p)
+      (pen-nw (concat "pen-asciinema-play -h " (pen-q url)))
+    (pen-sn (concat "pen-asciinema-play " (pen-q url)))))
 
 (provide 'pen-asciinema)
