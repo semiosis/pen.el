89109cd34e264559e678939b426d226e586fe606
Shane Mulligan <mullikine@gmail.com>
2022-01-22 13:34:27 +1300

    Sat 22 Jan 2022 13:34:27

--- src/pen-menu-bar.el
+++ src/pen-menu-bar.el
@@ -245,9 +245,9 @@
     (bindings--define-key menu [mi-pen-add-key-alephalpha]
       '(menu-item "Add AlephAlpha key" pen-add-key-alephalpha
                   :help "Add, edit or remove the AlephAlpha key"))
-    (bindings--define-key menu [mi-pen-add-key-alephalpha]
-      '(menu-item "Add AlephAlpha key" pen-add-key-alephalpha
-                  :help "Add, edit or remove the AlephAlpha key"))
+    (bindings--define-key menu [mi-pen-add-key-ai21]
+      '(menu-item "Add AI21 key" pen-add-key-ai21
+                  :help "Add, edit or remove the AI21 key"))
     (bindings--define-key menu [cancel-menu]
       '(menu-item "Cancel" identity-command
                   :help "Cancel out of this menu"))
