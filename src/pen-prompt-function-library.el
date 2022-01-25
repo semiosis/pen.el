e635ced71d65aedd415271cb4fd3d8776c457ea3
Shane Mulligan <mullikine@gmail.com>
2022-01-26 10:21:13 +1300

    Wed 26 Jan 2022 10:21:12

--- src/pen-prompt-function-library.el
+++ src/pen-prompt-function-library.el
@@ -2,9 +2,11 @@
 ;; e:$PROMPTS/translate-to-english-1.prompt
 (defun pen-show-translation (json)
   (interactive)
-  (let ((ht (json-parse-string json)))
-    (message (ht-get ht "original_language"))
-    (pen-etv (ht-get ht "output"))
+  (let* ((ht (json-parse-string json))
+         (orig-lang (ht-get ht "original_language"))
+         (output (ht-get ht "output")))
+
+    (pen-eipe output nil nil (concat "Original language: " orig-lang))
 
     ;; TODO Use pen-eipe
     ;; Use an overlay to display the original language
