e783694f6374da2b7106dbb5524807af196b0ccd
Shane Mulligan <mullikine@gmail.com>
2022-01-26 09:45:41 +1300

    Wed 26 Jan 2022 09:45:41

--- src/pen-prompt-function-library.el
+++ src/pen-prompt-function-library.el
@@ -1,3 +1,7 @@
+;; sp +/"^- json: \"pen-str translate-to-english-1-json\"" "$PROMPTS/translate-to-english-1.prompt"
+;; e:$PROMPTS/translate-to-english-1.prompt
+(defun pen-show-translation (json)
 
+  )
 
 (provide 'pen-prompt-function-library)
