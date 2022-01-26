2f85d62eb494d0290c773aed6ddb18d8ba7fdece
Shane Mulligan <mullikine@gmail.com>
2022-01-27 00:34:07 +1300

    Thu 27 Jan 2022 00:34:07

--- src/pen-common.el
+++ src/pen-common.el
@@ -1,3 +1,8 @@
-(defset pen-common-functions '(pf-quick-fix-code/1))
+(defset pen-common-prompt-functions '(pf-quick-fix-code/1))
+
+(defun pen-run-common-prompt-function ()
+  (interactive)
+  (let ((pf (fz pen-common-prompt-functions nil nil "Common prompt function: ")))
+    (call-interactively 'pf)))
 
 (provide 'pen-common)
