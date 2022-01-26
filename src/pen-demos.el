cf4e4c87f41451049bccca5c753b06fb9d2066b6
Shane Mulligan <mullikine@gmail.com>
2022-01-26 22:56:07 +1300

    Wed 26 Jan 2022 22:56:07

--- src/pen-demos.el
+++ src/pen-demos.el
@@ -16,4 +16,10 @@
    "https://asciinema.org/a/9Pbe1TbYMvbOd6DnlqYpomIan"
    "Paracosm Demo"))
 
+(defun pen-demo-human-promptee ()
+  (interactive)
+  (pen-tm-asciinema-play
+   "https://asciinema.org/a/aQaG1JRreNl6fqetlHLh83Dih"
+   "Human-Promptee Demo"))
+
 (provide 'pen-demos)
