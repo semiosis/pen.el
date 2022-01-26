81359e28e59c6d187b72df459bc13d21ea244952
Shane Mulligan <mullikine@gmail.com>
2022-01-27 01:53:01 +1300

    Thu 27 Jan 2022 01:53:01

--- src/pen-menu-bar.el
+++ src/pen-menu-bar.el
@@ -715,6 +715,9 @@
     (bindings--define-key menu [mi-pen-demo-human-promptee]
       '(menu-item "Human-Promptee" pen-demo-human-promptee
                   :help "A human as a promptee instead of a language model"))
+    (bindings--define-key menu [mi-pen-demo-human-promptee]
+      '(menu-item "Pet vim" pen-demo-pet
+                  :help "Running vim inside Pen.el"))
     (bindings--define-key menu [mi-pen-demo-imaginary-interpreter]
       '(menu-item "Imaginary Interpreter" pen-demo-imaginary-interpreter
                   :help "How to start and use an Imaginary Interpreter"))
