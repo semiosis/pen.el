40808a99423872a2c193b5f6d9a508b7878ccca1
Shane Mulligan <mullikine@gmail.com>
2022-01-27 00:00:06 +1300

    Thu 27 Jan 2022 00:00:06

--- src/pen-docs.el
+++ src/pen-docs.el
@@ -1,3 +1,16 @@
-
+(defun pen-read-tutorial ()
+  (interactive)
+  (let* ((dir (f-join pen-penel-directory "docs" "tutorials"))
+         (file (fz (pen-sn
+                    (concat
+                     (cmd "find" "." "-name" "*.org")
+                     " | sed 's/.\\///'")
+                    nil
+                    dir)
+                   nil nil
+                   "Tutorial: "))
+         (full-path (f-join dir file)))
+    (if (f-exists-p full-path)
+        (find-file full-path))))
 
 (provide 'pen-docs)
