deb574a24a77533560506a32597c4c78f6ab00db
Shane Mulligan <mullikine@gmail.com>
2022-01-30 18:25:27 +1300

    Sun 30 Jan 2022 18:25:27

--- scripts/glide.py
+++ scripts/glide.py
@@ -81,7 +81,7 @@ def show_images(batch: th.Tensor):
     Image.fromarray(reshaped.numpy()).show()
 
 # Sampling parameters
-prompt = PEN_PROMPT or "A surreal and surreptitious strawberry in a phantasmagoria of semiotic art."
+prompt = PEN_PROMPT or "A surreal and surreptitious strawberry in a semiotic drawing."
 batch_size = int(PEN_N_COMPLETIONS)
 guidance_scale = 3.0
 
