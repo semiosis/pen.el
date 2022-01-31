c25662e322566e5d36c36bcb0b4b7b7d60645fe6
Shane Mulligan <mullikine@gmail.com>
2022-01-31 14:22:16 +1300

    Mon 31 Jan 2022 14:22:16

--- src/pen-proxy.el
+++ src/pen-proxy.el
@@ -2,16 +2,16 @@
   "Example: localhost:9707"
   :type 'string
   :group 'pen
-  ;; :options (ht-keys pen-engines)
+  :options (list "localhost:9707")
   :set (lambda (_sym value)
          (set _sym value))
   :get (lambda (_sym)
          (eval (sor _sym nil)))
   :initialize #'custom-initialize-default)
 
-(defset pen-proxy nil)
+;; (defset pen-proxy nil)
 
 ;; Proxy is a host and a port
-(defset pen-proxy "localhost:9707")
+;; (defset pen-proxy "localhost:9707")
 
 (provide 'pen-proxy)
