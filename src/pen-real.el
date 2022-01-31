ae544b0b988b0dc6b89717eb2d7a2c75e31ceb90
Shane Mulligan <mullikine@gmail.com>
2022-02-01 00:39:06 +1300

    Tue 01 Feb 2022 00:39:06

--- src/pen-real.el
+++ src/pen-real.el
@@ -8,4 +8,12 @@
   ;; (pen-sps "swipl")
   (pet "ipython"))
 
+(defun swipl-host ()
+  (interactive)
+  (cterm "swipl"))
+
+(defun ipython-host ()
+  (interactive)
+  (cterm "ipython"))
+
 (provide 'pen-real)
