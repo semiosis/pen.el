bffdcd03102a9024433c25767eeba5f94782463f
Shane Mulligan <mullikine@gmail.com>
2022-02-03 20:46:22 +1300

    Thu 03 Feb 2022 20:46:22

--- src/pen-continuum.el
+++ src/pen-continuum.el
@@ -7,16 +7,31 @@
   (interactive (list (pen-trim-max-chars (buffer-string-visible) 300)))
   (setq pen-terminal-states(cons state pen-terminal-states)))
 
-(defun continuum (older-state old-state)
-  (interactive (list '(nil nil)))
-
-  (let ((r (-reverse pen-terminal-states)))
+(defun continuum-get-older ()
+  (let ((r (reverse pen-terminal-states)))
     (if (and (not older-state)
              (> (length pen-terminal-states) 1))
-        (setq older-state (second r)))
+        (second r))))
+
+(defun continuum-get-old ()
+  (let ((r (reverse pen-terminal-states)))
     (if (and (not old-state)
              (> (length pen-terminal-states) 1))
-        (setq old-state (first r))))
-  (pf-guess-your-terminal-s-future/2 older old))
+        (first r))))
+
+(defun continuum-get-current ()
+  (pen-trim-max-chars (buffer-string-visible) 300))
+
+(defun continuum-push ()
+  (interactive)
+  (setq pen-terminal-states (cons (continuum-get-current) pen-terminal-states)))
+
+(defun continuum (older-state old-state)
+  (interactive (list (continuum-get-older) (continuum-get-old)))
+
+  (pf-guess-your-terminal-s-future/3
+   (continuum-get-older)
+   (continuum-get-old)
+   (continuum-get-current)))
 
 (provide 'pen-continuum)
