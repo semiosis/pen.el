3a068bcdabeb8df8abc3d108f0b2ef58ac016f26
Shane Mulligan <mullikine@gmail.com>
2022-02-06 19:55:40 +1300

    Sun 06 Feb 2022 19:55:40

--- src/pen-fz.el
+++ src/pen-fz.el
@@ -1,7 +1,8 @@
 (require 'pen-helm)
 
-(defun pen-fz (list &key prompt &key full-frame &key initial-input &key must-match &key select-only-match &key hist-var &key add-props &key no-hist)
+(cl-defun pen-fz (candidates &key prompt &key full-frame &key initial-input &key must-match &key select-only-match &key hist-var &key add-props &key no-hist)
   ;; (cl-fz results :prompt prompt :key full-frame :key initial-input :key must-match :key select-only-match :key hist-var :key add-props :key no-hist)
-  (fz-helm results prmopt))
+  (fz-helm candidates (or (sor prompt)
+                          "Select: ")))
 
 (provide 'pen-fz)
