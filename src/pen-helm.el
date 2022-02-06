852da0e135a13d1b5c6a284dd99fbdc8a56e19c4
Shane Mulligan <mullikine@gmail.com>
2022-02-06 19:43:26 +1300

    Sun 06 Feb 2022 19:43:26

--- src/pen-helm.el
+++ src/pen-helm.el
@@ -1,3 +1,9 @@
 (require 'helm-buffers)
 
+(defun fz-default-return-query (list &optional prompt)
+  (setq prompt (or prompt ":"))
+  (helm-comp-read prompt list :must-match 'nil))
+
+(defalias 'pen-helm 'fz-default-return-query)
+
 (provide 'pen-helm)
