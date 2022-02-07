72927c6126ea2a3c261fb0e4b1c637e4be24d341
Shane Mulligan <mullikine@gmail.com>
2022-02-08 01:55:08 +1300

    Tue 08 Feb 2022 01:55:08

--- src/pen-fz.el
+++ src/pen-fz.el
@@ -2,7 +2,10 @@
 
 (cl-defun pen-fz (candidates &key prompt &key full-frame &key initial-input &key must-match &key select-only-match &key hist-var &key add-props &key no-hist)
   ;; (cl-fz results :prompt prompt :key full-frame :key initial-input :key must-match :key select-only-match :key hist-var :key add-props :key no-hist)
-  (fz-helm candidates (or (sor prompt)
-                          "Select: ")))
+  (fz-helm
+   ;; Remove ink
+   ;; (mapcar 'str candidates)
+   candidates (or (sor prompt)
+                  "Select: ")))
 
 (provide 'pen-fz)
