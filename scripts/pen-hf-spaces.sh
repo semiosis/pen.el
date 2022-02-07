d41c94ae79cdaf7fa40e987327cecd28b44088b2
Shane Mulligan <mullikine@gmail.com>
2022-02-07 13:44:28 +1300

    Mon 07 Feb 2022 13:44:28

--- scripts/pen-hf-spaces.sh
+++ scripts/pen-hf-spaces.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 export TTY
 
-curl -X POST https://hf.space/gradioiframe/valhalla/glide-text2im/+/api/predict/ \
+: "${PEN_MODEL:="valhalla/glide-text2im"}"
+
+test -n "$PEN_PROMPT" || {
+    echo No prompt given
+    exit 1
+}
+
+td="$(mktemp -d)"
+
+# Use 'jo'.
+
+data="$(jo data="$(jo -a "$PEN_PROMPT")")"
+
+curl -X POST "https://hf.space/gradioiframe/$PEN_MODEL/+/api/predict/" \
     -H 'Content-Type: application/json' \
-    -d '{"data": ["an oil painting of a corgi"]}'
+    -d "$data"
