910ebf386958c1aaf1f5415ae17e67413579e776
Shane Mulligan <mullikine@gmail.com>
2022-02-08 01:05:22 +1300

    Tue 08 Feb 2022 01:05:22

--- src/pen-mad-teaparty.el
+++ src/pen-mad-teaparty.el
@@ -10,9 +10,9 @@
                          nil nil "Person: ")))
   (if pet
       (if start-chatbot
-          (pen-sps (pen-cmd "pet" "-e" "(mtp-delay-start-channel)" "mtp" name))
-        (pen-sps (pen-cmd "pet" "mtp" name)))
-    (pen-sps (pen-cmd "mtp" name))))
+          (pen-nw (pen-cmd "pet" "-e" "(mtp-delay-start-channel)" "mtp" name))
+        (pen-nw (pen-cmd "pet" "mtp" name)))
+    (pen-nw (pen-cmd "mtp" name))))
 
 (defun pen-mtp-connect-with-name-using-pet (name)
   (interactive (list (fz (pen-list-fictional-characters (channel-get-users-string))
