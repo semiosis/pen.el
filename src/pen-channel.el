21d1eadb495916f35cbd63396f2a06896ba8e4ed
Shane Mulligan <mullikine@gmail.com>
2022-02-07 20:17:41 +1300

    Mon 07 Feb 2022 20:17:41

--- src/pen-channel.el
+++ src/pen-channel.el
@@ -202,7 +202,7 @@
             (progn
               (message "Chatbot with that name already running")
               timer)
-          (let ((newtimer (run-with-timer 2 30
+          (let ((newtimer (run-with-timer 2 15
                                           (eval
                                            `(lambda ()
                                               (if (buffer-killed? ,b)
