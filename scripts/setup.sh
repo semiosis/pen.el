e47abda6bc36bb3cdce3808036fe7444c9fe1dc0
Shane Mulligan <mullikine@gmail.com>
2022-02-10 11:55:30 +1300

    1644447330

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -607,3 +607,9 @@ done
 agi jo
 
 agi uuid
+
+# yggdrasil
+gpg --fetch-keys https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt
+gpg --export 569130E8CA20FBC4CB3FDE555898470A764B32C9 | sudo apt-key add -
+echo 'deb http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/ debian yggdrasil' | sudo tee /etc/apt/sources.list.d/yggdrasil.list
+apt-get update
