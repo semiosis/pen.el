345e7b2b0d4864d451c9e5f61ca87cce3fb9bbb7
Shane Mulligan <mullikine@gmail.com>
2022-02-09 23:07:11 +1300

    Wed 09 Feb 2022 23:07:11

--- src/pen-khala.el
+++ src/pen-khala.el
@@ -6,13 +6,13 @@
   (interactive)
   (pen-sn "khala" nil nil nil t))
 
-(defun khala-stop ()
-  (interactive)
-  (pen-sn "pen-kill-port 9837" nil nil nil t))
-
 (defun pen-get-khala-port ()
   (string-to-number
    (sor (pen-snc "cat $HOME/.pen/ports/khala.txt")
-        "7681")))
+        "9837")))
+
+(defun khala-stop ()
+  (interactive)
+  (pen-sn (pen-cmd "pen-kill-port" (str (pen-get-khala-port))) nil nil nil t))
 
 (provide 'pen-khala)
