a89cf0993d67a5c811fe835d913575ad42f875ff
Shane Mulligan <mullikine@gmail.com>
2022-02-13 22:46:54 +1300

    Sun 13 Feb 2022 22:46:54

--- src/pen.el
+++ src/pen.el
@@ -113,15 +113,18 @@
 (require 'pen-paste)
 (require 'pen-nlsh)
 (require 'pen-continuum)
-;; Otherwise, it will break host magit
-;; I still need this for editing lisp
+
+(require 'pen-lisp)
+(require 'pen-lispy)
+
 (if (pen-snq "inside-docker-p")
     (require 'pen-auto-mode-load))
+;; Otherwise, it will break host magit
+;; I still need this for editing lisp
 (require 'pen-selected)
 ;; (require 'pen-search)
 ;; (require 'pen-doc)
-;; (require 'pen-lisp)
-;; (require 'pen-lispy)
+
 
 (require 'pen-network)
 
