ce7040ca15a5169f6bf6f07a81a934b782cfec0a
Shane Mulligan <mullikine@gmail.com>
2022-02-13 21:44:17 +1300

    13.02.22

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -613,3 +613,5 @@ gpg --fetch-keys https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/
 gpg --export 569130E8CA20FBC4CB3FDE555898470A764B32C9 | sudo apt-key add -
 echo 'deb http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/ debian yggdrasil' | sudo tee /etc/apt/sources.list.d/yggdrasil.list
 apt-get update
+
+pip3 install ssdpy
