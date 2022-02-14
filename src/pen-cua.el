daa2422aaa90e84a7a3c9c2c0c236f96ecb66cc5
Shane Mulligan <mullikine@gmail.com>
2022-02-14 13:18:31 +1300

    Mon 14 Feb 2022 13:18:31

--- src/pen-cua.el
+++ src/pen-cua.el
@@ -1,8 +1,12 @@
 (require 'cua-base)
 
 (define-key global-map (kbd "<M-f5>") 'cua-paste)
-(define-key global-map (kbd "<C-y>") 'cua-paste)
-(define-key pen-map (kbd "<C-y>") 'cua-paste)
+
+(cua-mode 1)
+
+;; (define-key global-map (kbd "<C-y>") 'cua-paste)
+;; (define-key global-map (kbd "<C-y>") nil)
+;; (define-key pen-map (kbd "<C-y>") 'cua-paste)
 
 (setq cua-enable-cua-keys nil)
 
