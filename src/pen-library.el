28b7646b131b6398c928f2db8f6e742d207a6b71
Shane Mulligan <mullikine@gmail.com>
2022-02-14 18:44:21 +1300

    Mon 14 Feb 2022 18:44:21

--- src/pen-library.el
+++ src/pen-library.el
@@ -796,11 +796,6 @@ buffer which is not included when this function returns"
   (interactive)
   (if (not (or (region-active-p) (lispy-left-p)))
       (call-interactively 'pen-complete-words)
-    (comment (progn-noadvice
-              ;; (ad-deactivate 'evil-forward-word-begin) ;Not even this works
-              (advice-remove 'evil-forward-word-begin #'pen-doc-thing-at-point)
-              (call-interactively 'evil-forward-WORD-begin)
-              (advice-add 'evil-forward-word-begin :after #'pen-doc-thing-at-point)))
     (let ((pen nil))
       (execute-kbd-macro (kbd "M-w"))))
   (deactivate-mark))
