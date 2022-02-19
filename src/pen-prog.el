f15a88edc2e343e6169d95eb410ab2acfbc2a16b
Shane Mulligan <mullikine@gmail.com>
2022-02-19 22:36:55 +1300

    Sat 19 Feb 2022 22:36:55

--- src/pen-prog.el
+++ src/pen-prog.el
@@ -185,7 +185,7 @@
 
 (define-key prog-mode-map (kbd "H-v") 'handlenav/body)
 
-(define-key prog-mode-map "M-J" 'evil-join)
+(define-key prog-mode-map (kbd "M-J") 'evil-join)
 
 (define-key prog-mode-map (kbd "C-c C-o") 'org-open-at-point)
 (define-key prog-mode-map (kbd "C-c h f") 'handle-docfun)
