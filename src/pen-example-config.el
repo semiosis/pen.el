b49bf9592e7f9c453d942b696895939a0b1e39ad
Shane Mulligan <mullikine@gmail.com>
2022-02-19 23:49:20 +1300

    Sat 19 Feb 2022 23:49:20

--- src/pen-example-config.el
+++ src/pen-example-config.el
@@ -723,9 +723,9 @@
 (define-key pen-map (kbd "M-y M-d") #'pen-yank-dir)
 (define-key pen-map (kbd "M-y M-f") #'pen-yank-file)
 (define-key pen-map (kbd "M-y M-p") #'pen-yank-path)
-(define-key pen-map (kbd "M-y M-g") #'yank-git-path)
+(define-key pen-map (kbd "M-y M-g") #'pen-yank-git-path)
 (define-key pen-map (kbd "M-y M-o") #'open-git-path-chrome)
-(define-key pen-map (kbd "M-y M-m") #'yank-git-path-master)
-(define-key pen-map (kbd "M-y M-G") #'yank-git-path-sha)
+(define-key pen-map (kbd "M-y M-m") #'pen-yank-git-path-master)
+(define-key pen-map (kbd "M-y M-G") #'pen-yank-git-path-sha)
 
 (provide 'pen-example-config)
