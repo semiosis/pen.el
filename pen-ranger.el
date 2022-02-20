33e00b27da9375a61b3247dccca6c10fadb7201c
Shane Mulligan <mullikine@gmail.com>
2022-02-20 13:03:55 +1300

    Sun 20 Feb 2022 13:03:55

--- pen-ranger.el
+++ pen-ranger.el
@@ -1,16 +1,16 @@
-(defun term-ranger ()
+(defun pen-term-ranger ()
   (interactive)
   ;; ranger just doesn't look good in term
   ;; (pen-term-nsfa (concat "ranger " (pen-q (current-directory))))
   (pen-nw (concat "ranger " (pen-q (current-directory)))))
 
-(defun spv-ranger ()
+(defun pen-spv-ranger ()
   (interactive)
   (shell-command (concat "pen-tm -f -d -te spv -c " (pen-q default-directory) " ranger")))
 
-(defun sps-ranger (dir)
+(defun pen-sps-ranger (dir)
   (interactive (list default-directory))
   (shell-command (concat "pen-tm -f -d -te pen-sps -c " (pen-q dir) " ranger")))
-(defalias 'sh/ranger 'sps-ranger)
+(defalias 'pen-sh/ranger 'pen-sps-ranger)
 
 (provide 'pen-ranger)
