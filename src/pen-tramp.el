005fee0cd7eccc74cb99bb449d82f12e3726f342
Shane Mulligan <mullikine@gmail.com>
2022-03-15 09:36:25 +1300

    Tue 15 Mar 2022 09:36:25

--- src/pen-tramp.el
+++ src/pen-tramp.el
@@ -1,15 +1,15 @@
 ;; TRAMP gcloud ssh
-;; (add-to-list 'tramp-methods
-;;   '("gssh"
-;;     (tramp-login-program        "gssh")
-;;     (tramp-login-args           (("%h")))
-;;     (tramp-async-args           (("-q")))
-;;     (tramp-remote-shell         "/bin/sh")
-;;     (tramp-remote-shell-args    ("-c"))
-;;     (tramp-gw-args              (("-o" "GlobalKnownHostsFile=/dev/null")
-;;                                  ("-o" "UserKnownHostsFile=/dev/null")
-;;                                  ("-o" "StrictHostKeyChecking=no")))
-;;     (tramp-default-port         22)))
+(add-to-list 'tramp-methods
+  '("gssh"
+    (tramp-login-program        "gssh")
+    (tramp-login-args           (("%h")))
+    (tramp-async-args           (("-q")))
+    (tramp-remote-shell         "/bin/sh")
+    (tramp-remote-shell-args    ("-c"))
+    (tramp-gw-args              (("-o" "GlobalKnownHostsFile=/dev/null")
+                                 ("-o" "UserKnownHostsFile=/dev/null")
+                                 ("-o" "StrictHostKeyChecking=no")))
+    (tramp-default-port         22)))
 
 (require 'git-gutter+)
 
