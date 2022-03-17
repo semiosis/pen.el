4dd3b526c8f0db7fa388df2330cb128f4f6c8be9
Shane Mulligan <mullikine@gmail.com>
2022-03-17 13:41:20 +1300

    Thu 17 Mar 2022 13:41:20

--- src/pen-common.el
+++ src/pen-common.el
@@ -15,6 +15,17 @@
     (insert
      (pf-complete-inner-prose/2 prefix suffix))))
 
+(defun pen-complete-code ()
+  (interactive)
+  (if (pen-selected)
+      (call-interactively 'kill-region))
+  (let ((prefix (pen-preceding-lines 2))
+        (suffix (pen-proceeding-lines 2)))
+
+    (insert
+     (pf-complete-inner-code/2 prefix suffix))))
+
 (define-key pen-map (kbd "M-6") 'pen-complete-inner)
+(define-key pen-map (kbd "M-7") 'pen-complete-inner)
 
 (provide 'pen-common)
