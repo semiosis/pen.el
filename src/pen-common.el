db2ac874834296d55f3039ac0207db4a8981b81d
Shane Mulligan <mullikine@gmail.com>
2022-03-17 15:17:39 +1300

    17.03.22

--- src/pen-common.el
+++ src/pen-common.el
@@ -5,22 +5,40 @@
   (let ((pf (fz pen-common-prompt-functions nil nil "Common prompt function: ")))
     (call-interactively 'pf)))
 
-(defun pen-complete-inner ()
-  (interactive)
-  (let ((lines-context 2))
+(defun pen-complete-inner (lines-context)
+  "Complete inner text by providing lines-later-context lines of surrounding context."
+  (interactive (list 2))
+  (if (not (listp current-prefix-arg))
+      (setq lines-context (prefix-numeric-value current-prefix-arg)))
 
-    (if (not (listp current-prefix-arg))
-        (setq lines-context (prefix-numeric-value current-prefix-arg)))
+  (if (pen-selected)
+      (call-interactively 'kill-region))
+  (let ((prefix (pen-preceding-lines lines-context))
+        (suffix (pen-proceeding-lines lines-context)))
 
-    (if (pen-selected)
-        (call-interactively 'kill-region))
-    (let ((prefix (pen-preceding-lines lines-context))
-          (suffix (pen-proceeding-lines lines-context)))
+    (insert
+     (if (pen-code-p)
+         (pf-complete-inner-code/2 prefix suffix)
+       (pf-complete--prose/2 prefix suffix)))))
 
-      (insert
-       (if (pen-code-p)
-           (pf-complete-inner-code/2 prefix suffix)
-         (pf-complete--prose/2 prefix suffix))))))
+(defun pen-complete-prefix (lines-later-context)
+  "Complete preceding text by providing lines-later-context lines of latter context."  
+  (interactive (list 2))
+
+  (if (not (listp current-prefix-arg))
+      (setq lines-later-context (prefix-numeric-value current-prefix-arg)))
+
+  (if (pen-selected)
+      (call-interactively 'kill-region))
+
+  ;; Some prior context i.e. is a good idea to avoid getting 'once upon a time'
+  (let ((prefix (pen-preceding-lines 1))
+        (suffix (pen-proceeding-lines lines-later-context)))
+
+    (insert
+     (if (pen-code-p)
+         (pf-complete-inner-code/2 prefix suffix)
+       (pf-complete--prose/2 prefix suffix)))))
 
 (define-key pen-map (kbd "M-6") 'pen-complete-inner)
 
