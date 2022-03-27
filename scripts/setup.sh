be9255c9bb5d1cc60008c995ff2da56c62b2b576
Shane Mulligan <mullikine@gmail.com>
2022-03-27 19:32:11 +1300

    1648362731

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -669,3 +669,13 @@ mkdir -p /etc/apache2/sites-available
 # Haskell: - gcup, cabal, hls
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 # =gcup tui= to configure
+# To start a simple repl, run:
+#   ghci
+# 
+# To start a new haskell project in the current directory, run:
+#   cabal init --interactive
+# 
+# To install other GHC versions and tools, run:
+#   ghcup tui
+# 
+# If you are new to Haskell, check out https://www.haskell.org/ghcup/install/#first-steps
