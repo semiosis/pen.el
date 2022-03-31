0799a1c443d0aa09b76b5a96e0d4c319ee357777
Shane Mulligan <mullikine@gmail.com>
2022-03-31 20:56:49 +1300

    Thu 31 Mar 2022 20:56:49

--- src/pen-compile-run.el
+++ src/pen-compile-run.el
@@ -3,8 +3,8 @@
   (interactive)
   (cond ((bound-and-true-p go-playground-mode) (call-interactively 'go-playground-exec))
         ;; Unfortunately, cant use ~tm sps~ because the command can't find the LINES and COLUMNS, even with eval resize
-        ;; pen-sps works now
-        (t ;; (pen-snc (concat "unbuffer pen-tm -f -te -d pen-sps -x -pak -args pen-cr " crstr))
+        ;; sps works now
+        (t ;; (pen-snc (concat "unbuffer pen-tm -f -te -d sps -x -pak -args pen-cr " crstr))
          (let ((crstr
                 (cond ((derived-mode-p 'json-mode) (concat "-ft json " (pen-q (str (buffer-file-name)))))
                       ((derived-mode-p 'csv-mode) (concat "-ft csv " (pen-q (str (buffer-file-name)))))
@@ -12,16 +12,16 @@
                       )
                 ))
            (if (not (buffer-file-name))
-               (pen-sn (concat "pen-tm -f -S -i -tout pen-sps -x -pak -args pen-cr " crstr) (awk1 (buffer-string)))
-             (pen-snc (concat "unbuffer pen-tm -f -te -d pen-sps -x -pak -args pen-cr " crstr)))))))
+               (pen-sn (concat "pen-tm -f -S -i -tout sps -x -pak -args pen-cr " crstr) (awk1 (buffer-string)))
+             (pen-snc (concat "unbuffer pen-tm -f -te -d sps -x -pak -args pen-cr " crstr)))))))
 
 (defun compile-run-term ()
   "This is used to compile and run a source file."
   (interactive)
   (cond ((bound-and-true-p go-playground-mode) (call-interactively 'go-playground-exec))
         ;; Unfortunately, cant use ~tm sps~ because the command can't find the LINES and COLUMNS, even with eval resize
-        ;; pen-sps works now
-        (t ;; (pen-snc (concat "unbuffer pen-tm -f -te -d pen-sps -x -pak -args pen-cr " crstr))
+        ;; sps works now
+        (t ;; (pen-snc (concat "unbuffer pen-tm -f -te -d sps -x -pak -args pen-cr " crstr))
          (let ((crstr
                 (cond ((derived-mode-p 'json-mode) (concat "-ft json " (pen-q (str (buffer-file-name)))))
                       ((derived-mode-p 'csv-mode) (concat "-ft csv " (pen-q (str (buffer-file-name)))))
@@ -37,7 +37,7 @@
 ;;   (interactive)
 ;;   (cond ((bound-and-true-p go-playground-mode) (call-interactively 'go-playground-exec))
 ;;         ;; Unfortunately, cant use ~tm sps~ because the command can't find the LINES and COLUMNS, even with eval resize
-;;         ;; pen-sps works now
+;;         ;; sps works now
 ;;         (t (pen-term-nsfa (concat "pen-cr " (pen-q (str (buffer-file-name))) "; pen-pak")))))
 
 (defun compile-run-tm-ecompile ()
@@ -45,15 +45,15 @@
   (interactive)
   (cond ((bound-and-true-p go-playground-mode) (call-interactively 'go-playground-exec))
         ;; Unfortunately, cant use ~tm sps~ because the command can't find the LINES and COLUMNS, even with eval resize
-        ;; pen-sps works now
-        (t (pen-snc (concat "unbuffer pen-tm -f -te -d pen-sps -x -pak -args compile pen-cr " (pen-q (str (buffer-file-name))))))))
+        ;; sps works now
+        (t (pen-snc (concat "unbuffer pen-tm -f -te -d sps -x -pak -args compile pen-cr " (pen-q (str (buffer-file-name))))))))
 
 (defun compile-run-compile ()
   "This is used to compile and run a source file inside term."
   (interactive)
   (cond ((bound-and-true-p go-playground-mode) (call-interactively 'go-playground-exec))
         ;; Unfortunately, cant use ~tm sps~ because the command can't find the LINES and COLUMNS, even with eval resize
-        ;; pen-sps works now
+        ;; sps works now
         (t (compile (concat "pen-cr " (pen-q (str (buffer-file-name))) " | cat")))))
 
 (defun schema-run ()
@@ -61,7 +61,7 @@
   (interactive)
   (cond ((bound-and-true-p go-playground-mode) (call-interactively 'go-playground-exec))
         ;; Unfortunately, cant use ~tm sps~ because the command can't find the LINES and COLUMNS, even with eval resize
-        ;; pen-sps works now
-        (t (pen-snc (concat "unbuffer pen-tm -f -te -d pen-sps -x -args zh " (pen-q (str (buffer-file-name))))))))
+        ;; sps works now
+        (t (pen-snc (concat "unbuffer pen-tm -f -te -d sps -x -args zh " (pen-q (str (buffer-file-name))))))))
 
 (provide 'pen-compile-run)
