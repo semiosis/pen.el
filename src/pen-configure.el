a83e54ef90fb9da7ee858c83c56ac08b8e69dcc2
Shane Mulligan <mullikine@gmail.com>
2022-04-05 15:07:08 +1200

    05.04.22

--- src/pen-configure.el
+++ src/pen-configure.el
@@ -15,7 +15,10 @@
 
 (defun pen-edit-efm-conf ()
   (interactive)
-  (find-file (f-join penconfdir "efm-langserver-config.yaml")))
+  (cond
+   ((f-exists? (f-join penconfdir "efm-langserver-config.yaml")) (find-file (f-join penconfdir "efm-langserver-config.yaml")))
+   ((f-exists? (f-join pen-penel-directory "config" "efm-langserver-config.yaml")) (find-file (f-join pen-penel-directory "config" "efm-langserver-config.yaml")))
+   (t (error "No editable `efm-langserver-config.yaml` found."))))
 
 (defun pen-edit-timeouts-log ()
   (interactive)
