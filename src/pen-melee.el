b2421f9a5c170cfa72b70dcc584c1f955617c7c7
Shane Mulligan <mullikine@gmail.com>
2022-06-03 23:48:02 +1200

    1654256882

--- src/pen-melee.el
+++ src/pen-melee.el
@@ -1,3 +1,20 @@
+;; TODO
+;; Time Travelers' Potlatch
+
+;; TODO
+;; Parallel Collage
+;; https://www.zazie.at/Collaborations/ParallelCollage_6er/00_WebPages/ParallelCollageMenu.html
+;; This surrealist game requires a minimum of 3 players.
+;; Each player gives 2 graphic images and 2 lines of poetic text to the other
+;; players. All players then use the images and lines of text (including
+;; their own) to construct a collage-image and collage-poem. In the process
+;; of construction, a player can modify any item, in any way desired. Until
+;; everyone is finished, each person's work is kept hidden. When all images
+;; and poems are complete, they are shared among the players, with a
+;; comparison of the results.
+
+
+
 (defun melee-start-immitation-game ()
   (interactive)
   (message "In development"))
