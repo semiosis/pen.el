bd1d31bbeae965c0c05f8059176fe1b366541a0b
Shane Mulligan <mullikine@gmail.com>
2022-07-12 17:29:59 +1200

    Tue 12 Jul 2022 17:29:59

--- src/pen-ilink.el
+++ src/pen-ilink.el
@@ -4,7 +4,7 @@
 
 ;; - If the link type doesn't exist, then add it
 ;; - If there is no handler for the link type then prompt for the result
-;;   - Use an ifunc
+;;   - Use an ifunc instead of prompting directly
 
 ;; org-add-link-type
 
