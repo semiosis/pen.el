530f67917a5e1bc1234f30730abec8b6bc3245dd
Shane Mulligan <mullikine@gmail.com>
2022-07-12 17:29:50 +1200

    Tue 12 Jul 2022 17:29:50

--- src/pen-ilink.el
+++ src/pen-ilink.el
@@ -1,4 +1,12 @@
 ;; Imaginary org-link
 
+;; What is its purpose?
+
+;; - If the link type doesn't exist, then add it
+;; - If there is no handler for the link type then prompt for the result
+;;   - Use an ifunc
+
+;; org-add-link-type
+
 
 (provide 'pen-ilink)
