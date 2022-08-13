18cafd1c09c4fc02429eb36a5690d2039aac57af
Shane Mulligan <mullikine@gmail.com>
2022-08-13 22:26:14 +1200

    1660386374

--- src/pen-hippie-expand.el
+++ src/pen-hippie-expand.el
@@ -3,4 +3,6 @@
 
 (remove-from-list 'hippie-expand-try-functions-list 'yas-hippie-try-expand)
 
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-line try-complete-lisp-symbol-partially try-complete-lisp-symbol))
+
 (provide 'pen-hippie-expand)
