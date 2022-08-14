13e4f79ad0ec962ceb29c85e8b8aaade0d797bfb
Shane Mulligan <mullikine@gmail.com>
2022-08-14 13:37:11 +1200

    Sun 14 Aug 2022 13:37:11

--- src/pen-memoize.el
+++ src/pen-memoize.el
@@ -48,6 +48,67 @@
   (remhash args table))
 
 
+;; This was broken
+(comment
+ (defun memoize--wrap (func timeout)
+   "Return the memoized version of FUNC.
+TIMEOUT specifies how long the values last from last access. A
+nil timeout will cause the values to never expire, which will
+cause a memory leak as memoize is use, so use the nil value with
+care."
+   (let* (;;This also works for lambdas
+          (funcpps (pps func))
+          (funcslugdata (if (< 150 (length funcpps))
+                            (md5 funcpps)
+                          funcpps))
+          (funcslug (slugify (s-join "-" (pen-str2list funcslugdata))))
+          (tablename (concat "table-" funcslug))
+          (timeoutsname (concat "timeouts-" funcslug))
+          (table (make-or-load-hash-table tablename '(:test equal)))
+          (timeouts (make-or-load-hash-table timeoutsname '(:test equal))))
+     (eval
+      `(lambda (&rest args)
+         (let ((value (gethash args ,table)))
+           (unwind-protect
+               ;; (or value (puthash args (apply ,func args) ,table))
+               (let ((ret (or
+                           (and
+                            (not (pen-var-value-maybe 'pen-sh-update))
+                            (not (pen-var-value-maybe 'do-pen-update))
+                            (not (>= (prefix-numeric-value current-global-prefix-arg) 4))
+                            (not (>= (prefix-numeric-value current-prefix-arg) 4))
+                            value)
+                           ;; Add to the hash table and save the hash table
+                           (let ((newret (puthash args
+                                                  (or (apply ,func args)
+                                                      'MEMOIZE_NIL)
+                                                  ,table)))
+                             (if (featurep 'hashtable-print-readable)
+                                 (ht-cache ,tablename ,table))
+                             newret))))
+                 (if (equal ret 'MEMOIZE_NIL)
+                     (setq ret nil))
+                 ret)
+             (let ((existing-timer (gethash args ,timeouts))
+                   (timeout-to-use (or
+                                    ;; timeout comes from the calling 'memoize' function
+                                    (and (variable-p 'timeout)
+                                         timeout)
+                                    memoize-default-timeout)))
+               (when existing-timer
+                 (cancel-timer existing-timer))
+               (when timeout-to-use
+                 (puthash args
+                          (run-at-time timeout-to-use nil
+                                       (eval
+                                        `(lambda ()
+                                           (remhash ,args ,,table)
+                                           ;; It would probably be better to alert and ignore
+                                           ;; (try (remhash args ,table)
+                                           ;;      (message ,(concat "timer for memoized " funcslug " failed")))
+                                           ))) ,,timeouts))))))))))
+
+;; This fixed it
 (defun memoize--wrap (func timeout)
   "Return the memoized version of FUNC.
 TIMEOUT specifies how long the values last from last access. A
@@ -62,6 +123,8 @@ care."
          (funcslug (slugify (s-join "-" (pen-str2list funcslugdata))))
          (tablename (concat "table-" funcslug))
          (timeoutsname (concat "timeouts-" funcslug))
+         ;; (table (eval `(make-or-load-hash-table ,tablename :test 'equal)))
+         ;; (timeouts (eval `(make-or-load-hash-table ,timeoutsname :test 'equal)))
          (table (make-or-load-hash-table tablename '(:test equal)))
          (timeouts (make-or-load-hash-table timeoutsname '(:test equal))))
     (eval
@@ -69,21 +132,17 @@ care."
         (let ((value (gethash args ,table)))
           (unwind-protect
               ;; (or value (puthash args (apply ,func args) ,table))
-              (let ((ret (or
-                          (and
-                           (not (pen-var-value-maybe 'pen-sh-update))
-                           (not (pen-var-value-maybe 'do-pen-update))
-                           (not (>= (prefix-numeric-value current-global-prefix-arg) 4))
-                           (not (>= (prefix-numeric-value current-prefix-arg) 4))
-                           value)
-                          ;; Add to the hash table and save the hash table
-                          (let ((newret (puthash args
-                                                 (or (apply ,func args)
-                                                     'MEMOIZE_NIL)
-                                                 ,table)))
-                            (if (featurep 'hashtable-print-readable)
-                                (ht-cache ,tablename ,table))
-                            newret))))
+              (let ((ret (or (and
+                              ;; (not (>= (prefix-numeric-value current-global-prefix-arg) 4))
+                              value)
+                             ;; Add to the hash table and save the hash table
+                             (let ((newret (puthash args
+                                                    (or (apply ,func args)
+                                                        'MEMOIZE_NIL)
+                                                    ,table)))
+                               (if (featurep 'hashtable-print-readable)
+                                   (ht-cache ,tablename ,table))
+                               newret))))
                 (if (equal ret 'MEMOIZE_NIL)
                     (setq ret nil))
                 ret)
@@ -98,13 +157,11 @@ care."
               (when timeout-to-use
                 (puthash args
                          (run-at-time timeout-to-use nil
-                                      (eval
-                                       `(lambda ()
-                                         (remhash ,args ,,table)
-                                         ;; It would probably be better to alert and ignore
-                                         ;; (try (remhash args ,table)
-                                         ;;      (message ,(concat "timer for memoized " funcslug " failed")))
-                                         ))) ,,timeouts)))))))))
+                                      (lambda ()
+                                        ;; It would probably be better to alert and ignore
+                                        (try (remhash args ,table)
+                                             ;; (message ,(concat "timer for memoized " funcslug " failed"))
+                                             ))) ,timeouts)))))))))
 
 (defun ignore-errors-around-advice (proc &rest args)
   (ignore-errors
@@ -113,6 +170,9 @@ care."
 
 (advice-add 'memoize-restore :around #'ignore-errors-around-advice)
 (advice-add 'memoize :around #'ignore-errors-around-advice)
+
+;; (advice-remove 'memoize-restore #'ignore-errors-around-advice)
+;; (advice-remove 'memoize #'ignore-errors-around-advice)
 
 (memoize-restore 'lg-url-is-404)
 (memoize 'lg-url-is-404)
