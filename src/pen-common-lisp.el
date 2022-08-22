6021a0d098b8b5001ae247bcb67458d66d463fb3
Shane Mulligan <mullikine@gmail.com>
2022-08-22 20:40:27 +1200

    22.08.22

--- src/pen-common-lisp.el
+++ src/pen-common-lisp.el
@@ -1,8 +1,9 @@
 ;; https://roswell.github.io/Initial-Recommended-Setup.html
 
 ;; ros install slime
-(ignore-errors
-  (load (expand-file-name "/root/repos/roswell/lisp/helper.el")))
-(setq inferior-lisp-program "ros -Q run")
+(comment
+ (ignore-errors
+   (load (expand-file-name "/root/repos/roswell/lisp/helper.el")))
+ (setq inferior-lisp-program "ros -Q run"))
 
 (provide 'pen-common-lisp)
