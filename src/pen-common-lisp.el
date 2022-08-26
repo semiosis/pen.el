0aa16e14c286906323223c1fc755d5778b3c4c34
Shane Mulligan <mullikine@gmail.com>
2022-08-27 11:13:56 +1200

    27.08.22

--- src/pen-common-lisp.el
+++ src/pen-common-lisp.el
@@ -4,4 +4,31 @@
 (load (expand-file-name (pen-umn "$HOME/.roswell/helper.el")))
 (setq inferior-lisp-program "ros -Q run")
 
+(defun pen-slime-select-prompt-or-result ()
+  (interactive)
+
+  (if (< slime-repl-input-start-mark (point))
+      (progn
+        (call-interactively 'end-of-buffer)
+        (set-mark (point))
+        (goto-char slime-repl-input-start-mark)
+        (call-interactively 'exchange-point-and-mark))
+    (progn
+      (if (looking-back "^[^ ]+> .*")
+          (beginning-of-line))
+      (if (looking-back "^[^ ]+> ")
+          (progn
+            (set-mark (point))
+            (end-of-line))
+        (progn
+          (slime-repl-previous-prompt)
+          (forward-line)
+          (beginning-of-line)
+          (set-mark (point))
+          (slime-repl-next-prompt)
+          (previous-line)
+          (end-of-line))))))
+
+(define-key slime-repl-mode-map (kbd "M-h") 'pen-slime-select-prompt-or-result)
+
 (provide 'pen-common-lisp)
