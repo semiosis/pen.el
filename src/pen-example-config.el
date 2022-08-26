79319f2bd5431cd4903744bd9d63e0f00405b217
root <root@mele>
2022-08-26 09:58:03 +0000

    Fri 26 Aug 2022 09:58:03 AM UTC

--- src/pen-example-config.el
+++ src/pen-example-config.el
@@ -15,7 +15,12 @@
 (define-key global-map (kbd "C-M-\\") nil) ;Ensure that this bindings isnt taken
 (define-key function-key-map (kbd "C-M-\\") 'hyperify)
 (define-key global-map (kbd "M-h") nil)
-(define-key function-key-map (kbd "M-h") 'hyperify)
+
+;; I probably don't want either of these:
+;; The problem is the browser GUI needs it.
+;; (define-key function-key-map (kbd "M-h") 'hyperify)
+;; M-h is usually used for org-mark-element, my-lispy-select-parent-sexp, etc.
+;; However, it's an incredibly convenient binding...
 (define-key function-key-map (kbd "M-H") 'hyperify)
 
 (define-key global-map (kbd "M-_") 'irc-find-prev-line-with-diff-char)
