fd5eab6ad36e5ec5d59a94c2747a2c687ba9110d
Shane Mulligan <mullikine@gmail.com>
2022-08-29 11:40:59 +1200

    29.08.22

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -895,3 +895,11 @@ cargo install grex
 agi perl-doc
 
 agi wordnet
+
+IFS= read -r -d '' LOCATE_CODE <<'HEREDOC'
+PRUNE_BIND_MOUNTS="yes"
+# PRUNENAMES=".git .bzr .hg .svn"
+PRUNEPATHS="/tmp /volumes /var/spool /media /var/lib/os-prober /var/lib/ceph /home/.ecryptfs /var/lib/schroot"
+PRUNEFS="NFS afs autofs binfmt_misc ceph cgroup cgroup2 cifs coda configfs curlftpfs debugfs devfs devpts devtmpfs ecryptfs ftpfs fuse.ceph fuse.cryfs fuse.encfs fuse.glusterfs fuse.gvfsd-fuse fuse.mfs fuse.rozofs fuse.sshfs fusectl fusesmb hugetlbfs iso9660 lustre lustre_lite mfs mqueue ncpfs nfs nfs4 ocfs ocfs2 proc pstore rpc_pipefs securityfs shfs smbfs sysfs tmpfs tracefs udev udf usbfs"
+HEREDOC
+printf -- "%s\n" "$LOCATE_CODE" >> /etc/updatedb.conf
