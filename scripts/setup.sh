f85aa3bedf3815e87e3e870a2091f447debf26c3
Shane Mulligan <mullikine@gmail.com>
2022-09-01 23:43:08 +1200

    1662032588

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -903,3 +903,5 @@ PRUNEPATHS="/tmp /volumes /var/spool /media /var/lib/os-prober /var/lib/ceph /ho
 PRUNEFS="NFS afs autofs binfmt_misc ceph cgroup cgroup2 cifs coda configfs curlftpfs debugfs devfs devpts devtmpfs ecryptfs ftpfs fuse.ceph fuse.cryfs fuse.encfs fuse.glusterfs fuse.gvfsd-fuse fuse.mfs fuse.rozofs fuse.sshfs fusectl fusesmb hugetlbfs iso9660 lustre lustre_lite mfs mqueue ncpfs nfs nfs4 ocfs ocfs2 proc pstore rpc_pipefs securityfs shfs smbfs sysfs tmpfs tracefs udev udf usbfs"
 HEREDOC
 printf -- "%s\n" "$LOCATE_CODE" >> /etc/updatedb.conf
+
+pip install --force-reinstall aleph-alpha-client
