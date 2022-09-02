2f8cad3913713cccde4599339ab998463795ecda
Shane Mulligan <mullikine@gmail.com>
2022-09-03 08:29:42 +1200

    1662150582

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,7 @@
 
 export SSH_HOST_ALLOWED=n
 
+export DUMP=/root/dump
 export EMACSD=$HOME/.emacs.d
 mkdir -p "$EMACSD"
 mkdir -p "$HOME/dump"
@@ -904,4 +905,20 @@ PRUNEFS="NFS afs autofs binfmt_misc ceph cgroup cgroup2 cifs coda configfs curlf
 HEREDOC
 printf -- "%s\n" "$LOCATE_CODE" >> /etc/updatedb.conf
 
-pip install --force-reinstall aleph-alpha-client
+# Python3.8 for AlephAlpha
+# apt update
+# make -j 4, also runs tests, annoyingly
+(
+agi build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+mcd $DUMP/programs
+curl -O https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tar.xz
+tar Jxf Python-3.8.2.tar.xz
+cd Python-3.8.2
+./configure --enable-optimizations
+make -j 4
+make altinstall
+)
+
+pip3.8 install --force-reinstall ipython
+pip3.8 install --force-reinstall aleph-alpha-client
+
