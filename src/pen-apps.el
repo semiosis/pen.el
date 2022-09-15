14f4e499c866b58ddf069c7a202ed1990d80154b
Shane Mulligan <mullikine@gmail.com>
2022-09-15 16:54:13 +1200

    15.09.22

--- src/pen-apps.el
+++ src/pen-apps.el
@@ -13,4 +13,26 @@
   (interactive)
   (pen-sps "open-colors" nil nil default-directory))
 
+(defun h-m-m ()
+  (interactive)
+  ;; Sadly, doesn't work well with eterm
+
+  ;; hmm is certainly not perfect
+  ;; - C-l while editing creates newlines in the node
+  
+  ;; tmux works best
+  (pen-nw "hmm" nil nil default-directory)
+
+  ;; vterm works ok
+  ;; But I have not yet sorted all key bindings
+  ;; Currently not working:
+  ;; - arrow keys
+  ;; - C-h
+  ;; - Delete
+  ;; - C-l messes up the screen
+  ;; (pen-use-vterm
+  ;;  (pen-term "h-m-m" nil nil default-directory))
+  )
+
+
 (provide 'pen-apps)
