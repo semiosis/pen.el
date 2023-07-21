68ed00b845ae711a951cd0dd4522d7982574b74e
Shane Mulligan <mullikine@gmail.com>
2023-07-21 23:29:51 +1200

    21.07.23

--- src/pen-avy.el
+++ src/pen-avy.el
@@ -174,6 +174,8 @@ values to copy the link to the clipboard and/or primary as well."
 ;; Psalms 103:13: Like as a father pitieth his children, so the Lord pitieth them that fear him.
 ;; psalms
 
+;; cat $HOME/.emacs.d/host/pen.el/src/pen-avy.el | scrape-bible-references | parallel --pipe -L 10 -N1 words-to-avy-tuples -nbe $HOME/.emacs.d/host/pen.el/src/pen-avy.el | v
+
 (defun filter-cmd-collect (filter-cmd &optional fp-or-buf)
   (let* ((tempf
           (cond
@@ -195,15 +197,19 @@ values to copy the link to the clipboard and/or primary as well."
          (winend (window-end))
 
          ;; Because this always needs a file, I should just simplify all this by creating a temp file
-         (tuples (pen-eval-string (pen-sn (concat filter-cmd "|" "words-to-avy-tuples " (pen-q tempf))
-                                          (cond
-                                           ((and
-                                             (stringp tempf)
-                                             (f-file-p tempf))
-                                            (e/cat tempf))
-                                           (t
-                                            (buffer-string)))
-                                          nil))))
+         (tuples (pen-eval-string
+                  (concat
+                   "'("
+                   (pen-sn (concat filter-cmd "|" "parallel --pipe -L 10 -N1 words-to-avy-tuples -nbe " (pen-q tempf))
+                           (cond
+                            ((and
+                              (stringp tempf)
+                              (f-file-p tempf))
+                             (e/cat tempf))
+                            (t
+                             (buffer-string)))
+                           nil)
+                   ")"))))
 
     ;; Only delete it if it is a temp file
     (if (and (stringp tempf)
@@ -222,6 +228,79 @@ values to copy the link to the clipboard and/or primary as well."
   ;;         (buttons-collect 'glossary-candidate-button-face)
   ;;         (buttons-collect 'glossary-error-button-face))
   )
+
+(defvar bible-book-map-names
+  ;; Used to translate any of the latter elements into the first element of each tuple.
+  ;; I should also make I, 1, First, 2, 3, etc part of the generic regex.
+  ;; I still need to be able to translate them. So I still need this.
+  '(("Genesis" "Gen" "Ge" "Gn")
+    ("Exodus" "Ex" "Exod" "Exo")
+    ("Leviticus" "Lev" "Le" "Lv")
+    ("Numbers" "Num" "Nu" "Nm" "Nb")
+    ("Deuteronomy" "Deut" "De" "Dt")
+    ("Joshua" "Josh" "Jos" "Jsh")
+    ("Judges" "Judg" "Jdg" "Jg" "Jdgs")
+    ("Ruth" "Rth" "Ru")
+    ("I Samuel" "1 Samuel" "1 Sam" "1 Sm" "1 Sa" "I Sam" "I Sa" "1Sam" "1Sa" "1S" "1st Samuel" "1st Sam" "First Samuel" "First Sam")
+    ("II Samuel" "2 Samuel" "2 Sam" "2 Sm" "2 Sa" "II Sam" "II Sa" "2Sam" "2Sa" "2S" "2nd Samuel" "2nd Sam" "First Samuel" "First Sam")
+    ("I Kings" "1 Kings" "1 Kgs" "1 Ki" "1Kgs" "1Kin" "1Ki" "1K" "1st Kings" "1st Kgs" "First Kings" "First Kgs")
+    ("II Kings" "2 Kings" "2 Kgs" "2 Ki" "2Kgs" "2Kin" "2Ki" "2K" "2nd Kings" "2nd Kgs" "Second Kings" "Second Kgs")
+    ("I Chronicles" "1 Chronicles" "1 Chron" "1 Chr" "1 Ch" "1Chron" "1Chr" "I Chron" "I Chr" "I Ch" "1st Chronicles" "1st Chron" "First Chronicles" "First Chron")
+    ("II Chronicles" "2 Chronicles" "2 Chron" "2 Chr" "2 Ch" "2Chron" "2Chr" "II Chron" "II Chr" "II Ch" "2nd Chronicles" "2nd Chron" "Second Chronicles" "Second Chron")
+    ("Ezra" "Ezr" "Ez")
+    ("Nehemiah" "Neh" "Ne")
+    ("Esther" "Est" "Esth" "Es")
+    ("Job" "Jb")
+    ("Psalms" "Ps" "Psalm" "Pslm" "Psa" "Psm" "Pss")
+    ("Proverbs" "Prov" "Pro" "Prv" "Pr")
+    ("Ecclesiastes" "Eccles" "Eccle" "Ecc" "Ec" "Qoh")
+    ("Song of Solomon" "Song" "Song of Songs" "SOS" "So" "Canticle of Canticles" "Canticles" "Cant")
+    ("Isaiah" "Isa" "Is")
+    ("Jeremiah" "Je" "Jr")
+    ("Lamentations" "Lam" "La")
+    ("Ezekiel" "Ezek" "Eze" "Ezk")
+    ("Daniel" "Dan" "Da" "Dn")
+    ("Hosea" "Hos" "Ho")
+    ("Joel" "Jl")
+    ("Amos" "Am")
+    ("Obadiah" "Obad" "Ob")
+    ("Jonah" "Jnh" "Jon")
+    ("Micah" "Mic" "Mc")
+    ("Nahum" "Nah" "Na")
+    ("Habakkuk" "Hab" "Hb")
+    ("Zephaniah" "Zeph" "Zep" "Zp")
+    ("Haggai" "Hag" "Hg")
+    ("Zechariah" "Zech" "Zec" "Zc")
+    ("Malachi" "Mal" "Ml")
+    ("Matthew" "Matt" "Mt")
+    ("Mark" "Mrk" "Mar" "Mk" "Mr")
+    ("Luke" "Luk" "Lk")
+    ("John" "Joh" "Jhn" "Jn")
+    ("Acts" "Act" "Ac")
+    ("Romans" "Rom" "Ro" "Rm")
+    ("I Corinthians" "1 Corinthians" "1 Cor" "1 Co" "I Cor" "I Co" "1Cor" "1Co" "1Corinthians" "1st Corinthians" "First Corinthians")
+    ("II Corinthians" "2 Corinthians" "2 Cor" "2 Co" "II Cor" "II Co" "2Cor" "2Co" "2Corinthians" "2nd Corinthians" "Second Corinthians")
+    ("Galatians" "Gal" "Ga")
+    ("Ephesians" "Eph" "Ephes")
+    ("Philippians" "Phil" "Php" "Pp")
+    ("Colossians" "Col" "Co")
+    ("I Thessalonians" "1 Thess" "1 Thes" "1 Th" "1 Thessalonians" "I Thess" "I Thes" "I Th" "1Thessalonians" "1Thess" "1Thes" "1Th" "1st Thessalonians" "1st Thess" "First Thessalonians" "First Thess")
+    ("II Thessalonians" "2 Thess" "2 Thes" "2 Th" "2 Thessalonians" "II Thess" "II Thes" "II Th" "2Thessalonians" "2Thess" "2Thes" "2Th" "2nd Thessalonians" "2nd Thess" "Second Thessalonians" "Second Thess")
+    ("I Timothy" "1 Timothy" "1 Tim" "1 Ti" "I Timothy" "I Tim" "I Ti" "1Timothy" "1Tim" "1Ti" "1st Timothy" "1st Tim" "First Timothy" "First Time")
+    ("II Timothy" "2 Timothy" "2 Tim" "2 Ti" "II Timothy" "II Tim" "II Ti" "2Timothy" "2Tim" "2Ti" "2nd Timothy" "2nd Tim" "Second Timothy" "Second Time")
+    ("Titus" "Tit"
+     ;; "ti"
+     )
+    ("Philemon" "Philem" "Phm" "Pm")
+    ("Hebrews" "Heb")
+    ("James" "Jas" "Jm")
+    ("I Peter" "1 Peter" "1 Pet" "1 Pe" "1 Pt" "1 P" "I Pet" "I Pt" "I Pe" "1Peter" "1Pet" "1Pe" "1Pt" "1P" "I Peter" "1st Peter" "First Peter")
+    ("II Peter" "2 Peter" "2 Pet" "2 Pe" "2 Pt" "2 P" "II Pet" "II Pt" "II Pe" "2Peter" "2Pet" "2Pe" "2Pt" "2P" "II Peter" "2nd Peter" "Second Peter")
+    ("I John" "1 John" "1 Jhn" "1 Jn" "1 J" "1John" "1Jhn" "1Joh" "1Jn" "1Jo" "1st John" "First John")
+    ("II John" "2 John" "2 Jhn" "2 Jn" "2 J" "2John" "2Jhn" "2Joh" "2Jn" "2Jo" "2nd John" "Second John")
+    ("III John" "3 John" "3 Jhn" "3 Jn" "3 J" "3John" "3Jhn" "3Joh" "3Jn" "3Jo" "3rd John" "Third John")
+    ("Jude" "Jud" "Jd")
+    ("Revelation of John" "Revelation" "Rev" "Re")))
 
 ;; | =M-j M-v= | =ace-link-bible-ref= | =pen-map=
 (defun ace-link-bible-ref ()
