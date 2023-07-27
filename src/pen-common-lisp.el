c4d2b65e37a356cae2d1f12aac2c0a5ab39f91f5
Shane Mulligan <mullikine@gmail.com>
2023-07-27 16:18:04 +1200

    27.07.23

--- src/pen-common-lisp.el
+++ src/pen-common-lisp.el
@@ -88,9 +88,9 @@
 
 ;; sly
 
-(require 'sly)
-
 (remove-hook lisp-mode-hook 'slime-lisp-mode-hook)
+
+(require 'sly)
 
 (define-key sly-editing-mode-map (kbd "M-p") nil)
 (define-key sly-editing-mode-map (kbd "M-n") nil)
