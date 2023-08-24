054aea5794323c51d3971bc0856eb6b984e96110
Shane Mulligan <mullikine@gmail.com>
2023-08-24 15:51:20 +1200

    24.08.23

--- src/pen-digraphs.el
+++ src/pen-digraphs.el
@@ -1,3 +1,22 @@
+(defun japanese-digraph-select ()
+  (interactive)
+  (let ((filter "(KATAKANA|HIRAGANA|BOPOMOFO|PARENTHESIZED IDEOGRAPH)"))
+    (if (interactive-p)
+        (funcall-interactively 'digraph-select filter)
+      (digraph-select filter))))
+
+(defun latin-digraph-select ()
+  (interactive)
+  (if (interactive-p)
+      (funcall-interactively 'digraph-select "LATIN")
+    (digraph-select "LATIN")))
+
+(defun cyrillic-digraph-select ()
+  (interactive)
+  (if (interactive-p)
+      (funcall-interactively 'digraph-select "CYRILLIC")
+    (digraph-select "CYRILLIC")))
+
 (defun greek-digraph-select ()
   (interactive)
   (if (interactive-p)
@@ -14,7 +33,7 @@
   (interactive)
   (let ((digraph (tpop (cmd "digraph-select" filter) nil
                        :output_b t
-                       :width_pc 80)))
+                       :width_pc 90)))
     (if (interactive-p)
         (if buffer-read-only
             (xc digraph)
