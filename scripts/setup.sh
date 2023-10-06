85dfc9503cf218e4b8ae0e92e03eef092c80d877
Shane Mulligan <mullikine@gmail.com>
2023-10-06 23:06:49 +1300

    06.10.23

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1043,8 +1043,9 @@ pyf yamlfmt
 # Currently not working. Apparantly, it's normal for the latest version to not work
 # Install the rust language server (rust-analyzer) manually (rather than through emacs)
 rm -f ~/.cargo/bin/rust-analyzer
-rm -f ~/.local/bin/rust-analyzer
 rustup toolchain install nightly --component rust-analyzer-preview
+
+rm -f ~/.local/bin/rust-analyzer
 
 (
 cd "$(gc "https://github.com/rust-analyzer/rust-analyzer")"
