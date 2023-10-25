e44ef88f61be6dab5ed9dcf686e78de1aaf11c49
Shane Mulligan <mullikine@gmail.com>
2023-10-25 13:58:27 +1300

    25.10.23

--- src/pen-support.el
+++ src/pen-support.el
@@ -717,47 +717,54 @@ b_output is (t/nil) tm_session is the session of the new tmux window"
 
 (defun eslugify (title &optional joinlines length)
   "Return the slug of NODE."
-  (let ((slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-                           768 ; U+0300 COMBINING GRAVE ACCENT
-                           769 ; U+0301 COMBINING ACUTE ACCENT
-                           770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
-                           771 ; U+0303 COMBINING TILDE
-                           772 ; U+0304 COMBINING MACRON
-                           774 ; U+0306 COMBINING BREVE
-                           775 ; U+0307 COMBINING DOT ABOVE
-                           776 ; U+0308 COMBINING DIAERESIS
-                           777 ; U+0309 COMBINING HOOK ABOVE
-                           778 ; U+030A COMBINING RING ABOVE
-                           779 ; U+030B COMBINING DOUBLE ACUTE ACCENT
-                           780 ; U+030C COMBINING CARON
-                           795 ; U+031B COMBINING HORN
-                           803 ; U+0323 COMBINING DOT BELOW
-                           804 ; U+0324 COMBINING DIAERESIS BELOW
-                           805 ; U+0325 COMBINING RING BELOW
-                           807 ; U+0327 COMBINING CEDILLA
-                           813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-                           814 ; U+032E COMBINING BREVE BELOW
-                           816 ; U+0330 COMBINING TILDE BELOW
-                           817 ; U+0331 COMBINING MACRON BELOW
-                           )))
-    (cl-flet* ((nonspacing-mark-p (char) (memq char slug-trim-chars))
-               (strip-nonspacing-marks (s) (string-glyph-compose
-                                            (apply #'string
-                                                   (seq-remove #'nonspacing-mark-p
-                                                               (string-glyph-decompose s)))))
-               (cl-replace (title pair) (replace-regexp-in-string (car pair) (cdr pair) title)))
-      (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-") ;; convert anything not alphanumeric
-                      ("__*" . "_")                   ;; remove sequential underscores
-                      ("--*" . "-")                   ;; remove sequential dashes
-                      ("^_" . "")                     ;; remove starting underscore
-                      ("_$" . "")))                   ;; remove ending underscore
-             (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs))
-             (slug (downcase slug))
-             (slug (if length
-                      (substring slug 0 (- length 1))
-                      slug)))
-        slug)
-      )))
+  (if (not joinlines)
+      (pen-list2str (mapcar
+                     (eval
+                      `(lambda
+                         (s)
+                         (eslugify s t ,length)))
+                     (pen-str2list title)))
+    (let ((slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+                             768  ; U+0300 COMBINING GRAVE ACCENT
+                             769  ; U+0301 COMBINING ACUTE ACCENT
+                             770  ; U+0302 COMBINING CIRCUMFLEX ACCENT
+                             771  ; U+0303 COMBINING TILDE
+                             772  ; U+0304 COMBINING MACRON
+                             774  ; U+0306 COMBINING BREVE
+                             775  ; U+0307 COMBINING DOT ABOVE
+                             776  ; U+0308 COMBINING DIAERESIS
+                             777  ; U+0309 COMBINING HOOK ABOVE
+                             778  ; U+030A COMBINING RING ABOVE
+                             779 ; U+030B COMBINING DOUBLE ACUTE ACCENT
+                             780 ; U+030C COMBINING CARON
+                             795 ; U+031B COMBINING HORN
+                             803 ; U+0323 COMBINING DOT BELOW
+                             804 ; U+0324 COMBINING DIAERESIS BELOW
+                             805 ; U+0325 COMBINING RING BELOW
+                             807 ; U+0327 COMBINING CEDILLA
+                             813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+                             814 ; U+032E COMBINING BREVE BELOW
+                             816 ; U+0330 COMBINING TILDE BELOW
+                             817 ; U+0331 COMBINING MACRON BELOW
+                             )))
+      (cl-flet* ((nonspacing-mark-p (char) (memq char slug-trim-chars))
+                 (strip-nonspacing-marks (s) (string-glyph-compose
+                                              (apply #'string
+                                                     (seq-remove #'nonspacing-mark-p
+                                                                 (string-glyph-decompose s)))))
+                 (cl-replace (title pair) (replace-regexp-in-string (car pair) (cdr pair) title)))
+        (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-") ;; convert anything not alphanumeric
+                        ("__*" . "_") ;; remove sequential underscores
+                        ("--*" . "-") ;; remove sequential dashes
+                        ("^_" . "")   ;; remove starting underscore
+                        ("_$" . ""))) ;; remove ending underscore
+               (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs))
+               (slug (downcase slug))
+               (slug (if length
+                         (substring slug 0 (- length 1))
+                       slug)))
+          slug)
+        ))))
 
 (defalias 'slugify 'eslugify)
 
