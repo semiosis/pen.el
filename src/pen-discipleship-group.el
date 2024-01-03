eace14b2f5764ca74b40cd133942fda100767df8
Shane Mulligan <mullikine@gmail.com>
2024-01-04 11:51:40 +1300

    Thu 04 Jan 2024 11:51:40 AM NZDT

--- src/pen-discipleship-group.el
+++ src/pen-discipleship-group.el
@@ -1,3 +1,7 @@
 (defun d-group-memorize-scripture ()
   (interactive)
   (dired (umn "$PEN/documents/notes/ws/discipleship-group/memorization")))
+
+(defun d-group-hear-journal ()
+  (interactive)
+  (dired (umn "$PEN/documents/notes/ws/discipleship-group/hear-journal")))
