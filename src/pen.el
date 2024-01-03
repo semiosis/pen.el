aa1a50d71ef7a45b99e3f6faf53f0b58bc499ab7
Shane Mulligan <mullikine@gmail.com>
2024-01-04 12:03:31 +1300

    Thu 04 Jan 2024 12:03:31 PM NZDT

--- src/pen.el
+++ src/pen.el
@@ -45,6 +45,15 @@
   ;; depend on backquote.el.
   (list 'function (cons 'lambda cdr)))
 
+(defmacro y (&rest cdr)
+  (declare (doc-string 2) (indent defun)
+           (debug (&define lambda-list lambda-doc
+                           [&optional ("interactive" interactive)]
+                           def-body)))
+  ;; Note that this definition should not use backquotes; subr.el should not
+  ;; depend on backquote.el.
+  (list 'function (cons 'lambda cdr)))
+
 (defmacro lm (&rest body)
   "Interactive lambda with no arguments."
   `(lambda () (interactive) ,@body))
