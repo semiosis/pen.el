1b171087fa15fa72a86646e7779e096ca73cb581
Shane Mulligan <mullikine@gmail.com>
2024-01-04 12:04:14 +1300

    Thu 04 Jan 2024 12:04:14 PM NZDT

--- src/pen.el
+++ src/pen.el
@@ -30,33 +30,7 @@
 (defun around-advice-disable-nil (proc &rest args)
   nil)
 
-;; Strangely, even these aliases caused lots of problems
-;; perhaps if I want to use λ then I should change the emacs C code
-;; (defalias 'λ 'lambda)
-;; (defalias 'y 'lambda)
-
-;; Or, perhaps this will work
-(defmacro λ (&rest cdr)
-  (declare (doc-string 2) (indent defun)
-           (debug (&define lambda-list lambda-doc
-                           [&optional ("interactive" interactive)]
-                           def-body)))
-  ;; Note that this definition should not use backquotes; subr.el should not
-  ;; depend on backquote.el.
-  (list 'function (cons 'lambda cdr)))
-
-(defmacro y (&rest cdr)
-  (declare (doc-string 2) (indent defun)
-           (debug (&define lambda-list lambda-doc
-                           [&optional ("interactive" interactive)]
-                           def-body)))
-  ;; Note that this definition should not use backquotes; subr.el should not
-  ;; depend on backquote.el.
-  (list 'function (cons 'lambda cdr)))
-
-(defmacro lm (&rest body)
-  "Interactive lambda with no arguments."
-  `(lambda () (interactive) ,@body))
+(require 'pen-lambda)
 
 (defmacro comment (&rest body) nil)
 ;; (defalias 'comment 'ignore)
