75635ba51161f51ec8190ac0941a72618205ca7b
Shane Mulligan <mullikine@gmail.com>
2024-01-04 14:16:13 +1300

    Thu 04 Jan 2024 02:16:13 PM NZDT

--- src/pen-discipleship-group.el
+++ src/pen-discipleship-group.el
@@ -6,6 +6,10 @@
   (interactive)
   (dired (umn "$PEN/documents/notes/ws/discipleship-group/hear-journal")))
 
+(defun d-group-hear-preparation ()
+  (interactive)
+  (dired (umn "$PEN/documents/notes/ws/discipleship-group/preparation")))
+
 ;; [[brain:agenda/Church::Discipleship group <2023-12-29 Fri 12:00 +1w>]]
 (defun d-group-agenda ()
   (interactive)
