499a29a9e02003c0830206a524a24397c83aac1f
Shane Mulligan <mullikine@gmail.com>
2024-01-05 22:52:02 +1300

    1704448322

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2107,6 +2107,7 @@ agi fp-compiler fp-ide fp-docs fp-units-base fp-units-db
 /usr/bin/x86_64-linux-gnu-fpcmkcfg-3.0.4
 
 # emacs lsp booster (for performance)
+# https://www.reddit.com/r/emacs/comments/18ybxsa/emacs_lspmode_performance_booster/?%24deep_link=true&correlation_id=866ffc1d-a52d-4ad9-8551-1c03ff8b282a&post_fullname=t3_18ybxsa&post_index=1&ref=email_digest&ref_campaign=email_digest&ref_source=email&%243p=e_as&_branch_match_id=1080262788655345442&_branch_referrer=H4sIAAAAAAAAA22QXWrDMBCET%2BO%2B2a4l27iFEAql11hkaZWI6g9pjdPbd900fSpIaPiGnR10Jcr1te8LGuOoUzl33sXPXuZzI0aZTwiqPrFMxV1cVB624k%2FXY6qRb4344LPve%2Fc7r1NgUPhiULryyyRgpEMOy9d6q%2Bphgq85JIOQsdhUgooaYU2pEh4BjeTs0SBmOBo18p3Kho2YdSoFvSKXIjjDfJlna%2FVgWjUJ047KvLTLNA3toJ%2BltcsqFsFL58zJYDfvowp4xEn4a3Q3XTR4Y2dgUNCy4qLOg3EXrHSHoFXIyl3i%2F25NW9H48BhuFECnSPwHTH%2FWkCOP382XFY15AQAA
 (
 cd "$(gc "https://github.com/blahgeek/emacs-lsp-booster")"
 cargo install --force --path .
