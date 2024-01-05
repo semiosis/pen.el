a44c921a7456dd9668fceb5d6fde0482b4da1bb8
Shane Mulligan <mullikine@gmail.com>
2024-01-06 12:54:44 +1300

    Sat 06 Jan 2024 12:54:44 PM NZDT

--- src/pen-eww.el
+++ src/pen-eww.el
@@ -1858,7 +1858,7 @@ instead of `browse-url-new-window-flag'."
 
 ;; Mirrors pages from a site
 ;; TODO Get this to use a URL initally instead of an eww buffer - I need a =eww= callback for this.
-;; TODO Also, get it to close the eww buffers once done
+;; TODO Also, get it to close the eww buffers once done mirroring a page along with its links
 (defun eww-open-all-links (&optional filter recursively mirrored)
   (interactive (list (read-string-hist "filter: " (concat "//" (unregexify
                                                                 (url-domain url-current-lastloc
