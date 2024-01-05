018a5835f4d6018b4bcdb88d1e89cffb4e5221ef
Shane Mulligan <mullikine@gmail.com>
2024-01-06 10:14:00 +1300

    Sat 06 Jan 2024 10:14:00 AM NZDT

--- src/pen-man.el
+++ src/pen-man.el
@@ -1,4 +1,5 @@
 ;; Don't use the script because it breaks when an info-page exists
+;; v +/"echo \"Info for" "$EMACSD/pen.el/scripts/container/man"
 ;; (setq manual-program "man")
 (setq manual-program "/usr/bin/man")
 
