096c5f4e406bb07b3e2a0a76b06b152d29d3cb20
Shane Mulligan <mullikine@gmail.com>
2024-01-05 17:35:34 +1300

    Fri 05 Jan 2024 05:35:34 PM NZDT

--- src/pen-calibre.el
+++ src/pen-calibre.el
@@ -141,7 +141,7 @@ Optional argument CANDIDATE is the selected item."
          (yn "Open org version?"))
         (find-file org-fp)
       (if (yn "Make org version?")
-          (tm-sps (f-join "any2org" org-fp))
+          (tm-sps (cmd "any2org" org-fp))
         (find-file fp)))))
 
 
