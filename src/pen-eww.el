25d9580a436b82977bc54d2254aa034f163c4a91
Shane Mulligan <mullikine@gmail.com>
2024-01-06 12:37:41 +1300

    Sat 06 Jan 2024 12:37:40 PM NZDT

--- src/pen-eww.el
+++ src/pen-eww.el
@@ -1857,8 +1857,10 @@ instead of `browse-url-new-window-flag'."
    (url (lg-eww url))))
 
 (defun eww-open-all-links (&optional filter)
-  (interactive (list (read-string-hist "filter: " (url-domain (get-path nil t)))))
-  (cl-loop for url in (pen-str2list (buffer-links))
+  (interactive (list (read-string-hist "filter: " (concat "^" (unregexify (url-domain (get-path nil t)))))))
+  (cl-loop for url in (-filter
+                       (eval `(lambda (s) (string-match ,filter s)))
+                       (pen-str2list (buffer-links)))
         do (progn
              (lg-eww url)
              (sleep 2))))
