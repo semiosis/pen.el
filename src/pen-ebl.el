645c8eb925cb21a996db7dbe47373a8f57cbe93c
Shane Mulligan <mullikine@gmail.com>
2024-01-06 23:08:00 +1300

    Sat 06 Jan 2024 11:08:00 PM NZDT

--- src/pen-ebl.el
+++ src/pen-ebl.el
@@ -1,5 +1,8 @@
 ;; EBL stands for Emacs-Lisp Bash
 ;; It should be able to generate bash (and maybe POSIX shell)
 
+;; Similar to this:
+;; - e:$EMACSD/pen.el/src/pen-epl.el
+;; - e:$EMACSD/pen.el/src/pen-epl-test.epl
 
 (provide 'pen-ebl)
