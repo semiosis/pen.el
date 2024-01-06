53c2eabc724a89adc382a2a7de9f88c6b859b65a
Shane Mulligan <mullikine@gmail.com>
2024-01-07 01:57:49 +1300

    Sun 07 Jan 2024 01:57:49 AM NZDT

--- src/pen-org.el
+++ src/pen-org.el
@@ -412,6 +412,8 @@ With argument N not nil or 1, move forward N - 1 lines first."
 ;; (advice-add 'fpvd-org-table-export :around #'advice-auto-yes)
 ;; (advice-remove 'fpvd-org-table-export #'advice-auto-yes)
 
+(advice-add 'org-table-export :around #'advise-to-yes)
+
 (advice-add 'kill-buffer :around #'advise-to-yes)
 ;; (advice-add 'fpvd-org-table-export :around #'advise-to-yes)
 
