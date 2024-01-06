b67da00202c33e39e518fb27685960be73909286
Shane Mulligan <mullikine@gmail.com>
2024-01-07 01:00:08 +1300

    Sun 07 Jan 2024 01:00:08 AM NZDT

--- src/pen-hyperdrive.el
+++ src/pen-hyperdrive.el
@@ -1,5 +1,5 @@
 (when (package-installed-p 'hyperdrive)
-       (global-set-key (kbd "C-c h") #'hyperdrive-menu)
+       (global-set-key (kbd "H-r") #'hyperdrive-menu)
        (hyperdrive-menu-bar-mode 1))
 
 (provide 'pen-hyperdrive)
