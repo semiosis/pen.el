b5b3964050fb677ea1730c16de5809394d3d2fac
Shane Mulligan <mullikine@gmail.com>
2024-01-08 15:07:34 +1300

    Mon 08 Jan 2024 03:07:34 PM NZDT

--- src/pen-addressbook.el
+++ src/pen-addressbook.el
@@ -121,6 +121,8 @@
 (define-key ebdb-mode-map (kbd "w r") nil)
 (define-key ebdb-mode-map (kbd "w") nil)
 
+(define-key ebdb-mode-map (kbd "w") 'get-path)
+
 (define-key ebdb-mode-map (kbd "M-W f") 'ebdb-copy-fields-as-kill)
 (define-key ebdb-mode-map (kbd "M-W m") 'ebdb-copy-mail-as-kill)
 (define-key ebdb-mode-map (kbd "M-W r") 'ebdb-copy-records-as-kill)
