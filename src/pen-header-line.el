d5d798dd83bb46ab5e345128c367679208f60320
Shane Mulligan <mullikine@gmail.com>
2024-01-09 23:35:10 +1300

    Tue 09 Jan 2024 11:35:10 PM NZDT

--- src/pen-header-line.el
+++ src/pen-header-line.el
@@ -3,6 +3,12 @@
 (require 'path-headerline-mode)
 
 (path-headerline-mode t)
+(path-headerline-mode -1)
+
+;; This places the modeline up the top, just below the tab bar
+;; but it has a different
+;; (mini-header-line-mode t)
+;; (mini-header-line-mode -1)
 
 (minibuffer-header-mode t)
 
