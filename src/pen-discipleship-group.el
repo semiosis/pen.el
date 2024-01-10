5b96ee4fdcc575a37d945cdfc50b92cc0718ae78
Shane Mulligan <mullikine@gmail.com>
2024-01-11 11:52:07 +1300

    1704927127

--- src/pen-discipleship-group.el
+++ src/pen-discipleship-group.el
@@ -61,7 +61,7 @@
 
 (defun d-group-linkify-bible-verse-ref (ref)
   (if (sor ref)
-      (concat "[[sh:tpop nem nasb " ref " ]]")
+      (concat "[[sh:tpop nem nasb " ref " ][" ref "]]")
     ""))
 
 ;; (d-group-weekly-scripture-memory-hear-journal-table-rows 5)
