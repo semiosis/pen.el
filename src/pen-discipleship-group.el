ddc4af1fd0237194919dffd4ae4143c42f16a03a
Shane Mulligan <mullikine@gmail.com>
2024-01-11 11:06:24 +1300

    Thu 11 Jan 2024 11:06:24 AM NZDT

--- src/pen-discipleship-group.el
+++ src/pen-discipleship-group.el
@@ -60,7 +60,8 @@
                         d-group-weekly-scripture-memory-nt260))))
 
 (defun d-group-weekly-scripture-memory-hear-journal-table-rows ()
-  "| [[sh:tpop nem nasb `(d-group-get-weekly-scripture-memory)`]] | [[sh:tpop nem nasb `(d-group-get-weekly-reading)`]] |")
+  (yasnippet-ex)
+  (pen-yas-expand-string "| [[sh:tpop nem nasb `(d-group-get-weekly-scripture-memory)`]] | [[sh:tpop nem nasb `(d-group-get-weekly-reading)`]] |"))
 
 (defun d-group-weekly-journal ()
   (interactive)
