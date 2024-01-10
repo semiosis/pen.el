48a59dcfdb142a9b98b1c7cd5b5aaa2659c5d7e8
Shane Mulligan <mullikine@gmail.com>
2024-01-11 10:41:40 +1300

    Thu 11 Jan 2024 10:41:40 AM NZDT

--- src/pen-discipleship-group.el
+++ src/pen-discipleship-group.el
@@ -39,7 +39,8 @@
                  "Romans 1-3"))))
 
 (defun d-group-get-weekly-reading ()
-  (list2str (cdr (assoc (date-week-number)
+  ;; Since I'm reading for the next week, do a +1
+  (list2str (cdr (assoc (+ 1 (date-week-number))
                         d-group-weekly-readings-nt260))))
 
 (defset d-group-weekly-scripture-memory-nt260
