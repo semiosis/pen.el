1fb02c4f90e6e6efc691a1a2b86ee9ef274f7886
Shane Mulligan <mullikine@gmail.com>
2024-01-11 13:28:51 +1300

    1704932931

--- src/pen-lm-completers.el
+++ src/pen-lm-completers.el
@@ -1,32 +1,34 @@
 (require 'pen-openai)
 
 (defset pen-completion-backends
+        '()
   ;; name completion command and a list of engines
-  `(("OpenAI GPT-3"
-     "openai-complete.sh"
-     ,(pen-openai-list-engines)
-     "available")
+  ;;`(("OpenAI GPT-3"
+  ;;   "openai-complete.sh"
+  ;;   ,(pen-openai-list-engines)
+  ;;   "available")
 
-    ("EleutherAI GPT-J API from AIX"
-     "aix-complete.sh"
-     ("GPT-J-6B")
-     "available")
+  ;;  ("EleutherAI GPT-J API from AIX"
+  ;;   "aix-complete.sh"
+  ;;   ("GPT-J-6B")
+  ;;   "available")
 
-    ("EleutherAI GPT-J"
-     "gpt-j-complete.sh"
-     ("6B"))
+  ;;  ("EleutherAI GPT-J"
+  ;;   "gpt-j-complete.sh"
+  ;;   ("6B"))
 
-    ("EleutherAI GPT-Neo"
-     "gpt-neo-complete.sh"
-     ("2.7B" "1.3B"))
+  ;;  ("EleutherAI GPT-Neo"
+  ;;   "gpt-neo-complete.sh"
+  ;;   ("2.7B" "1.3B"))
 
-    ("EleutherAI GPT-NeoX"
-     "gpt-neox-complete.sh"
-     ("175B"))
+  ;;  ("EleutherAI GPT-NeoX"
+  ;;   "gpt-neox-complete.sh"
+  ;;   ("175B"))
 
-    ("booste"
-     "booste"
-     ("GPT2" "GPT2-XL"))))
+  ;;  ("booste"
+  ;;   "booste"
+  ;;   ("GPT2" "GPT2-XL")))
+  )
 
 (defun pen-select-completion-backend ()
   (interactive)
