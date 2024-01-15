3ff4cd7b9de6c176dd288c7da8f9d508e844d49c
Shane Mulligan <mullikine@gmail.com>
2024-01-15 22:03:45 +1300

    Mon 15 Jan 2024 10:03:45 PM NZDT

--- src/pen-ledger.el
+++ src/pen-ledger.el
@@ -23,4 +23,28 @@
 ;; I think I will start with ledger and move to hledger
 ;; This is so I can learn ledger from the info page
 
+;;; Basic configuration
+(require 'hledger-mode)
+
+;; To open files with .journal extension in hledger-mode
+(add-to-list 'auto-mode-alist '("\\.journal\\'" . hledger-mode))
+
+;; Provide the path to you journal file.
+;; The default location is too opinionated.
+(setq hledger-jfile "/path/to/your/journal-file.journal")
+
+;;; Auto-completion for account names
+;; For company-mode users,
+(add-to-list 'company-backends 'hledger-company)
+
+;; For auto-complete users,
+(add-to-list 'ac-modes 'hledger-mode)
+(add-hook 'hledger-mode-hook
+    (lambda ()
+        (setq-local ac-sources '(hledger-ac-source))))
+
+;; For easily adjusting dates.
+(define-key hledger-mode-map (kbd "<kp-add>") 'hledger-increment-entry-date)
+(define-key hledger-mode-map (kbd "<kp-subtract>") 'hledger-decrement-entry-date)
+
 (provide 'pen-ledger)
