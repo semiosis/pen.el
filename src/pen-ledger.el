737a19319af40921585c30920b471c927578c2f8
Shane Mulligan <mullikine@gmail.com>
2024-01-15 20:49:25 +1300

    Mon 15 Jan 2024 08:49:25 PM NZDT

--- src/pen-ledger.el
+++ src/pen-ledger.el
@@ -2,12 +2,12 @@
 (require 'ledger-import)
 (require 'flycheck-ledger)
 (require 'company-ledger)
+(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
 
 ;; https://hledger.org/
 (require 'hledger-mode)
 (require 'flymake-hledger)
 (require 'flycheck-hledger)
-
 
 ;; I should be using hledger, not ledger
 ;; https://hledger.org/ledger.html
@@ -15,10 +15,5 @@
 
 ;; https://github.com/ledger/ledger
 ;; https://www.ledger-cli.org/
-
-;; (autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
-;; (add-to-list 'load-path (expand-file-name "/path/to/ledger/source/lisp/"))
-
-(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
 
 (provide 'pen-ledger)
