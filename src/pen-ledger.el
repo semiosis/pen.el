be56e0406d1a8c69489381ef4272f0a7aa151adb
Shane Mulligan <mullikine@gmail.com>
2024-01-15 20:43:50 +1300

    Mon 15 Jan 2024 08:43:50 PM NZDT

--- src/pen-ledger.el
+++ src/pen-ledger.el
@@ -1,10 +1,10 @@
-;; (require 'ledger-mode)
-;; (require 'ledger-import)
-;; (require 'hledger-mode)
-;; (require 'flymake-hledger)
-;; (require 'flycheck-ledger)
-;; (require 'flycheck-hledger)
-;; (require 'company-ledger)
+(require 'ledger-mode)
+(require 'ledger-import)
+(require 'hledger-mode)
+(require 'flymake-hledger)
+(require 'flycheck-ledger)
+(require 'flycheck-hledger)
+(require 'company-ledger)
 
 ;; https://github.com/ledger/ledger
 ;; https://www.ledger-cli.org/
