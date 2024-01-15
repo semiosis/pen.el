da5c4b3d2ac074e51ae0d468a82b177bf417c413
Shane Mulligan <mullikine@gmail.com>
2024-01-15 20:51:58 +1300

    Mon 15 Jan 2024 08:51:58 PM NZDT

--- src/pen-ledger.el
+++ src/pen-ledger.el
@@ -4,6 +4,8 @@
 (require 'company-ledger)
 (add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
 
+;; [[info:(ledger-mode) Top]]
+
 ;; https://hledger.org/
 (require 'hledger-mode)
 (require 'flymake-hledger)
@@ -12,6 +14,9 @@
 ;; I should be using hledger, not ledger
 ;; https://hledger.org/ledger.html
 
+;; But there is no info page for hledger
+
+;; https://hledger.org/1.32/hledger.html
 
 ;; https://github.com/ledger/ledger
 ;; https://www.ledger-cli.org/
