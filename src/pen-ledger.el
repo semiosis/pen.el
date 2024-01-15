d90f2a3b034ef15ee59c8aef072375ea14a2b415
Shane Mulligan <mullikine@gmail.com>
2024-01-15 20:44:33 +1300

    Mon 15 Jan 2024 08:44:33 PM NZDT

--- src/pen-ledger.el
+++ src/pen-ledger.el
@@ -10,5 +10,10 @@
 ;; https://github.com/ledger/ledger
 ;; https://www.ledger-cli.org/
 
+(autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
+
+;; (add-to-list 'load-path (expand-file-name "/path/to/ledger/source/lisp/"))
+
+(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
 
 (provide 'pen-ledger)
