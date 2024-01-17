b7bf535156fd80121501ecb047e48e9a1434af99
Shane Mulligan <mullikine@gmail.com>
2024-01-17 13:32:18 +1300

    Wed 17 Jan 2024 01:32:17 PM NZDT

--- src/pen-discipleship-group.el
+++ src/pen-discipleship-group.el
@@ -101,7 +101,7 @@
                           (str2lines (d-group-get-weekly-reading-nt week))
                           (str2lines (d-group-get-weekly-reading-ot week)))
          collect
-         (concat "| " (d-group-linkify-bible-verse-ref (car row) t) " | " (d-group-linkify-bible-verse-ref (cadr row)) " |")))
+         (concat "| " (d-group-linkify-bible-verse-ref (car row) t) " | " (d-group-linkify-bible-verse-ref (cadr row)) " | " (d-group-linkify-bible-verse-ref (cadr row)) " |")))
   ;; (pen-yas-expand-string "| [[sh:tpop nem nasb `(d-group-get-weekly-scripture-memory)`]] | [[sh:tpop nem nasb `(d-group-get-weekly-reading)`]] |")
   )
 
