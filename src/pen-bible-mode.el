4a7fc4be16a9483e6aeda102231fa2f111043e3c
Shane Mulligan <mullikine@gmail.com>
2024-01-21 23:16:56 +1300

    Sun 21 Jan 2024 11:16:56 PM NZDT

--- src/pen-bible-mode.el
+++ src/pen-bible-mode.el
@@ -1657,8 +1657,8 @@ produced by `bible-mode-exec-diatheke'. Outputs text to active buffer with prope
 
 (define-key bible-mode-map (kbd "F") 'bible-mode-fast-toggle)
 
-;; (get-chapter-title "Genesis" "30")
-;; (get-chapter-title "Leviticus" "6")
+;; (bible-get-chapter-title "Genesis" "30")
+;; (bible-get-chapter-title "Leviticus" "6")
 (defun bible-get-chapter-title (book chapter)
   (interactive (list (read-string "Book: ")
                      (read-string "Chapter: ")))
