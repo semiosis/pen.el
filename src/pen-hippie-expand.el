b4d8bccf5207db484fb83684c5393c3ea16cf56d
Shane Mulligan <mullikine@gmail.com>
2024-01-23 04:01:26 +1300

    Tue 23 Jan 2024 04:01:26 AM NZDT

--- src/pen-hippie-expand.el
+++ src/pen-hippie-expand.el
@@ -6,6 +6,7 @@
 (setq hippie-expand-try-functions-list '(try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-line try-complete-lisp-symbol-partially try-complete-lisp-symbol))
 
 (defun pen-hippie-expand ()
+  (interactive)
   (if
       (and
        (major-mode-p 'emacs-lisp-mode)
