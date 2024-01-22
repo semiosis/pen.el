697715771f95c02252701e495291e7bcb6a3fa2e
Shane Mulligan <mullikine@gmail.com>
2024-01-23 04:00:54 +1300

    Tue 23 Jan 2024 04:00:54 AM NZDT

--- src/pen-hippie-expand.el
+++ src/pen-hippie-expand.el
@@ -5,4 +5,13 @@
 
 (setq hippie-expand-try-functions-list '(try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-line try-complete-lisp-symbol-partially try-complete-lisp-symbol))
 
+(defun pen-hippie-expand ()
+  (if
+      (and
+       (major-mode-p 'emacs-lisp-mode)
+       (string-equal " " (thing-at-point 'char))
+       (not (thing-at-point 'symbol)))
+      (call-interactively 'elisp-complete-interactive-arg)
+    (call-interactively 'hippie-expand)))
+
 (provide 'pen-hippie-expand)
