ab413fcef1d30a72d868c308cd63f0fba92287b5
Shane Mulligan <mullikine@gmail.com>
2024-01-22 23:22:06 +1300

    Mon 22 Jan 2024 11:22:06 PM NZDT

--- src/pen-bible-mode.el
+++ src/pen-bible-mode.el
@@ -1645,6 +1645,7 @@ produced by `bible-mode-exec-diatheke'. Outputs text to active buffer with prope
     ;; (-find (lambda (e) (string-equal "Leviticus" (car e))) bible-mode-book-chapters)
     ;; (-find-index (lambda (e) (string-equal "Leviticus" (car e))) bible-mode-book-chapters)
 
+    ;; Interestingly, this lambda has access to book
     (+ 1 (-find-index (lambda (e) (string-equal book (car e))) bible-mode-book-chapters))
     ;; Get the index
     ;; verse
