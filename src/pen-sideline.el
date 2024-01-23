14cc439e6c0a004f82d58179318d5a93b1c9e1c8
Shane Mulligan <mullikine@gmail.com>
2024-01-23 16:57:39 +1300

    1705982259

--- src/pen-sideline.el
+++ src/pen-sideline.el
@@ -1,5 +1,7 @@
-sideline
-sideline-lsp
-sideline-flymake
-sideline-flycheck
-sideline-blame
+(require 'sideline)
+(require 'sideline-lsp)
+(require 'sideline-flymake)
+(require 'sideline-flycheck)
+(require 'sideline-blame)
+
+(provide 'sideline)
