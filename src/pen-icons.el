6894dcaaeed68c2adcd1b7df0a6a1fe6bcf522bf
Shane Mulligan <mullikine@gmail.com>
2024-01-24 00:23:43 +1300

    1706009023

--- src/pen-icons.el
+++ src/pen-icons.el
@@ -1,5 +1,8 @@
 (require 'icons)
 
+;; (setq icon-preference
+;;       '(image emoji symbol text))
+;; This is so that in the GUI, plain text is preferred to iconss
 (setq icon-preference
       '(text image emoji symbol))
 
