eca1a5001b2871ce0aac345c5d82a1eff95882a9
Shane Mulligan <mullikine@gmail.com>
2024-01-23 14:16:23 +1300

    Tue 23 Jan 2024 02:16:23 PM NZDT

--- src/pen-bible-audio.el
+++ src/pen-bible-audio.el
@@ -1,6 +1,6 @@
 (defset bible-audio-dir (umn "$PEN/bible-audio"))
 
 ;; TODO Start and stop an audio recording, with a time limit on it.
-;; Handle the audio recording process.
+;; Handle the audio recording process, and send a SIGINT when done.
 
 (provide 'pen-bible-audio)
