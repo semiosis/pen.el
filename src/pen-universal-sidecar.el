cf66c0685dc01751d3b5ad2a5cd24615808f687c
Shane Mulligan <mullikine@gmail.com>
2024-01-23 17:05:06 +1300

    Tue 23 Jan 2024 05:05:06 PM NZDT

--- src/pen-universal-sidecar.el
+++ src/pen-universal-sidecar.el
@@ -1,3 +1,6 @@
-
+(require 'universal-sidecar)
+(require 'universal-sidecar-roam)
+(require 'universal-sidecar-elfeed-score)
+(require 'universal-sidecar-elfeed-related)
 
 (provide 'pen-universal-sidecar)
