3cd0130e4567ffb0aefe09f30def8b44763bad51
Shane Mulligan <mullikine@gmail.com>
2024-01-23 17:17:14 +1300

    Tue 23 Jan 2024 05:17:14 PM NZDT

--- src/pen-universal-sidecar.el
+++ src/pen-universal-sidecar.el
@@ -6,13 +6,18 @@
 (setq universal-sidecar-buffer-name-format
       '"*sidecar* (%F)")
 
+;; This is to make it work inside a terminal as well
 (defun universal-sidecar-get-name (&optional frame)
   "Get the name of the sidecar buffer for FRAME.
 
 If FRAME is nil, use `selected-frame'."
   (let* ((frame (or frame (selected-frame)))
-         (id (frame-parameter frame 'window-id)))
-    (format-spec universal-sidecar-buffer-name-format (list (cons ?F id)))))
+         (id (frame-parameter frame 'window-id))
+         (client (frame-parameter frame 'client)))
+    ;; (tv (frame-parameters frame))
+    (format-spec universal-sidecar-buffer-name-format (list (cons ?F
+                                                                  (or id
+                                                                      client))))))
 
 (add-to-list 'universal-sidecar-sections 'buffer-git-status)
 ;; However, if we want the opposite behavior (don't show renames),
