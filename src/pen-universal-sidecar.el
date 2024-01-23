8171d1160fc829da8508b607edf919b5f4fdb57b
Shane Mulligan <mullikine@gmail.com>
2024-01-23 17:07:46 +1300

    Tue 23 Jan 2024 05:07:46 PM NZDT

--- src/pen-universal-sidecar.el
+++ src/pen-universal-sidecar.el
@@ -3,6 +3,11 @@
 (require 'universal-sidecar-elfeed-score)
 (require 'universal-sidecar-elfeed-related)
 
+(add-to-list 'universal-sidecar-sections 'buffer-git-status)
+;; However, if we want the opposite behavior (don't show renames),
+;; we'd configure it as shown below.
+;; (add-to-list 'universal-sidecar-sections '(buffer-git-status :show-renames t))
+
 (add-to-list 'universal-sidecar-sections
              '(universal-sidecar-roam-section org-roam-backlinks-section))
 
