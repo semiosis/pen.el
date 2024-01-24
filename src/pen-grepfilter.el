735843a04f7695805d5bed7cbb6ddf580d79d86b
Shane Mulligan <mullikine@gmail.com>
2024-01-24 20:44:53 +1300

    Wed 24 Jan 2024 08:44:53 PM NZDT

---
+++ src/pen-grepfilter.el
@@ -0,0 +1,70 @@
+;; e:$EMACSD/pen.el/scripts/grepfilter-transform.els
+
+(defun grepfilter-transform (input-fp matches-fp bytepos-fp transformed-fp)
+  (interactive (list (read-string "Input fp: ")
+                     (read-string "Matches fp: ")
+                     (read-string "Bytepos fp: ")
+                     (read-string "Transformed fp: ")))
+
+  (let* ((input (e/cat input-fp))
+         (matches-list
+          (-filter-not-empty-string
+           (s-lines (e/cat matches-fp))))
+         (transformed-list
+          (-filter-not-empty-string
+           (s-lines (e/cat transformed-fp))))
+         (transformation-list
+          (-zip matches-list transformed-list))
+         (bytepos-list
+          ;; I need a temp buffer to recalculate byte positions
+          (with-temp-buffer
+            (insert input)
+            (beginning-of-buffer)
+
+            (mapcar
+             (lambda (s)
+               (let* ((bytepos (s-replace-regexp "^[^:]+:[0-9]+:\\([0-9]+\\).*" "\\1" s))
+                      (matchstr (s-replace-regexp "^[^:]+:[0-9]+:[0-9]+:\\(.*\\).*" "\\1" s))
+                      (len (length matchstr)))
+
+                 (list (byte-position (string-to-number bytepos)) len matchstr)))
+             (-filter-not-empty-string
+              (s-lines (e/cat bytepos-fp))))))
+
+         ;; (temp
+         ;;  (tv (pps bytepos-list)))
+
+         ;; Don't use blanked input - it was a bad idea
+         ;; instead, simply make the replacements
+         ;; and keep a byte offset as I go.
+         (transformed-input
+          (with-temp-buffer
+            (insert input)
+            (beginning-of-buffer)
+            (let ((offset 0))
+              (cl-loop for bytepos in bytepos-list
+                       do
+                       (let* ((beg (+ offset
+                                      ;; byte-position crashes the whole script if its variable is too big for the function
+                                      (car bytepos)))
+                              (len (car (cdr bytepos)))
+                              (end (+ beg len))
+                              (substr (buffer-substring beg end))
+                              (replacement (cdr (assoc substr transformation-list)))
+                              (diff (- (length replacement) len)))
+
+                         (goto-char beg)
+                         (delete-region beg end)
+                         (insert replacement)
+
+                         (setq offset (+ offset diff)))))
+            (buffer-string)))
+
+
+         (output
+          transformed-input
+          ))
+
+    output))
+
+(provide 'pen-grepfilter)
