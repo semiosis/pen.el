7ad0ae869a055b4a6d8b2df28c8d0e609396c8fc
Shane Mulligan <mullikine@gmail.com>
2024-01-24 22:33:57 +1300

    1706088837

--- scripts/setup-term.sh
+++ scripts/setup-term.sh
@@ -62,7 +62,7 @@ export PATH=$EMACSD/host/pen.el/scripts/container:$EMACSD/pen.el/scripts/contain
 export PATH="$PATH:/root/go/bin"
 export PATH="$PATH:/root/.cargo/bin/cargo"
 export PATH="$PATH:/root/repos/go-ethereum/build/bin"
-export PATH="$PATH:$(find "$SCRIPTS" -maxdepth 1 -mindepth 1 -type d | sed -z "s~\n~:~g" | sed "s/:\$//")"
+export PATH="$PATH:$(find "$SCRIPTS" -maxdepth 4 -mindepth 1 -type d | sed -z "s~\n~:~g" | sed "s/:\$//")"
 
 # For the host:
 # export PATH="$PATH:$(find "$SCRIPTS" -maxdepth 1 -mindepth 1 -type d | grep -v '/\.git' | sed -z "s~\n~:~g" | sed "s/:\$//")"
