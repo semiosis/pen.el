4bfba5a3cddbfdeb0693895a2388afd2cfc579a4
Shane Mulligan <mullikine@gmail.com>
2024-01-25 19:46:47 +1300

    Thu 25 Jan 2024 07:46:47 PM NZDT

--- src/pen-buffers.el
+++ src/pen-buffers.el
@@ -3,6 +3,10 @@
 ;; (setq kill-buffer-query-functions
 ;;       '(pen-dont-kill-scratch))
 
+(setq kill-buffer-query-functions
+      '(pen-dont-kill-scratch))
+
+;; Frustratingly, this makes the 'File saved' message disappear quickly.
 (add-hook 'kill-buffer-query-functions
           (lambda () (not-modified) t))
 
