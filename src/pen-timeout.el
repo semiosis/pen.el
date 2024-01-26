874fdfc80f5d13e45b3b37b455f4bc2bfd258b6d
Shane Mulligan <mullikine@gmail.com>
2024-01-27 12:23:06 +1300

    Sat 27 Jan 2024 12:23:05 PM NZDT

--- src/pen-timeout.el
+++ src/pen-timeout.el
@@ -3,6 +3,7 @@
 ;; This function may run at most once every 2 seconds.
 ;; It also memoizes, which is good.
 (timeout-throttle! 'ph-get-path-string 2.0)
+(timeout-throttle! 'pen-compose-mode-line 1.0)
 
 (comment
  (defun test-throttle-fun ()
