3e6a94badc1c138099f4b9323f8cb5384e0c31d4
Shane Mulligan <mullikine@gmail.com>
2024-01-26 15:41:17 +1300

    Fri 26 Jan 2024 03:41:17 PM NZDT

---
+++ src/pen-completing-read.el
@@ -0,0 +1,38 @@
+(setq lexical-binding t)
+
+(defun dogs-filter (seq)
+  (lambda (str pred flag)
+    (pcase flag
+      ('metadata
+       (list 'metadata
+             (cons 'annotation-function
+                   (lambda (c)
+                     (format "\n\t%s" (alist-get c seq nil nil #'string=))))))
+      ('t
+       (if (string-blank-p str)
+           (all-completions str seq)
+         (all-completions
+          str
+          (lambda (s _ _)
+            (seq-map
+             #'car
+             (seq-filter
+              (lambda (x)
+                (unless (string-blank-p str)
+                  (or
+                   (s-contains-p str (car x) :ignore-case)
+                   (s-contains-p str (cdr x) :ignore-case))))
+              seq)))))))))
+
+(let* ((coll '(("Affenpinscher" .  "Loyal and amusing")
+               ("Akita" . "Ancient Japanese")
+               ("Bulldog" . "Kind but courageous")
+               ("Caucasian Shepherd" . "Serious guarding breed")
+               ("Miniature Schnauzer" . "Long-lived and low-shedding"))))
+
+  (completing-read
+   "Select a breed: "
+   (dogs-filter coll)))
+
+
+(provide 'pen-completing-read)
