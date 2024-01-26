16a7c493896d447b9c6b6b70bc22e3aa168e7f29
Shane Mulligan <mullikine@gmail.com>
2024-01-26 16:08:31 +1300

    Fri 26 Jan 2024 04:08:31 PM NZDT

--- src/pen-support.el
+++ src/pen-support.el
@@ -1090,7 +1090,7 @@ This also exports PEN_PROMPTS_DIR, so lm-complete knows where to find the .promp
     (when item
       ;; (concat " # " (second item))
       (cond
-       ((listp item) (concat " # " (str (second item))))
+       ((consp item) (concat " # " (str (cdr item))))
        ((stringp item) "")
        (t "")))))
 
