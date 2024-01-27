0634ed21f306aa341e833fc81289d620099dbd8b
Shane Mulligan <mullikine@gmail.com>
2024-01-27 14:19:40 +1300

    Sat 27 Jan 2024 02:19:40 PM NZDT

--- src/pen-profiler.el
+++ src/pen-profiler.el
@@ -1,5 +1,9 @@
+;; This should be for debugging and profiling
+
 (define-key pen-map (kbd "s-P P") 'profiler-start)
 (define-key pen-map (kbd "s-P S") 'profiler-stop)
 (define-key pen-map (kbd "s-P R") 'profiler-report)
+
+(define-key pen-map (kbd "s-D D") 'debug-on-entry)
 
 (provide 'pen-profiler)
