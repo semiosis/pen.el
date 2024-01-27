a1266c871d29ffdc4922d364773246cdcadd2306
Shane Mulligan <mullikine@gmail.com>
2024-01-27 13:35:13 +1300

    Sat 27 Jan 2024 01:35:13 PM NZDT

--- src/pen-timeout.el
+++ src/pen-timeout.el
@@ -8,6 +8,7 @@
 (timeout-throttle! 'ph-get-path-string 2.0)
 (timeout-throttle! 'pen-compose-mode-line 1.0)
 (timeout-throttle! 'pen-redraw-glossary-buttons-when-window-scrolls-or-file-is-opened 2.0)
+(timeout-throttle! 'git-gutter+-refresh 2.0)
 
 (comment
  (timeout-throttle! 'git-gutter+-refresh 2.0)
