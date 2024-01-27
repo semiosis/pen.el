02ef37afee3498c3b77af7a20c12af2bd3667717
Shane Mulligan <mullikine@gmail.com>
2024-01-27 18:42:53 +1300

    1706334173

--- src/pen-universal-sidecar.el
+++ src/pen-universal-sidecar.el
@@ -70,7 +70,7 @@ If FRAME is nil, use `selected-frame'."
                     (and file
                          (format "Demo: %s" file))
                     "Crossreferences"))
-         (crossrefs (snc "in-pen bible-get-cross-references | wrlp cif bible-canonicalise-cross-reference | sed 's/^\\(.*\\)/[[bible:\\1]]'"
+         (crossrefs (snc "in-pen bible-get-cross-references | wrlp cif bible-canonicalise-cross-reference | sed 's/^\\(.*\\)/[[bible:\\1]]/'"
                          (concat (car ref-tuple) " " (cadr ref-tuple) ":" (caddr ref-tuple))))
          (cmdout (shell-command-to-string "pwd")))
     (universal-sidecar-insert-section demo-section title
