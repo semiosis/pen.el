e25d6dff51c78752f049c24c08acec6a1e868d8a
Shane Mulligan <mullikine@gmail.com>
2024-01-27 13:15:05 +1300

    Sat 27 Jan 2024 01:15:05 PM NZDT

--- src/pen-timeout.el
+++ src/pen-timeout.el
@@ -6,10 +6,10 @@
 (timeout-throttle! 'pen-compose-mode-line 1.0)
 (timeout-throttle! 'pen-redraw-glossary-buttons-when-window-scrolls-or-file-is-opened 2.0)
 
-(provide 'pen-timeout)
-
 (comment
  (defun test-throttle-fun ()
    (message (vime "strftime(\"%c\")")))
  (timeout-throttle! 'test-throttle-fun 2.0)
  (test-throttle-fun))
+
+(provide 'pen-timeout)
