f17c548ca588d21bf9515430513743981bff9453
Shane Mulligan <mullikine@gmail.com>
2024-01-27 21:43:34 +1300

    Sat 27 Jan 2024 09:43:34 PM NZDT

---
+++ src/pen-line-changed.el
@@ -0,0 +1,20 @@
+(defvar current-line-number (line-number-at-pos))
+
+(defvar changed-line-hook nil)
+
+(defun update-line-number ()
+  (let ((new-line-number (line-number-at-pos)))
+    (when (not (equal new-line-number current-line-number))
+      (setq current-line-number new-line-number)
+      (run-hooks 'changed-line-hook))))
+
+(add-hook 'post-command-hook #'update-line-number)
+
+;; Example of hook usage
+
+(defun my-line-func ()
+  (message "This is the current line: %s" current-line-number))
+
+(add-hook 'changed-line-hook #'my-line-func)
+
+(provide 'pen-line-changed)
