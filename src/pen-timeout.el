32487e4a0f341b14570645ba5fa8adea6a41318a
Shane Mulligan <mullikine@gmail.com>
2024-01-27 13:30:22 +1300

    Sat 27 Jan 2024 01:30:22 PM NZDT

--- src/pen-timeout.el
+++ src/pen-timeout.el
@@ -1,11 +1,15 @@
 (require 'timeout)
 
+(defun timeout-unthrottle! (func)
+  (timeout-throttle! func 0))
+
 ;; This function may run at most once every 2 seconds.
 ;; It also memoizes, which is good.
 (timeout-throttle! 'ph-get-path-string 2.0)
 (timeout-throttle! 'pen-compose-mode-line 1.0)
 (timeout-throttle! 'pen-redraw-glossary-buttons-when-window-scrolls-or-file-is-opened 2.0)
 (timeout-throttle! 'git-gutter+-refresh 2.0)
+(timeout-unthrottle! 'git-gutter+-refresh)
 
 (comment
  (defun test-throttle-fun ()
