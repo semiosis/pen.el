a5a7f542cf14fd71d526631911150a79e497961c
Shane Mulligan <mullikine@gmail.com>
2024-02-09 21:26:14 +1300

    09.02.24

--- src/pen-grepfilter.el
+++ src/pen-grepfilter.el
@@ -1,7 +1,7 @@
 ;; e:$EMACSD/pen.el/scripts/grepfilter-transform.els
 
 ;; Two types of filters
-;; - filters ($EMACSD/pen.el/config/filters.sh)
+;; - filters ($EMACSD/pen.el/config/filters/filters.sh)
 ;;   - extract
 ;;   - transform
 
