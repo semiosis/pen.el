9d4716a9bfa5ab9ca6f436da2bdccbe15d3b88fb
Shane Mulligan <mullikine@gmail.com>
2024-02-17 17:37:58 +1300

    17.02.24

--- src/pen-hypertext.el
+++ src/pen-hypertext.el
@@ -1,3 +1,7 @@
+;; v +/"hypertext-mode" "$EMACSD/pen.el/src/pen-manage-minor-mode.el"
+
+;; https://dantorop.info/project/emacs-animation/
+
 (define-derived-mode hypertext-mode fundamental-mode "Hypertext"
   "Mode for clickable ascii text graphics.
 \\{hypertext-mode-map}"
