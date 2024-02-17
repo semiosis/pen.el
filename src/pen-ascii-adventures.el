54459175772313efaa53b48c6e739bc57b4fd45f
Shane Mulligan <mullikine@gmail.com>
2024-02-17 22:58:43 +1300

    Sat 17 Feb 2024 10:58:43 PM NZDT

--- src/pen-ascii-adventures.el
+++ src/pen-ascii-adventures.el
@@ -14,6 +14,7 @@
 ;;     - And they don't need to be discovered.
 ;;       - Pretty much everything should be visible.
 ;; - It's like a super elaborate Magic School Bus exploration game.
+;; - Just go from area to area.
 
 
 ;; Design decisions:
