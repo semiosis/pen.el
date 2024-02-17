f55bfe7a2dac5d55ef53cc074908c37f89cbec4d
Shane Mulligan <mullikine@gmail.com>
2024-02-17 19:38:49 +1300

    17.02.24

--- src/pen-ascii-adventures.el
+++ src/pen-ascii-adventures.el
@@ -11,23 +11,38 @@ A map should mainly simply connect places together.
   (message "%s" "Go to the map"))
 
 ;; Yeah, call it a 'place'.
+;; [[info:(eieio) Slot Options]]
 (defclass place ()                      ; No superclasses
   ((name :initarg :name
          :initform ""
          :type string
          :custom string
          :documentation "An area.")
-   (animation-speed :initarg :birthday
+   (animation-speed :initarg :timer
                     :initform 1
                     :custom integer
                     :type integer
                     :documentation "The animation speed.")
-   (frames :initarg :phone
-           :initform ""
-           :documentation "Phone number.")
-   (cash :initarg :phone
-         :initform ""
-         :documentation "Phone number."))
-  "A class for tracking people I know.")
+   ;; (frames :initarg :phone
+   ;;         :initform ""
+   ;;         :documentation "Phone number.")
+   )
+  "A class for describing an area.")
+
+;; Here, area is a variable
+(cl-defmethod aa/goto-place ((area place) &optional scriptname)
+  "Dial the phone for the person PERS.
+     Execute the program SCRIPTNAME to dial the phone."
+  (message "Going to %s"  (slot-value area 'name))
+
+  (slot-value pers 'animation-speed))
+
+(defvar entrance
+  (person :name "Eric" :birthday "June" :phone "555-5555"))
+
+(defun load-place-from-file (path)
+  (setq path (umn path)))
+
+(load-place-from-file "$HOME/notes/ws/ascii-adventures/house.org")
 
 (provide 'pen-ascii-adventures)
