cfd2a816092657c6f07392e4573716248fb23c80
Shane Mulligan <mullikine@gmail.com>
2024-03-03 02:35:15 +1300

    Sun 03 Mar 2024 02:35:15 AM NZDT

--- src/pen-platform-game.el
+++ src/pen-platform-game.el
@@ -2,7 +2,7 @@
 
 ;; I want:
 ;; - ASCII graphics with animations as objects represented in EIEIO.
-;; - A canvas, which may redraw every second.
+;; - A canvas, which may redraw according to a timer.
 
 
 (provide 'pen-platform-game)
