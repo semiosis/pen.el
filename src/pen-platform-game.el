c4ca088a3f6210a96fc93e5dd0ded75a65ff9b3d
Shane Mulligan <mullikine@gmail.com>
2024-03-03 02:34:55 +1300

    Sun 03 Mar 2024 02:34:55 AM NZDT

--- src/pen-platform-game.el
+++ src/pen-platform-game.el
@@ -1,6 +1,8 @@
 ;; Plan:
 
-;; I want ASCII graphics with animations as objects represented in EIEIO.
+;; I want:
+;; - ASCII graphics with animations as objects represented in EIEIO.
+;; - A canvas, which may redraw every second.
 
 
 (provide 'pen-platform-game)
