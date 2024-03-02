fc5bb23010baf99036471548a8b72bef802c9ce8
Shane Mulligan <mullikine@gmail.com>
2024-03-03 02:39:24 +1300

    Sun 03 Mar 2024 02:39:24 AM NZDT

--- src/pen-platform-game.el
+++ src/pen-platform-game.el
@@ -4,5 +4,9 @@
 ;; - ASCII graphics with animations as objects represented in EIEIO.
 ;; - A canvas, which may redraw according to a timer.
 
+;; Objects:
+;; - canvas
+;; - animation
+
 
 (provide 'pen-platform-game)
