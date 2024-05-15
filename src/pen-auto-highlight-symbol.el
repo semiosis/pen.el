83bc72975f9ad578159d1ec0d211cad85c9a613d
Shane Mulligan <mullikine@gmail.com>
2024-05-16 11:44:45 +1200

    16.05.24

--- src/pen-auto-highlight-symbol.el
+++ src/pen-auto-highlight-symbol.el
@@ -2,6 +2,8 @@
 
 (define-key auto-highlight-symbol-mode-map (kbd "M-<right>") nil)
 (define-key auto-highlight-symbol-mode-map (kbd "M-<left>") nil)
+(define-key auto-highlight-symbol-mode-map (kbd "M-S-<right>") nil)
+(define-key auto-highlight-symbol-mode-map (kbd "M-S-<left>") nil)
 (define-key auto-highlight-symbol-mode-map (kbd "M--") nil)
 
 (provide 'pen-auto-highlight-symbol)
