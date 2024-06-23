3684be0f1aa19f6d4bdc6404645404b15f40283e
Shane Mulligan <mullikine@gmail.com>
2024-06-23 19:31:37 +1200

    23.06.24

--- config/filters/grepfilters.sh
+++ config/filters/grepfilters.sh
@@ -2,3 +2,5 @@ unlinkify                                                   # Unlinkify/unclinki
 relinkify                                                   # Linkify/Clinkify all urls
 grepfilter "sed 's/./_/g'" "rosie grep -o subs net.any"     # Blank out IP addresses
 grepfilter "sed 's/./_/g'" "xurls"                          # Blank out URLs
+biblegateway-relinkify
+biblegateway-unlinkify
