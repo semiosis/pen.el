88fe7c3fb39a38d80450e442932ad069df40300e
Shane Mulligan <mullikine@gmail.com>
2024-07-14 10:09:21 +1200

    14.07.24

--- config/filters/grepfilters.sh
+++ config/filters/grepfilters.sh
@@ -1,5 +1,7 @@
-unlinkify                                                   # Unlinkify/unclinkify all urls
 relinkify                                                   # Linkify/Clinkify all urls
+unlinkify                                                   # Unlinkify/unclinkify all urls
+relinkify-all                                               # Linkify/Clinkify all known links (URL, Biblegateway, etc.)
+unlinkify-all                                               # Unlinkify/unclinkify all known links (URL, Biblegateway, etc.)
 grepfilter "sed 's/./_/g'" "rosie grep -o subs net.any"     # Blank out IP addresses
 grepfilter "sed 's/./_/g'" "xurls"                          # Blank out URLs
 biblegateway-relinkify
