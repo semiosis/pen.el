36193fa3fd343ad4f0ce3776af388f3403d350b0
Shane Mulligan <mullikine@gmail.com>
2024-08-31 12:14:56 +1200

    31.08.24

--- src/pen-eshell.el
+++ src/pen-eshell.el
@@ -430,4 +430,86 @@ environment, as specified in `eshell-variable-aliases-list'."
         ("9" ,(lambda () (nth 8 eshell-command-arguments)) nil t)
         ("*" (eshell-command-arguments . nil))))
 
+;; TODO: Make it so =eshell= makes buttons out of =ls= results
+;; Frustratingly, it seems like the global map is not respecting the mouse text properties
+(defun eshell-ls-decorated-name (file)
+  "Return FILE, possibly decorated."
+  (if eshell-ls-use-colors
+      (let ((face
+             (cond
+              ((not (cdr file))
+               'eshell-ls-missing)
+
+              ((stringp (cadr file))
+               'eshell-ls-symlink)
+
+              ((eq (cadr file) t)
+               'eshell-ls-directory)
+
+              ((not (eshell-ls-filetype-p (cdr file) ?-))
+               'eshell-ls-special)
+
+              ((and (/= (user-uid) 0)   ; root can execute anything
+                    (eshell-ls-applicable (cdr file) 3
+                                          'file-executable-p (car file)))
+               'eshell-ls-executable)
+
+              ((not (eshell-ls-applicable (cdr file) 1
+                                          'file-readable-p (car file)))
+               'eshell-ls-unreadable)
+
+              ((string-match eshell-ls-archive-regexp (car file))
+               'eshell-ls-archive)
+
+              ((string-match eshell-ls-backup-regexp (car file))
+               'eshell-ls-backup)
+
+              ((string-match eshell-ls-product-regexp (car file))
+               'eshell-ls-product)
+
+              ((string-match eshell-ls-clutter-regexp (car file))
+               'eshell-ls-clutter)
+
+              ((not (eshell-ls-applicable (cdr file) 2
+                                          'file-writable-p (car file)))
+               'eshell-ls-readonly)
+              (eshell-ls-highlight-alist
+               (let ((tests eshell-ls-highlight-alist)
+                     value)
+                 (while tests
+                   (if (funcall (caar tests) (car file) (cdr file))
+                       (setq value (cdar tests) tests nil)
+                     (setq tests (cdr tests))))
+                 value)))))
+
+        (let* ((map (make-sparse-keymap))
+               (link-start 0)
+               (link-end (length (car file)))
+
+               (text
+                (progn
+                  (if face
+                      (add-text-properties link-start link-end
+                                           `(keymap ,map
+                                                    font-lock-face ,face
+                                                    mouse-face highlight
+                                                    ;; help-echo "mouse-2: visit this file in other window"
+                                                    help-echo "RET: find file")
+                                           (car file))
+                    (add-text-properties link-start link-end
+                                         `(keymap ,map
+                                                  mouse-face highlight
+                                                  ;; help-echo "mouse-2: visit this file in other window"
+                                                  help-echo "RET: find file")
+                                         (car file))))))
+
+          ;; (define-key map [mouse-down-3] 'dired-mouse-find-file-other-window)
+          ;; (define-key map [mouse-down-1] 'pen-find-file)
+          ;; RET - this works but I need a non-mouse function
+          (define-key map [?\r] 'pen-find-file)
+          ;; (put-text-property link-start link-end 'keymap map (car file))
+
+          text)))
+  (car file))
+
 (provide 'pen-eshell)
