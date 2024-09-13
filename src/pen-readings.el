fb5c5e667e822096d4c52eb5774ac4d41c802ed9
Shane Mulligan <mullikine@gmail.com>
2024-09-14 11:57:58 +1200

    14.09.24

--- src/pen-readings.el
+++ src/pen-readings.el
@@ -1,6 +1,19 @@
 ;; [[https://www.youtube.com/results?search_query=David+Suchet++%23psalms][David Suchet #Psalms]]
 ;; [[https://www.youtube.com/playlist?list=PL2k_typBtbQefGU24bjDObfsc4HhHna8N][youtube.com: David Suchet NIV Psalms]]
+;; https://www.youtube.com/@HearGodsWord
+
+;; TODO: Automate loading these
+;; It has all the Chapters for all the books in NIV
+;; https://www.youtube.com/@HearGodsWord/playlists
+;; Psalms:
+;; https://www.youtube.com/playlist?list=PL2k_typBtbQefGU24bjDObfsc4HhHna8N
 (defset pen-scripture-readings
-        `(("Psalms 51" "[[https://www.youtube.com/watch?v=JMCIE1ERKnM][youtube.com: David Suchet NIV Bible 0529 Psalm 51]]")))
+        `(("Psalms 1" "[[https://www.youtube.com/watch?v=mLEFeeYy5NE][youtube.com: David Suchet NIV Bible 0479 Psalm 1]]")
+          ("Psalms 2" "[[https://www.youtube.com/watch?v=ntfiKI4XkeI&list=PL2k_typBtbQefGU24bjDObfsc4HhHna8N&index=2][youtube.com: David Suchet NIV Bible 0480 Psalm 2]]")
+          ("Psalms 3" "[[https://www.youtube.com/watch?v=I-7EU5HMKSc&list=PL2k_typBtbQefGU24bjDObfsc4HhHna8N&index=3][youtube.com: David Suchet NIV Bible 0481 Psalm 3]]")
+          ("Psalms 19" "[[https://www.youtube.com/watch?v=lj6EHlOtXS4&list=PL2k_typBtbQefGU24bjDObfsc4HhHna8N&index=19][youtube.com: David Suchet NIV Bible 0497 Psalm 19]]")
+          ("Psalms 23" "[[https://www.youtube.com/watch?v=2YuqD2zTuZs][youtube.com: David Suchet NIV Bible 0501 Psalm 23]]")
+          ("Psalms 51" "[[https://www.youtube.com/watch?v=JMCIE1ERKnM][youtube.com: David Suchet NIV Bible 0529 Psalm 51]]")
+          ("Psalms 91" "[[https://www.youtube.com/watch?v=pxc-kkBSRhc][youtube.com: David Suchet NIV Bible 0569 Psalm 91]]")))
 
 (provide 'pen-readings)
