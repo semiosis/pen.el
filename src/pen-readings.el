dd6d35aad98bf58db12516689b4b6324fc5d7a53
Shane Mulligan <mullikine@gmail.com>
2024-09-14 11:51:44 +1200

    Sat 14 Sep 2024 11:51:44 AM NZST

---
+++ src/pen-readings.el
@@ -0,0 +1,5 @@
+(defset pen-scripture-readings
+        `(("Psalms 51" "[[https://www.youtube.com/watch?v=JMCIE1ERKnM][youtube.com: David Suchet NIV Bible 0529 Psalm 51]]")))
+
+
+(provide 'pen-readings)
