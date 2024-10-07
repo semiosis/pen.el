468f9248daaa4bd9e7fd40a3552abf11358c63be
Shane Mulligan <mullikine@gmail.com>
2024-10-07 16:30:43 +1300

    07.10.24

--- src/pen-readings.el
+++ src/pen-readings.el
@@ -2,6 +2,8 @@
 ;; [[https://www.youtube.com/playlist?list=PL2k_typBtbQefGU24bjDObfsc4HhHna8N][youtube.com: David Suchet NIV Psalms]]
 ;; https://www.youtube.com/@HearGodsWord
 
+;; "[[https://www.youtube.com/watch?v=hjf0R9LlHdM][youtube.com: Sit and Relax with The Word of God]]"
+
 ;; TODO: Automate loading these
 ;; It has all the Chapters for all the books in NIV
 ;; https://www.youtube.com/@HearGodsWord/playlists
@@ -14,6 +16,7 @@
           ("Psalms 19" "[[https://www.youtube.com/watch?v=lj6EHlOtXS4&list=PL2k_typBtbQefGU24bjDObfsc4HhHna8N&index=19][youtube.com: David Suchet NIV Bible 0497 Psalm 19]]")
           ("Psalms 23" "[[https://www.youtube.com/watch?v=2YuqD2zTuZs][youtube.com: David Suchet NIV Bible 0501 Psalm 23]]")
           ("Psalms 51" "[[https://www.youtube.com/watch?v=JMCIE1ERKnM][youtube.com: David Suchet NIV Bible 0529 Psalm 51]]")
-          ("Psalms 91" "[[https://www.youtube.com/watch?v=pxc-kkBSRhc][youtube.com: David Suchet NIV Bible 0569 Psalm 91]]")))
+          ("Psalms 91" "[[https://www.youtube.com/watch?v=pxc-kkBSRhc][youtube.com: David Suchet NIV Bible 0569 Psalm 91]]")
+          ("Psalms 121" "[[https://www.youtube.com/watch?v=t_auzHMHOm4][youtube.com: David Suchet NIV Bible 0599 Psalm 121]]")))
 
 (provide 'pen-readings)
