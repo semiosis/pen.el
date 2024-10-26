360b20e10637708179bedfbfb6020e50164d2f26
Shane Mulligan <mullikine@gmail.com>
2024-10-26 15:39:17 +1300

    26.10.24

--- src/pen-javascript.el
+++ src/pen-javascript.el
@@ -3,8 +3,9 @@
 (defun js-guess-and-set-indent-width ()
   (interactive)
 
-  (let ((width (guess-style-guess-indent)))
-    (setq js-indent-level width)))
+  (ignore-errors
+    (let ((width (guess-style-guess-indent)))
+      (setq js-indent-level width))))
 
 (add-hook 'js-mode-hook 'js-guess-and-set-indent-width)
 
