69a2e7eb1627020dc982022c0cc3541a9b88ab13
Shane Mulligan <mullikine@gmail.com>
2024-12-04 14:47:07 +1300

    04.12.24

--- src/pen-after-init.el
+++ src/pen-after-init.el
@@ -11,4 +11,38 @@
 
 (add-hook 'after-init-hook 'after-init-loads)
 
+(defun crc32 (s)
+  (pen-snc "hash-crc32" s))
+
+(defun hex2dec (hex)
+  "hex can be one hex num per line"
+  (let ((results
+         (cl-loop for n in (str2lines hex) collect
+                  (string-to-number (pen-snc (concat "echo " n " | math hex2dec"))))))
+    (if (eq 1 (length results))
+        (car results)
+      results)))
+
+(defun pen-tcp-server-get-port ()
+  (+ 5000 
+     (/ (hex2dec (crc32 (daemonp))) 10000000)))
+
+(defun pen-start-tcp-server-repl ()
+  (interactive)
+  (sps (cmd-f "zrepl" "pen-tcp-server" (str (pen-tcp-server-get-port)) "(tetris)")))
+
+;; [[sh:"pen-tcp-server" "5369" "(pen-disable-all-faces)"]]
+;; [[sh:"pen-tcp-server" "5369" "(pen-enable-all-faces)"]]
+
+(defun pen-start-tcp-server ()
+  (interactive)
+  ;; I should also save the ports somewhere.
+  ;; This way I can send commands to emacs from shell scripts, such as toggling black and white mode
+  
+  (ignore-errors
+    (let ((port (pen-tcp-server-get-port)))
+      (tcp-server-start port))))
+
+(add-hook 'after-init-hook 'pen-start-tcp-server)
+
 (provide 'pen-after-init)
