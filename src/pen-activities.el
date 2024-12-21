afe5f2cff033d31bfcf38cbf077dca6991bd7279
Shane Mulligan <mullikine@gmail.com>
2024-12-21 13:16:22 +1300

    21.12.24

--- src/pen-activities.el
+++ src/pen-activities.el
@@ -3,4 +3,22 @@
 ;; https://github.com/alphapapa/activities.el
 ;; https://raw.githubusercontent.com/alphapapa/activities.el/master/README.org
 
+(use-package activities
+  :init
+  (activities-mode)
+  (activities-tabs-mode)
+  ;; Prevent `edebug' default bindings from interfering.
+  (setq edebug-inhibit-emacs-lisp-mode-bindings t)
+
+  :bind
+  (("C-x C-a C-n" . activities-new)
+   ("C-x C-a C-d" . activities-define)
+   ("C-x C-a C-a" . activities-resume)
+   ("C-x C-a C-s" . activities-suspend)
+   ("C-x C-a C-k" . activities-kill)
+   ("C-x C-a RET" . activities-switch)
+   ("C-x C-a b" . activities-switch-buffer)
+   ("C-x C-a g" . activities-revert)
+   ("C-x C-a l" . activities-list)))
+
 (provide 'pen-activities)
