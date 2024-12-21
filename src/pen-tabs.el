215dfa8b8f630221478dbac1ac0575df409091ad
Shane Mulligan <mullikine@gmail.com>
2024-12-21 15:40:45 +1300

    21.12.24

--- src/pen-tabs.el
+++ src/pen-tabs.el
@@ -1,7 +1,13 @@
 (require 'tab-bar)
 (require 'tab-line)
 
+;; Always keep the tab bar hidden, even when the mode is on.
+;; This is the way to go to avoid the failed assertion
+(setq tab-bar-show nil)
 ;; Disable the built-in emacs tab-bar-mode and keep using tab-line-mode for the moment
 (tab-bar-mode -1)
+(tab-bar-mode t)
+
+(tab-line-mode -1)
 
 (provide 'pen-tabs)
